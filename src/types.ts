9e5c0f9b28c4122ad61495099d611d6d2ea87a9f
Anxo Rodriguez <anxolin@gmail.com>
2019-09-10 13:09:27 +0200

    Add token list

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,14 @@
-declare var VERSION: string
+export declare var VERSION: string
+
+export enum Network {
+  Mainnet = 1,
+  Rinkeby = 4,
+}
+
+export interface TokenDetails {
+  name?: string
+  symbol?: string
+  decimals?: number
+  address: string
+  image?: string
+}
