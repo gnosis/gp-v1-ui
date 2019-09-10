d4f464b6d7b1e0a00594bb238c61086bce81c026
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-09-10 17:49:24 +0200

    Grouping tests

--- test/components/DepositWidget.test.ts
+++ test/components/DepositWidget.test.ts
@@ -1,11 +1,13 @@
 import { getButtonText, ButtonTypes } from '../../src/components/DepositWidget/Row'
 
-test('Deposit button text', () => {
-  expect(getButtonText(ButtonTypes.DEPOSIT, '')).toMatch(/Deposit/)
-})
-test('Withdraw button text', () => {
-  expect(getButtonText(ButtonTypes.WITHDRAW, '')).toMatch(/Withdraw/)
-})
-test('Deposit button text', () => {
-  expect(getButtonText(ButtonTypes.ENABLE, 'TTT')).toMatch(/Enable TTT/)
+describe('Get text for buttons', () => {
+  test('Deposit', () => {
+    expect(getButtonText(ButtonTypes.DEPOSIT, '')).toMatch(/Deposit/)
+  })
+  test('Withdraw', () => {
+    expect(getButtonText(ButtonTypes.WITHDRAW, '')).toMatch(/Withdraw/)
+  })
+  test('Enable', () => {
+    expect(getButtonText(ButtonTypes.ENABLE, 'TTT')).toMatch(/Enable TTT/)
+  })
 })
