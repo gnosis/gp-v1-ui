fca2b7f8d7c59aca6590ffbdb8641a3c920ad666
Anxo Rodriguez <anxolin@gmail.com>
2019-09-10 14:06:35 +0200

    Show images in widget

--- src/components/DepositWidget/Table.tsx
+++ src/components/DepositWidget/Table.tsx
@@ -1,46 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Row, RowProps } from './Row'
+import { Row } from './Row'
+
+import { Network, TokenBalanceDetails } from 'types'
+import { tokenApi } from 'api'
 
 const TableWrapper = styled.table``
 
-const rowData: RowProps[] = [
-  {
-    tokenLogo: 'img.png',
-    tokenName: 'TTT',
+function _getBalances(): TokenBalanceDetails[] {
+  const tokens = tokenApi.getTokens(Network.Rinkeby)
+  // Mock implementation
+  return tokens.map(token => ({
+    ...token,
     exchangeWallet: 0,
     pendingDeposits: 0,
     pendingWithdraws: 0,
-  },
-  {
-    tokenLogo: 'img.png',
-    tokenName: 'SSS',
-    exchangeWallet: 0,
-    pendingDeposits: 0,
-    pendingWithdraws: 0,
-    enableToken: true,
-  },
-]
+    enabled: true,
+  }))
+}
 
-const Table: React.FC = () => (
-  <TableWrapper>
-    <thead>
-      <tr>
-        <th></th>
-        <th>Token</th>
-        <th>Exchange wallet</th>
-        <th>Pending deposits</th>
-        <th>Pending withdraws</th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {rowData.map(data => (
-        <Row key={data.tokenName} {...data} />
-      ))}
-    </tbody>
-  </TableWrapper>
-)
+const Table: React.FC = () => {
+  const tokenBalancesList = _getBalances()
+
+  return (
+    <TableWrapper>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Token</th>
+          <th>Exchange wallet</th>
+          <th>Pending deposits</th>
+          <th>Pending withdraws</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {tokenBalancesList.map(tokenBalances => (
+          <Row key={tokenBalances.addressMainnet} tokenBalances={tokenBalances} />
+        ))}
+      </tbody>
+    </TableWrapper>
+  )
+}
 
 export default Table
