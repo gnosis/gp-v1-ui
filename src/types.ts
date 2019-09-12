4ec336af6e54fa75cfbf6d15d81d21801e7a176e
Anxo Rodriguez <anxolin@gmail.com>
2019-09-12 12:02:20 +0200

    Define the basic API for stable coin

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import BN from 'bn.js'
 export declare var VERSION: string
 
 export enum Network {
@@ -23,4 +24,36 @@ export interface TokenBalanceDetails extends TokenDetails {
 
 export interface TokenApi {
   getTokens: (networkId: number) => TokenDetails[]
+}
+
+export interface Order {
+  buyTokenId: number
+  sellTokenId: number
+  validFrom: number
+  validUntil: number
+  isSellOrder: boolean
+  priceNumerator: BN
+  priceDenominator: BN
+  remainingAmount: BN
+}
+
+export interface AddOrderParams {
+  buyTokenId: number
+  sellTokenId: number
+  isSellOrder: boolean
+  validUntil: number
+  sellAmount: BN
+  buyAmount: BN
+}
+
+export interface StableCoinConverterApi {
+  getOrders(userAddress: string): Promise<Order[]>
+  getNumTokens(): Promise<number>
+  getFeeDenominator(): Promise<number>
+  addToken(tokenAddress: string): Promise<void>
+  placeOrder(orderParams: AddOrderParams): Promise<number>
+  cancelOrder(orderId: number): Promise<void>
+  getUserAddress(userId: number): Promise<string> // tokenAddressToIdMap
+  getUserId(userAddress: string): Promise<number>
+  getCurrentPrice(tokenId: number): Promise<BN>
 }
