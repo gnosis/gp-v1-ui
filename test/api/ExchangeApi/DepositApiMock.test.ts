3ef3546aa7999486b8b6013e7ae3d04647f31158
Anxo Rodriguez <anxolin@gmail.com>
2019-09-13 17:45:20 +0200

    Implement some tests for deposit API

--- test/api/ExchangeApi/DepositApiMock.test.ts
+++ test/api/ExchangeApi/DepositApiMock.test.ts
@@ -1,14 +1,57 @@
 import { DepositApi } from 'types'
 
-import { BATCH_TIME } from 'constants/'
+import { BATCH_TIME, ZERO } from 'constants/'
 import { DepositApiMock } from 'api/ExchangeApi/mock/DepositApiMock'
 import * as testHelpers from '../../testHelpers'
+
+const { USER_1, USER_2, TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5, AMOUNT } = testHelpers
 
 let instance: DepositApi
 
 beforeAll(() => {
-  instance = new DepositApiMock()
+  instance = new DepositApiMock({
+    [USER_1]: {
+      [TOKEN_1]: {
+        balance: ZERO,
+        pendingDeposits: {
+          amount: ZERO,
+          stateIndex: 0,
+        },
+        pendingWithdraws: {
+          amount: ZERO,
+          stateIndex: 0,
+        },
+      },
+    },
+  })
   testHelpers.mockTimes()
+})
+
+beforeEach(() => {
+  instance = new DepositApiMock({
+    [USER_1]: {
+      [TOKEN_1]: {
+        balance: ZERO,
+        pendingDeposits: { amount: ZERO, stateIndex: 0 },
+        pendingWithdraws: { amount: ZERO, stateIndex: 0 },
+      },
+      [TOKEN_2]: {
+        balance: AMOUNT,
+        pendingDeposits: { amount: ZERO, stateIndex: 0 },
+        pendingWithdraws: { amount: ZERO, stateIndex: 0 },
+      },
+      [TOKEN_4]: {
+        balance: ZERO,
+        pendingDeposits: { amount: AMOUNT, stateIndex: 1 },
+        pendingWithdraws: { amount: AMOUNT, stateIndex: 1 },
+      },
+      [TOKEN_5]: {
+        balance: ZERO,
+        pendingDeposits: { amount: AMOUNT, stateIndex: testHelpers.BATCH_NUMBER },
+        pendingWithdraws: { amount: AMOUNT, stateIndex: testHelpers.BATCH_NUMBER },
+      },
+    },
+  })
 })
 
 describe('Basic view functions', () => {
@@ -26,4 +69,175 @@ describe('Basic view functions', () => {
     const remainingSeconds = await instance.getSecondsRemainingInBatch()
     expect(remainingSeconds).toBe(BATCH_TIME - testHelpers.BATCH_SECOND)
   })
+})
+
+describe('Get balance', () => {
+  test('Unknown user address', async () => {
+    // GIVEN: A user that doesn't have any deposits
+
+    // WHEN: We get the balance
+    const balance = await instance.getBalance(USER_2, TOKEN_1)
+
+    // THEN: The balance is zero
+    expect(balance).toEqual(ZERO)
+  })
+
+  test('Unknown token', async () => {
+    // GIVEN: A user that has deposits for some tokens, but not for TOKEN_3
+
+    // WHEN: We get the balance for TOKEN_3
+    const balance = await instance.getBalance(USER_1, TOKEN_3)
+
+    // THEN: The balance is zero
+    expect(balance).toEqual(ZERO)
+  })
+
+  test('Token with no balance', async () => {
+    // GIVEN: A user that has balance zero for TOKEN_1
+
+    // WHEN: We get the balance for TOKEN_1
+    const balance = await instance.getBalance(USER_1, TOKEN_1)
+
+    // THEN: The balance is zero
+    expect(balance).toEqual(ZERO)
+  })
+
+  test('Token with balance', async () => {
+    // GIVEN: A user that has balance for TOKEN_2
+
+    // WHEN: We get the balance for TOKEN_2
+    const balance = await instance.getBalance(USER_1, TOKEN_2)
+
+    // THEN: The balance is AMOUNT
+    expect(balance).toEqual(AMOUNT)
+  })
+})
+
+describe('Get pending deposit amounts', () => {
+  test('Unknown user address', async () => {
+    // GIVEN: An user that hasn't deposit any tokens yet
+    // WHEN: -
+
+    // THEN: there's no pending deposits
+    expect(await instance.getPendingDepositAmount(USER_2, TOKEN_1)).toEqual(ZERO)
+
+    // THEN: there's no batch number
+    expect(await instance.getPendingDepositBatchNumber(USER_2, TOKEN_1)).toEqual(0)
+  })
+
+  test('Unknown token', async () => {
+    // GIVEN: An user with no token balance for TOKEN_3
+    // WHEN: -
+
+    // THEN: there's no pending deposits
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_3)).toEqual(ZERO)
+
+    // THEN: there's no batch number
+    expect(await instance.getPendingDepositBatchNumber(USER_1, TOKEN_3)).toEqual(0)
+  })
+
+  test('No pending balance', async () => {
+    // GIVEN: An user with no balance for TOKEN_2
+    // WHEN: -
+
+    // THEN: there's no pending deposits
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_2)).toEqual(ZERO)
+  })
+})
+
+describe('Get pending withdraw amounts', () => {
+  test('Unknown user address', async () => {
+    // GIVEN: An user that hasn't any tokens yet
+    // WHEN: -
+
+    // THEN: there's no pending withdraws
+    expect(await instance.getPendingWithdrawAmount(USER_2, TOKEN_1)).toEqual(ZERO)
+
+    // THEN: there's no batch number
+    expect(await instance.getPendingWithdrawBatchNumber(USER_2, TOKEN_1)).toEqual(0)
+  })
+
+  test('Unknown token', async () => {
+    // GIVEN: An user with no token balance for TOKEN_3
+    // WHEN: -
+
+    // THEN: there's no pending withdraws
+    expect(await instance.getPendingWithdrawAmount(USER_1, TOKEN_3)).toEqual(ZERO)
+
+    // THEN: there's no batch number
+    expect(await instance.getPendingWithdrawBatchNumber(USER_1, TOKEN_3)).toEqual(0)
+  })
+
+  test('No pending balance', async () => {
+    // GIVEN: An user with no balance for TOKEN_2
+    // WHEN: -
+
+    // THEN: there's no pending withdraws
+    expect(await instance.getPendingWithdrawAmount(USER_1, TOKEN_2)).toEqual(ZERO)
+  })
+})
+
+describe('Deposit', () => {
+  test('Unknown token', async () => {
+    // GIVEN: An user with no token balance for TOKEN_3 and no pending deposits
+    expect(await instance.getBalance(USER_1, TOKEN_3)).toEqual(ZERO)
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_3)).toEqual(ZERO)
+
+    // WHEN: Deposits AMOUNT
+    await instance.deposit(USER_1, TOKEN_3, AMOUNT)
+
+    // THEN: There's still no balance
+    expect(await instance.getBalance(USER_1, TOKEN_3)).toEqual(ZERO)
+
+    // THEN: There's a pending deposit of AMOUNT
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_3)).toEqual(AMOUNT)
+  })
+
+  test('No balance', async () => {
+    // GIVEN: An user with no token balance for TOKEN_1 and no pending deposits
+    expect(await instance.getBalance(USER_1, TOKEN_1)).toEqual(ZERO)
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_1)).toEqual(ZERO)
+
+    // WHEN: Deposits AMOUNT
+    await instance.deposit(USER_1, TOKEN_1, AMOUNT)
+
+    // THEN: There's still no balance
+    expect(await instance.getBalance(USER_1, TOKEN_1)).toEqual(ZERO)
+
+    // THEN: There's a pending deposit of AMOUNT
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_1)).toEqual(AMOUNT)
+  })
+
+  test('Applicable pending balance', async () => {
+    // GIVEN: An user with no balance for TOKEN_4 but and an applicable pending deposits
+    expect(await instance.getBalance(USER_1, TOKEN_4)).toEqual(ZERO)
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_4)).toEqual(AMOUNT)
+
+    // WHEN: Deposits AMOUNT
+    await instance.deposit(USER_1, TOKEN_4, AMOUNT)
+
+    // THEN: The previous pending deposit is applied
+    expect(await instance.getBalance(USER_1, TOKEN_4)).toEqual(AMOUNT)
+
+    // THEN: There's a pending deposit of AMOUNT
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_4)).toEqual(AMOUNT)
+  })
+
+  test('Unapplicable pending balance', async () => {
+    // GIVEN: An user with no balance for TOKEN_5 and a non applicable pending deposit
+    expect(await instance.getBalance(USER_1, TOKEN_5)).toEqual(ZERO)
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_5)).toEqual(AMOUNT)
+
+    // WHEN: Deposits AMOUNT
+    await instance.deposit(USER_1, TOKEN_5, AMOUNT)
+
+    // THEN: There's still no balance
+    expect(await instance.getBalance(USER_1, TOKEN_5)).toEqual(ZERO)
+
+    // THEN: There's a pending deposit of AMOUNT + AMOUNT
+    expect(await instance.getPendingDepositAmount(USER_1, TOKEN_5)).toEqual(AMOUNT.add(AMOUNT))
+  })
+
+  // TODO: requestWithdraw
+  // TODO: withdraw
 })
