dec642c6718d32bb54647464b653b39e2c638d72
Anxo Rodriguez <anxolin@gmail.com>
2019-09-13 18:12:16 +0200

    Add Order and AddOrderParams types

--- src/types/index.ts
+++ src/types/index.ts
@@ -88,6 +88,27 @@ export interface DepositApi {
   withdraw(userAddress: string, tokenAddress: string, txOptionalParams?: TxOptionalParams): Promise<TxResult<void>>
 }
 
+export interface Order {
+  buyTokenId: number
+  sellTokenId: number
+  validFrom: number
+  validUntil: number
+  isSellOrder: boolean
+  priceNumerator: BN
+  priceDenominator: BN
+  remainingAmount: BN
+}
+
+export interface AddOrderParams {
+  userAddress: string
+  buyTokenId: number
+  sellTokenId: number
+  isSellOrder: boolean
+  validUntil: number
+  sellAmount: BN
+  buyAmount: BN
+}
+
 export interface ExchangeApi extends DepositApi {
   getOrders(userAddress: string): Promise<Order[]>
   getNumTokens(): Promise<number>
