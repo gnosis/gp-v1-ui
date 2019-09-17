9d66f04ce92871087dbbb995bfc65f0b86d0ecd7
Anxo Rodriguez <anxolin@gmail.com>
2019-09-17 15:23:10 +0200

    Create mock API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,22 @@
-import { TokenApi, Network } from 'types'
+import { Network, TokenApi, DepositApi } from 'types'
 import { TokenApiImpl } from './TokenApi/TokenApiImpl'
+import { DepositApiMock } from './ExchangeApi/mock/DepositApiMock'
+import balanceStates from '../../test/data/balanceStates'
+
+const isMock = process.env.MOCK === 'true'
+
+function createTokenApi(): TokenApi {
+  return new TokenApiImpl([Network.Mainnet, Network.Rinkeby])
+}
+
+function createDepositApi(): DepositApi {
+  if (true || isMock) {
+    return new DepositApiMock({ balanceStates })
+  } else {
+    throw new Error('Only has been implemented')
+  }
+}
 
 // Build APIs
-export const tokenApi: TokenApi = new TokenApiImpl([Network.Mainnet, Network.Rinkeby])
+export const tokenApi = createTokenApi()
+export const depositApi = createDepositApi()
