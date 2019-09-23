6dfddcf90f1221ae6dd0990715019931720a2fa2
Anxo Rodriguez <anxolin@gmail.com>
2019-09-23 19:39:26 +0200

    Fix mock data to use the right decimals

--- test/data/erc20Allowances.ts
+++ test/data/erc20Allowances.ts
@@ -1,29 +1,29 @@
 import { ZERO, ALLOWANCE_VALUE } from 'const'
 import { USER_1, TOKEN_1, CONTRACT, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5, TOKEN_6, TOKEN_7 } from './basic'
-import { AMOUNT, AMOUNT_SMALL } from './basic'
+import BN from 'bn.js'
 
 export default {
   [USER_1]: {
     [TOKEN_1]: {
-      [CONTRACT]: ZERO,
+      [CONTRACT]: ZERO, // 0, WETH: decimals=18
     },
     [TOKEN_2]: {
-      [CONTRACT]: ALLOWANCE_VALUE,
+      [CONTRACT]: new BN('500000'), // 0.5, USDT: decimals=6
     },
     [TOKEN_3]: {
-      [CONTRACT]: ALLOWANCE_VALUE,
+      [CONTRACT]: ALLOWANCE_VALUE, // MAX, TUSD: decimals=18
     },
     [TOKEN_4]: {
-      [CONTRACT]: AMOUNT,
+      [CONTRACT]: new BN('1000000'), // 1, USDC: decimals=6
     },
     [TOKEN_5]: {
-      [CONTRACT]: AMOUNT_SMALL,
+      [CONTRACT]: new BN('1000000000000000000'), // 1, PAX: decimals=18
     },
     [TOKEN_6]: {
-      [CONTRACT]: ALLOWANCE_VALUE,
+      [CONTRACT]: ALLOWANCE_VALUE, // MAX, GUSD: decimals=6
     },
     [TOKEN_7]: {
-      [CONTRACT]: ZERO,
+      [CONTRACT]: ZERO, // 0, DAI: decimals=18
     },
   },
 }
--- test/data/exchangeBalanceStates.ts
+++ test/data/exchangeBalanceStates.ts
@@ -1,7 +1,7 @@
 import BN from 'bn.js'
 import { ZERO } from 'const'
 
-import { USER_1, TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5, TOKEN_6 } from './basic'
+import { USER_1, TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5, TOKEN_6, TOKEN_7 } from './basic'
 import { BalancesByUserAndToken } from 'api/exchange/DepositApiMock'
 
 const FLUX_ZERO = {
@@ -17,16 +17,18 @@ const STATE_ZERO = {
 
 const exchangeBalanceStates: BalancesByUserAndToken = {
   [USER_1]: {
-    [TOKEN_1]: STATE_ZERO,
+    [TOKEN_1]: STATE_ZERO, // 0. WETH: decimals=18
     [TOKEN_2]: {
-      balance: new BN('1750173903215790623569'), // 1,750.07
+      // 0. USDT: decimals=6
+      balance: new BN('1750173903'), // 1,750.07
       pendingDeposits: {
-        amount: new BN('850000000000000000'), // 0.85,
+        amount: new BN('850000'), // 0.85,
         batchId: 1,
       },
       pendingWithdraws: FLUX_ZERO,
     },
     [TOKEN_3]: {
+      // 0. TUSDC: decimals=18
       balance: new BN('500000000000000000'), // 0.5
       pendingDeposits: FLUX_ZERO,
       pendingWithdraws: {
@@ -34,16 +36,18 @@ const exchangeBalanceStates: BalancesByUserAndToken = {
         batchId: 1,
       },
     },
-    [TOKEN_4]: STATE_ZERO,
-    [TOKEN_5]: STATE_ZERO,
+    [TOKEN_4]: STATE_ZERO, // 0. USDC: decimals=6
+    [TOKEN_5]: STATE_ZERO, // 0. PAX: decimals=18
     [TOKEN_6]: {
-      balance: new BN('50048296532871266216000'), // 500,482.96
+      // 0. GUSD: decimals=6
+      balance: new BN('50048296532'), // 500,482.96
       pendingDeposits: FLUX_ZERO,
       pendingWithdraws: {
-        amount: new BN('101479327327532326732'), // 1,
+        amount: new BN('101479327'), // 1,
         batchId: Number.MAX_SAFE_INTEGER,
       },
     },
+    [TOKEN_7]: undefined, // 0. DAI: decimals=18
   },
 }
 
