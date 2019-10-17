630ca10c6d91007b7d4b593c8976eba09db4f1f2
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-10-17 12:17:19 -0700

    Added base TradeWidget

---
+++ src/components/TradeWidget/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+import Widget from 'components/layout/Widget'
+
+const TradeWidget: React.FC = () => {
+  return <Widget></Widget>
+}
+
+export default TradeWidget
