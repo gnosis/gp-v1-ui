a6386b033ec76769ab4ee460de1d7e30db05a49d
Velenir <velenir@gmail.com>
2019-10-25 18:24:41 +0300

    add toBN to utils re-export from web3-utils

--- src/utils/ethereum.ts
+++ src/utils/ethereum.ts
@@ -1,6 +1,6 @@
 import BN from 'bn.js'
 
-export { fromWei, toWei, isBN } from 'web3-utils'
+export { fromWei, toWei, isBN, toBN } from 'web3-utils'
 
 export function toBnOrNull(value: string | number): BN | null {
   if (value === undefined || value === null || value === '') {
