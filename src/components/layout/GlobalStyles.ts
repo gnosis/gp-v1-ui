1e2a6aa3a72bd0860a16cef20a24a8bbafbc1a29
Leandro Boscariol <alfetopito@users.noreply.github.com>
2019-10-25 12:11:17 -0700

    100/trading page widget (#128)
    * Refactoring out a base Widget component
    
    * Moving `widget` global css to Widget component
    
    * Making Widget diplay:flex column
    
    * Extracted TokenImg into a separated component
    
    * Adding TokenRow component to TradeWidget
    
    * Added base TradeWidget
    
    * Implemented basic TradeWidget
    
    * Refactoring EtherscanLink component to allow styled-components
    subclassing
    
    * Simplifying TokenImg component
    
    * Removed redundant `+` from regex
    
    * Returning `null` rather than `<></>` when empty component
    
    * Using array.find rather than array.filter
    
    * Simplifying link rendering
    
    * Fixing type on trade order button

--- src/components/layout/GlobalStyles.ts
+++ src/components/layout/GlobalStyles.ts
@@ -146,27 +146,18 @@ const GlobalStyles = createGlobalStyle`
     }
   }
 
-  .widget,
+  //TODO: extract into a Page component
   .page {
     background-color: white;
     margin: -3rem auto 3rem auto;
-    box-shadow: 1px 1px #e8e8e8;    
+    box-shadow: 1px 1px #e8e8e8;
     min-height: 25rem;
+    padding: 2rem 10vw 2rem 10vw;
+    width: 95vw;
 
     @media (max-width: 768px) {
       margin: auto;
     }
-  }
-
-  .widget {
-    padding: 2em;
-    border-radius: 10px;
-    min-width: 58vw;
-  }
-
-  .page {
-    padding: 2rem 10vw 2rem 10vw;
-    width: 95vw;
   }
 
 `
