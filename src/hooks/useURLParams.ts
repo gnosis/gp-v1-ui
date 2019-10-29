026f1bbf32cc7a44ab7373daf2be41d4328e2974
David Sato <david.sato64@gmail.com>
2019-10-29 15:50:17 +0100

    useURLParams, useLocation

---
+++ src/hooks/useURLParams.ts
@@ -0,0 +1,27 @@
+import { useState, useMemo } from 'react'
+
+function setURLFilterParams(filterString: string, moveNow?: boolean): string | void | boolean {
+  if (!window || !window.history) return false
+  const defaultLocation = window.location.href.split('?')[0]
+  if (!filterString) return null
+  return moveNow
+    ? (window.location.href = `${defaultLocation}${filterString}`)
+    : window.history.replaceState(null, null, `?${filterString}`)
+}
+
+export function useLocation(): URLSearchParams {
+  const [search, setSearch] = useState(location.search)
+
+  useMemo(() => {
+    search ? console.debug('SEARCH: ', search) : console.debug('No search params')
+    setSearch(search)
+  }, [search])
+
+  return new URLSearchParams(search)
+}
+
+export default function useURLParams(newParams: string): void {
+  useMemo(() => {
+    setURLFilterParams(newParams)
+  }, [newParams])
+}
