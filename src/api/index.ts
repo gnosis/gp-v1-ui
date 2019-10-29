6bec266fb5fdff50bec4445918584b9ecbaaeca0
Velenir <velenir@gmail.com>
2019-10-29 10:42:33 +0300

    turn mocks on independently of each other

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { Network, TokenList, WalletApi, DepositApi, Erc20Api } from 'types'
-// import { WalletApiMock } from './wallet/WalletApiMock'
+import { WalletApiMock } from './wallet/WalletApiMock'
 import WalletApiImpl from './wallet/WalletApiImpl'
 import { TokenListApiImpl } from './tokenList/TokenListApiImpl'
 import { TokenListApiMock } from './tokenList/TokenListApiMock'
@@ -7,36 +7,36 @@ import { Erc20ApiMock } from './erc20/Erc20ApiMock'
 import { DepositApiMock } from './exchange/DepositApiMock'
 import { tokenList, exchangeBalanceStates, erc20Balances, erc20Allowances } from '../../test/data'
 
-const isMock = process.env.MOCK === 'true'
+const isWalletMock = process.env.MOCK_WALLET === 'true'
+const isTokenListMock = process.env.MOCK_TOKEN_LIST === 'true'
+const isErc20Mock = process.env.MOCK_ERC20 === 'true'
+const isDepositMock = process.env.MOCK_DEPOSIT === 'true'
 
 function createWalletApi(): WalletApi {
   let walletApi
-  // if (isMock) {
-  //   walletApi = new WalletApiMock()
-  //   window['walletApi'] = walletApi // register for convenience
-  // } else {
-  // TODO: Add actual implementation
-  // throw new Error('Not implemented yet. Only mock implementation available')
+  if (isWalletMock) {
+    walletApi = new WalletApiMock()
+  } else {
   walletApi = new WalletApiImpl()
+  }
   window['walletApi'] = walletApi // register for convenience
-  // }
   return walletApi
 }
 
 function createTokenListApi(): TokenList {
   let tokenListApi
-  if (isMock) {
+  if (isTokenListMock) {
     tokenListApi = new TokenListApiMock(tokenList)
-    window['tokenListApi'] = tokenListApi // register for convenience
   } else {
     tokenListApi = new TokenListApiImpl([Network.Mainnet, Network.Rinkeby])
   }
+  window['tokenListApi'] = tokenListApi // register for convenience
   return tokenListApi
 }
 
 function createErc20Api(): Erc20Api {
   let erc20Api
-  if (isMock) {
+  if (isErc20Mock) {
     erc20Api = new Erc20ApiMock({ balances: erc20Balances, allowances: erc20Allowances })
     window['erc20Api'] = erc20Api // register for convenience
   } else {
@@ -48,7 +48,7 @@ function createErc20Api(): Erc20Api {
 
 function createDepositApi(erc20Api: Erc20Api): DepositApi {
   let depositApi
-  if (isMock) {
+  if (isDepositMock) {
     depositApi = new DepositApiMock(exchangeBalanceStates, erc20Api)
     window['depositApi'] = depositApi // register for convenience
   } else {
