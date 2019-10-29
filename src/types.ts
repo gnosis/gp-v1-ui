bf33b32697547ea55333a744d899f76584e3c688
Velenir <velenir@gmail.com>
2019-10-29 18:12:48 +0300

    add types for ERC20 contract

--- src/types.ts
+++ src/types.ts
@@ -120,7 +120,7 @@ export interface Erc20Api {
     spenderAddress: string,
     amount: BN,
     txOptionalParams?: TxOptionalParams,
-  ): Promise<TxResult<boolean>>
+  ): Promise<Receipt>
 
   transfer(
     tokenAddress: string,
@@ -128,5 +128,66 @@ export interface Erc20Api {
     toAddress: string,
     amount: BN,
     txOptionalParams?: TxOptionalParams,
-  ): Promise<TxResult<boolean>>
+  ): Promise<Receipt>
+
+  transferFrom(
+    senderAddress: string,
+    tokenAddress: string,
+    fromAddress: string,
+    toAddress: string,
+    amount: BN,
+    txOptionalParams?: TxOptionalParams,
+  ): Promise<Receipt>
+}
+
+import { Contract } from 'web3-eth-contract'
+import { PromiEvent, TransactionConfig, TransactionReceipt } from 'web3-core'
+
+type CallTxOptions = Pick<TransactionConfig, 'from' | 'gas' | 'gasPrice'>
+type SendTxOptions = Pick<TransactionConfig, 'from' | 'gas' | 'gasPrice' | 'value'>
+type EstimateGasTxOptions = Pick<TransactionConfig, 'from' | 'gas' | 'value'>
+
+type CallTxCallback = <T>(error: Error | null, result: T) => void
+type SendTxCallback = (error: Error | null, transactionHash: string) => void
+type EstimateGasTxCallback = (error: Error | null, gasAmount: number) => void
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+interface TransactionObject<T, U extends any[] = []> {
+  arguments: U
+
+  call(callback: CallTxCallback): Promise<T>
+  call(tx?: CallTxOptions, callback?: CallTxCallback): Promise<T>
+
+  send(callback: SendTxCallback): PromiEvent<TransactionReceipt>
+  send(tx?: SendTxOptions, callback?: SendTxCallback): PromiEvent<TransactionReceipt>
+
+  estimateGas(callback: EstimateGasTxCallback): Promise<number>
+  estimateGas(tx?: EstimateGasTxOptions, callback?: EstimateGasTxCallback): Promise<number>
+
+  encodeABI(): string
+}
+
+export interface ERC20 extends Contract {
+  clone(): ERC20
+
+  methods: {
+    totalSupply(): TransactionObject<string>
+    decimals(): TransactionObject<string>
+    symbol(): TransactionObject<string>
+    name(): TransactionObject<string>
+
+    balanceOf(owner: string): TransactionObject<string, [string]>
+
+    allowance(owner: string, spender: string): TransactionObject<string, [string, string]>
+
+    approve(spender: string, value: number | string | BN): TransactionObject<boolean, [string, string | number | BN]>
+
+    transfer(to: string, value: number | string | BN): TransactionObject<boolean, [string, string | number | BN]>
+
+    transferFrom(
+      from: string,
+      to: string,
+      value: number | string | BN,
+    ): TransactionObject<boolean, [string, string, string | number | BN]>
+  }
 }
