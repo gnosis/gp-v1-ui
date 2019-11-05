ac3572918ff9836193a96c116122a80c3a4b3676
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-11-05 15:44:05 -0800

    Implemented ExchangeApiMock with unit tests

--- src/api/exchange/ExchangeApiMock.ts
+++ src/api/exchange/ExchangeApiMock.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import BN from 'bn.js'
+import assert from 'assert'
 
 import { DepositApiMock, BalancesByUserAndToken } from './DepositApiMock'
 import { ExchangeApi, Order, PlaceOrderParams, Erc20Api } from 'types'
-import { BATCH_TIME } from 'const'
+import { FEE_DENOMINATOR } from 'const'
 
 export interface OrdersByUser {
   [userAddress: string]: Order[]
@@ -13,31 +14,35 @@ export interface OrdersByUser {
  * Basic implementation of Stable Coin Converter API
  */
 export class ExchangeApiMock extends DepositApiMock implements ExchangeApi {
-  private _registeredTokens: string[]
-  private _orders: OrdersByUser
+  private registeredTokens: string[]
+  private tokenAddressToId: { [address in string]: number } // reverse mapping of registeredTokens for faster access
+  private maxTokens: number
+  private orders: OrdersByUser
 
   public constructor(
     balanceStates: BalancesByUserAndToken,
     erc20Api: Erc20Api,
     registeredTokens: string[] = [],
+    maxTokens: number = 10000,
     ordersByUser: OrdersByUser = {},
   ) {
     super(balanceStates, erc20Api)
-    this._registeredTokens = registeredTokens
-    this._orders = ordersByUser
+    this.registeredTokens = registeredTokens
+    this.tokenAddressToId = registeredTokens.reduce((obj, address, index) => {
+      obj[address] = index
+      return obj
+    }, {})
+    this.maxTokens = maxTokens
+    this.orders = ordersByUser
   }
 
-  public async getBatchTime(): Promise<number> {
-    return BATCH_TIME
-  }
-
-  public async getOrders(_userAddress: string): Promise<Order[]> {
-    this._initOrders(_userAddress)
-    throw new Error('Not implemented yet')
+  public async getOrders(userAddress: string): Promise<Order[]> {
+    this._initOrders(userAddress)
+    return this.orders[userAddress]
   }
 
   public async getNumTokens(): Promise<number> {
-    return this._registeredTokens.length
+    return this.registeredTokens.length
   }
 
   /**
@@ -45,39 +50,57 @@ export class ExchangeApiMock extends DepositApiMock implements ExchangeApi {
    * i.e. 1/1000 = 0.1%
    */
   public async getFeeDenominator(): Promise<number> {
-    throw new Error('Not implemented yet')
+    return FEE_DENOMINATOR
   }
 
-  public async addToken(_tokenAddress: string): Promise<void> {
-    throw new Error('Not implemented yet')
+  public async getTokenAddressById(tokenId: number): Promise<string> {
+    assert(typeof this.registeredTokens[tokenId] === 'string', 'Must have ID to get Address')
+    return this.registeredTokens[tokenId]
   }
 
-  public async placeOrder(_orderParams: PlaceOrderParams): Promise<number> {
-    throw new Error('Not implemented yet')
-  }
-
-  public async cancelOrder(_senderAddress: string, _orderId: number): Promise<void> {
-    this._initOrders
-    throw new Error('Not implemented yet')
-  }
-
-  public async getTokenAddressById(_tokenId: number): Promise<string> {
-    throw new Error('Not implemented yet')
-  }
-
-  public async getTokenIdByAddress(_tokenAddress: string): Promise<number> {
-    throw new Error('Not implemented yet')
+  public async getTokenIdByAddress(tokenAddress: string): Promise<number> {
+    assert(typeof this.tokenAddressToId[tokenAddress] === 'number', 'Must have Address to get ID')
+    return this.tokenAddressToId[tokenAddress]
   }
 
   public async getCurrentPrice(_tokenId: number): Promise<BN> {
     throw new Error('Not implemented yet')
   }
 
+  public async addToken(tokenAddress: string): Promise<void> {
+    assert(typeof this.tokenAddressToId[tokenAddress] !== 'number', 'Token already registered')
+    assert(this.registeredTokens.length < this.maxTokens, 'Max tokens reached')
+    this.registeredTokens.push(tokenAddress)
+    this.tokenAddressToId[tokenAddress] = this.registeredTokens.length - 1
+  }
+
+  public async placeOrder(orderParams: PlaceOrderParams): Promise<number> {
+    this._initOrders(orderParams.userAddress)
+
+    this.orders[orderParams.userAddress].push({
+      buyTokenId: orderParams.buyTokenId,
+      sellTokenId: orderParams.sellTokenId,
+      validFrom: await this.getCurrentBatchId(),
+      validUntil: orderParams.validUntil,
+      priceNumerator: orderParams.buyAmount,
+      priceDenominator: orderParams.sellAmount,
+      remainingAmount: orderParams.sellAmount,
+    })
+    return this.orders[orderParams.userAddress].length - 1
+  }
+
+  public async cancelOrder(senderAddress: string, orderId: number): Promise<void> {
+    this._initOrders(senderAddress)
+    if (this.orders[senderAddress][orderId]) {
+      this.orders[senderAddress][orderId].validUntil = (await this.getCurrentBatchId()) - 1
+    }
+  }
+
   /********************************    private methods   ********************************/
   private _initOrders(userAddress: string): void {
-    const userOrders = this._orders[userAddress]
+    const userOrders = this.orders[userAddress]
     if (!userOrders) {
-      this._orders[userAddress] = []
+      this.orders[userAddress] = []
     }
   }
 }
--- test/api/ExchangeApi/ExchangeApiMock.test.ts
+++ test/api/ExchangeApi/ExchangeApiMock.test.ts
@@ -1,15 +1,16 @@
 import ExchangeApiMock from 'api/exchange/ExchangeApiMock'
 import Erc20ApiMock from 'api/erc20/Erc20ApiMock'
-import { BATCH_TIME, ZERO, ONE } from 'const'
+import { ZERO, ONE, FEE_DENOMINATOR } from 'const'
 import { ExchangeApi, Erc20Api } from 'types'
 import * as testHelpers from '../../testHelpers'
 
-const { USER_1, FEE_TOKEN, TOKEN_1, TOKEN_2, TOKEN_4, TOKEN_5, TOKEN_6, AMOUNT, AMOUNT_SMALL } = testHelpers
+const { USER_1, USER_2, USER_3, FEE_TOKEN, TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, BATCH_ID } = testHelpers
 
 jest.mock('api/erc20/Erc20ApiMock')
 
 let instance: ExchangeApi
 let mockErc20Api: Erc20Api
+const tokens = [FEE_TOKEN, TOKEN_1, TOKEN_2]
 
 beforeAll(() => {
   testHelpers.mockTimes()
@@ -17,57 +18,165 @@ beforeAll(() => {
 
 beforeEach(() => {
   mockErc20Api = new Erc20ApiMock()
-  const tokens = [FEE_TOKEN, TOKEN_1, TOKEN_2]
-  instance = new ExchangeApiMock(
-    {
-      [USER_1]: {
-        [TOKEN_1]: {
-          balance: ZERO,
-          pendingDeposits: { amount: ZERO, batchId: 0 },
-          pendingWithdraws: { amount: ZERO, batchId: 0 },
-        },
-        [TOKEN_2]: {
-          balance: AMOUNT,
-          pendingDeposits: { amount: ZERO, batchId: 0 },
-          pendingWithdraws: { amount: ZERO, batchId: 0 },
-        },
-        [TOKEN_4]: {
-          balance: ZERO,
-          pendingDeposits: { amount: AMOUNT, batchId: 1 },
-          pendingWithdraws: { amount: AMOUNT, batchId: 1 },
-        },
-        [TOKEN_5]: {
-          balance: AMOUNT,
-          pendingDeposits: { amount: AMOUNT, batchId: testHelpers.BATCH_ID },
-          pendingWithdraws: { amount: AMOUNT, batchId: testHelpers.BATCH_ID },
-        },
-        [TOKEN_6]: {
-          balance: AMOUNT,
-          pendingDeposits: { amount: AMOUNT_SMALL, batchId: 1 },
-          pendingWithdraws: { amount: AMOUNT_SMALL, batchId: 1 },
-        },
+  instance = new ExchangeApiMock({}, mockErc20Api, tokens, 4, {
+    [USER_1]: [
+      {
+        buyTokenId: 1,
+        sellTokenId: 2,
+        validFrom: 0,
+        validUntil: 6,
+        priceNumerator: ONE,
+        priceDenominator: ZERO,
+        remainingAmount: ONE,
       },
-    },
-    mockErc20Api,
-    tokens,
-    {
-      [USER_1]: [
-        {
-          buyTokenId: 1,
-          sellTokenId: 2,
-          validFrom: 0,
-          validUntil: 6,
-          priceNumerator: ONE,
-          priceDenominator: ZERO,
-          remainingAmount: ONE,
-        },
-      ],
-    },
-  )
+    ],
+  })
 })
 
 describe('Basic view functions', () => {
-  test('batch time', async () => {
-    expect(await instance.getBatchTime()).toBe(BATCH_TIME)
+  test('fee denominator', async () => {
+    expect(await instance.getFeeDenominator()).toBe(FEE_DENOMINATOR)
+  })
+
+  test('num tokens', async () => {
+    expect(await instance.getNumTokens()).toBe(tokens.length)
+  })
+
+  describe('get orders', () => {
+    it('returns empty when no orders placed by user', async () => {
+      expect(await instance.getOrders(USER_2)).toHaveLength(0)
+    })
+
+    it('returns all placed orders', async () => {
+      expect(await instance.getOrders(USER_1)).toHaveLength(1)
+    })
+  })
+})
+
+describe('Token view methods', () => {
+  describe('get token id by address', () => {
+    it('returns correct id when found', async () => {
+      expect(await instance.getTokenIdByAddress(tokens[1])).toBe(1)
+    })
+
+    it('throws when id not found', async () => {
+      try {
+        await instance.getTokenIdByAddress(TOKEN_4)
+        fail('Should not reach')
+      } catch (e) {
+        expect(e.message).toMatch(/^Must have Address to get ID$/)
+      }
+    })
+  })
+
+  describe('get token address by id', () => {
+    it('returns correct address when found', async () => {
+      expect(await instance.getTokenAddressById(0)).toBe(tokens[0])
+    })
+
+    it('throws when address not found', async () => {
+      try {
+        await instance.getTokenAddressById(10)
+        fail('Should not reach')
+      } catch (e) {
+        expect(e.message).toMatch(/^Must have ID to get Address$/)
+      }
+    })
+  })
+})
+
+describe('addToken', () => {
+  it('adds token not registered', async () => {
+    const tokenCount = await instance.getNumTokens()
+
+    await instance.addToken(TOKEN_3)
+
+    expect(await instance.getNumTokens()).toBe(tokenCount + 1)
+    expect(await instance.getTokenIdByAddress(TOKEN_3)).toBe(tokenCount)
+    expect(await instance.getTokenAddressById(tokenCount)).toBe(TOKEN_3)
+  })
+
+  it('throws when token already registered', async () => {
+    try {
+      await instance.addToken(tokens[0])
+      fail('Should not reach')
+    } catch (e) {
+      expect(e.message).toMatch(/^Token already registered$/)
+    }
+  })
+  it('throws when MAX_TOKENS reached', async () => {
+    try {
+      await instance.addToken(TOKEN_4)
+      fail('Should not reach')
+    } catch (e) {
+      expect(e.message).toMatch(/^Max tokens reached$/)
+    }
+  })
+})
+
+describe('placeOrder', () => {
+  const expected = {
+    buyTokenId: 1,
+    sellTokenId: 2,
+    validFrom: BATCH_ID,
+    validUntil: BATCH_ID + 6,
+    priceNumerator: ONE,
+    priceDenominator: ONE,
+    remainingAmount: ONE,
+  }
+
+  const params = {
+    userAddress: '',
+    buyTokenId: expected.buyTokenId,
+    sellTokenId: expected.sellTokenId,
+    validUntil: expected.validUntil,
+    buyAmount: expected.priceNumerator,
+    sellAmount: expected.priceDenominator,
+  }
+
+  test('place order not first', async () => {
+    const ordersCount = (await instance.getOrders(USER_1)).length
+
+    params.userAddress = USER_1
+    expect(await instance.placeOrder(params)).toBe(ordersCount)
+    const actual = (await instance.getOrders(USER_1)).pop()
+    expect(actual).toEqual(expected)
+  })
+
+  test('place first order', async () => {
+    expect((await instance.getOrders(USER_3)).length).toBe(0)
+    params.userAddress = USER_2
+
+    expect(await instance.placeOrder(params)).toBe(0)
+    const actual = (await instance.getOrders(USER_2)).pop()
+    expect(actual).toEqual(expected)
+  })
+})
+describe('cancelOrder', () => {
+  test('cancel existing order', async () => {
+    const orderId = (await instance.getOrders(USER_1)).length - 1
+
+    await instance.cancelOrder(USER_1, orderId)
+
+    const actual = (await instance.getOrders(USER_1))[orderId]
+    expect(actual.validUntil).toBe(BATCH_ID - 1)
+  })
+
+  test('cancel non existing order does nothing', async () => {
+    const expected = await instance.getOrders(USER_1)
+
+    await instance.cancelOrder(USER_1, expected.length + 1)
+
+    const actual = await instance.getOrders(USER_1)
+    expect(actual).toEqual(expected)
+  })
+
+  test('cancel non existing order, user with no orders does nothing', async () => {
+    const expected = await instance.getOrders(USER_2)
+
+    await instance.cancelOrder(USER_2, expected.length + 1)
+
+    const actual = await instance.getOrders(USER_2)
+    expect(actual).toEqual(expected)
   })
 })
