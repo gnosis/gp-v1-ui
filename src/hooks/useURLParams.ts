64936b858126de52dde5b31b61b15e94e14e1ff1
David Sato <david.sato64@gmail.com>
2019-11-06 16:26:22 +0100

    simplified useURLParams
    1. removed code already available in react-router

--- src/hooks/useURLParams.ts
+++ src/hooks/useURLParams.ts
@@ -1,26 +1,12 @@
-import { useState, useMemo } from 'react'
+import { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
-function setURLFilterParams(filterString: string, moveNow?: boolean): string | void | boolean {
-  if (!window || !window.history) return false
-  const defaultLocation = window.location.href.split('?')[0]
-  if (!filterString) return null
-  return moveNow
-    ? (window.location.href = `${defaultLocation}${filterString}`)
-    : window.history.replaceState(null, null, `?${filterString}`)
+function useURLParams(newParams?: string): void {
+  const history = useHistory()
+
+  useEffect(() => {
+    history.push(newParams)
+  }, [history, newParams])
 }
 
-export function useLocation(): URLSearchParams {
-  const [search, setSearch] = useState(location.search)
-
-  useMemo(() => {
-    setSearch(search)
-  }, [search])
-
-  return new URLSearchParams(search)
-}
-
-export default function useURLParams(newParams: string): void {
-  useMemo(() => {
-    setURLFilterParams(newParams)
-  }, [newParams])
-}
+export default useURLParams
