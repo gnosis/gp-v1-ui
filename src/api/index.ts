84ba41f33fdb1586331a7504f4d151a1bc620577
Anxo Rodriguez <anxolin@gmail.com>
2019-11-08 19:05:23 +0100

    30/stable coin api  part2 (#36)
    * Add interface definition for the exchange
    
    * Add mock implementation, all methods throw exception for now
    
    * Add Order and AddOrderParams types
    
    * Moved to current folder
    
    * Updated interface to match contract
    
    * Added senderAddress parameter to cancelOrder method
    
    * Added first basic test to ExchangeApiMock
    
    * Implemented ExchangeApiMock with unit tests
    
    * Creating ExchangeApi mock instance
    
    * Removed getCurrentPrice method, currently not implemented
    
    * Accepting txOptionalParams and returning receipt on ExchangeApi mutating
    calls

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
-import { Network, TokenList, WalletApi, DepositApi, Erc20Api } from 'types'
+import { Network, TokenList, WalletApi, DepositApi, Erc20Api, ExchangeApi } from 'types'
 import { WalletApiMock } from './wallet/WalletApiMock'
 import { TokenListApiImpl } from './tokenList/TokenListApiImpl'
 import { TokenListApiMock } from './tokenList/TokenListApiMock'
 import { Erc20ApiMock } from './erc20/Erc20ApiMock'
 import { DepositApiMock } from './exchange/DepositApiMock'
-import { tokenList, exchangeBalanceStates, erc20Balances, erc20Allowances } from '../../test/data'
+import { ExchangeApiMock } from './exchange/ExchangeApiMock'
+import { tokenList, exchangeBalanceStates, erc20Balances, erc20Allowances, FEE_TOKEN } from '../../test/data'
 
 const isMock = process.env.MOCK === 'true'
 
@@ -55,8 +56,21 @@ function createDepositApi(erc20Api: Erc20Api): DepositApi {
   return depositApi
 }
 
+function createExchangeApi(erc20Api: Erc20Api): ExchangeApi {
+  if (isMock) {
+    const tokens = [FEE_TOKEN, ...tokenList.map(token => token.address)]
+    const exchangeApi = new ExchangeApiMock(exchangeBalanceStates, erc20Api, tokens)
+    window['exchangeApi'] = exchangeApi
+    return exchangeApi
+  } else {
+    // TODO: Add actual implementation
+    throw new Error('Not implemented yet. Only mock implementation available')
+  }
+}
+
 // Build APIs
 export const walletApi: WalletApi = createWalletApi()
 export const tokenListApi: TokenList = createTokenListApi()
 export const erc20Api: Erc20Api = createErc20Api()
 export const depositApi: DepositApi = createDepositApi(erc20Api)
+export const exchangeApi: ExchangeApi = createExchangeApi(erc20Api)
--- src/types/index.ts
+++ src/types/index.ts
@@ -88,6 +88,36 @@ export interface DepositApi {
   withdraw(userAddress: string, tokenAddress: string, txOptionalParams?: TxOptionalParams): Promise<TxResult<void>>
 }
 
+export interface Order {
+  buyTokenId: number
+  sellTokenId: number
+  validFrom: number
+  validUntil: number
+  priceNumerator: BN
+  priceDenominator: BN
+  remainingAmount: BN
+}
+
+export interface PlaceOrderParams {
+  userAddress: string
+  buyTokenId: number
+  sellTokenId: number
+  validUntil: number
+  buyAmount: BN
+  sellAmount: BN
+}
+
+export interface ExchangeApi extends DepositApi {
+  getOrders(userAddress: string): Promise<Order[]>
+  getNumTokens(): Promise<number>
+  getFeeDenominator(): Promise<number>
+  getTokenAddressById(tokenId: number): Promise<string> // tokenAddressToIdMap
+  getTokenIdByAddress(tokenAddress: string): Promise<number>
+  addToken(tokenAddress: string, txOptionalParams?: TxOptionalParams): Promise<TxResult<void>>
+  placeOrder(orderParams: PlaceOrderParams, txOptionalParams?: TxOptionalParams): Promise<TxResult<number>>
+  cancelOrder(senderAddress: string, orderId: number, txOptionalParams?: TxOptionalParams): Promise<TxResult<void>>
+}
+
 export interface WalletInfo {
   isConnected: boolean
   userAddress?: string
