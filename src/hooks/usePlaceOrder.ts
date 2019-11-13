8ed5e26d1b5912f64049f7ce68ea50b8197553e2
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-11-13 12:07:29 -0800

    Memoizing place order function based on userAddress

--- src/hooks/usePlaceOrder.ts
+++ src/hooks/usePlaceOrder.ts
@@ -2,7 +2,7 @@ import BN from 'bn.js'
 import { toast } from 'react-toastify'
 
 import { TokenDetails, PlaceOrderParams as ExchangeApiPlaceOrderParams } from 'types'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { exchangeApi } from 'api'
 import { log } from 'utils'
 import { txOptionalParams } from 'utils/transaction'
@@ -29,48 +29,52 @@ export const usePlaceOrder = ({ callback }: Params): Result => {
   const [isSubmitting, setIsSubmitting] = useState(false)
   const { userAddress } = useWalletConnection()
 
-  async function placeOrder({ buyAmount, buyToken, sellAmount, sellToken }: PlaceOrderParams): Promise<void> {
-    if (!userAddress) {
-      toast.error('Wallet is not connected!')
-      return
-    }
-
-    setIsSubmitting(true)
-    log(
-      `Placing order: buy ${buyAmount.toString()} ${buyToken.symbol} sell ${sellAmount.toString()} ${sellToken.symbol}`,
-    )
-
-    try {
-      const [sellTokenId, buyTokenId, batchId] = await Promise.all([
-        exchangeApi.getTokenIdByAddress(sellToken.address),
-        exchangeApi.getTokenIdByAddress(buyToken.address),
-        exchangeApi.getCurrentBatchId(),
-      ])
-
-      const validUntil = batchId + DEFAULT_ORDER_DURATION
-
-      const params: ExchangeApiPlaceOrderParams = {
-        userAddress,
-        buyTokenId,
-        sellTokenId,
-        validUntil,
-        buyAmount,
-        sellAmount,
+  const placeOrder = useMemo((): ((params: PlaceOrderParams) => Promise<void>) => {
+    return async ({ buyAmount, buyToken, sellAmount, sellToken }: PlaceOrderParams): Promise<void> => {
+      if (!userAddress) {
+        toast.error('Wallet is not connected!')
+        return
       }
-      const result = await exchangeApi.placeOrder(params, txOptionalParams)
-      log(`The transaction has been mined: ${result.receipt.transactionHash}`)
 
-      toast.success(`Placed order id=${result.data} valid for 30min`)
+      setIsSubmitting(true)
+      log(
+        `Placing order: buy ${buyAmount.toString()} ${buyToken.symbol} sell ${sellAmount.toString()} ${
+          sellToken.symbol
+        }`,
+      )
 
-      callback()
-    } catch (e) {
-      log(`Error placing order`, e)
-      toast.error(`Error placing order: ${e.message}`)
-    } finally {
-      //TODO: use mounted hook thingy when available
-      setIsSubmitting(false)
+      try {
+        const [sellTokenId, buyTokenId, batchId] = await Promise.all([
+          exchangeApi.getTokenIdByAddress(sellToken.address),
+          exchangeApi.getTokenIdByAddress(buyToken.address),
+          exchangeApi.getCurrentBatchId(),
+        ])
+
+        const validUntil = batchId + DEFAULT_ORDER_DURATION
+
+        const params: ExchangeApiPlaceOrderParams = {
+          userAddress,
+          buyTokenId,
+          sellTokenId,
+          validUntil,
+          buyAmount,
+          sellAmount,
+        }
+        const result = await exchangeApi.placeOrder(params, txOptionalParams)
+        log(`The transaction has been mined: ${result.receipt.transactionHash}`)
+
+        toast.success(`Placed order id=${result.data} valid for 30min`)
+
+        callback()
+      } catch (e) {
+        log(`Error placing order`, e)
+        toast.error(`Error placing order: ${e.message}`)
+      } finally {
+        //TODO: use mounted hook thingy when available
+        setIsSubmitting(false)
+      }
     }
-  }
+  }, [callback, userAddress])
 
   return { placeOrder, isSubmitting }
 }
