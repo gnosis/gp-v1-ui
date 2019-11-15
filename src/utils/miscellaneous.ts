cce915f1e6ce7fe18d75ff1816fe46faa4e20e2d
David Sato <david.sato64@gmail.com>
2019-11-15 16:06:54 +0100

    .toString is string method, check for this

--- src/utils/miscellaneous.ts
+++ src/utils/miscellaneous.ts
@@ -5,16 +5,16 @@ function noop(..._args: any[]): void {}
 
 export const log = process.env.NODE_ENV === 'test' ? noop : console.log
 
-export function getToken<T extends TokenDetails>(
-  key: string,
+export function getToken<T extends TokenDetails, K extends keyof T>(
+  key: K,
   value: string | undefined = '',
   tokens: T[] | undefined | null,
 ): T | undefined {
   const valueUppercase = value.toUpperCase()
   return (tokens || []).find((token: T): boolean => {
-    const value = token[key]
-    if (value) {
-      return value.toString().toUpperCase() === valueUppercase
+    const tokenKeyValue = token[key]
+    if (tokenKeyValue) {
+      return typeof tokenKeyValue === 'string' && tokenKeyValue.toString().toUpperCase() === valueUppercase
     } else {
       return false
     }
