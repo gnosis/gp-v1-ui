90047aebd934cec232103ec27b96d03d698a2158
Velenir <velenir@gmail.com>
2019-11-18 15:15:17 +0300

    change onSentTransaction usage

--- src/utils/transaction.tsx
+++ src/utils/transaction.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { TxOptionalParams, Receipt } from 'types'
+import { TxOptionalParams } from 'types'
 import { TxNotification } from 'components/TxNotification'
 import { toast } from 'react-toastify'
 
 export const txOptionalParams: TxOptionalParams = {
-  onSentTransaction: (receipt: Receipt): void => {
-    if (receipt.transactionHash) {
-      toast.info(<TxNotification txHash={receipt.transactionHash} />)
+  onSentTransaction: (transactionHash: string): void => {
+    if (transactionHash) {
+      toast.info(<TxNotification txHash={transactionHash} />)
     } else {
-      console.error(`Failed to get notification for tx ${receipt.transactionHash}`)
+      console.error(`Failed to get notification for tx ${transactionHash}`)
     }
   },
 }
