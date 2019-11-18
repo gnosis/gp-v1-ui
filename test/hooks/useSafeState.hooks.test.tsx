d34289308ad844dd5d446ea68793f3800ec5ec20
David Sato <david.sato64@gmail.com>
2019-11-18 09:59:16 +0100

    test pt 1

--- test/hooks/useSafeState.hooks.test.tsx
+++ test/hooks/useSafeState.hooks.test.tsx
@@ -48,7 +48,7 @@ afterEach(() => {
 })
 
 describe('Tests button click state change', () => {
-  it('Renders and SafeState works to change state', async () => {
+  it('Renders and useSafeState works to change state', async () => {
     act(() => {
       ReactDOM.render(<TestComponent safeUpdate />, container)
     })
@@ -65,50 +65,5 @@ describe('Tests button click state change', () => {
     await act(() => new Promise((accept): NodeJS.Timeout => setTimeout((): void => accept('done'), 300)))
 
     expect(h1.textContent).toBe('SAFE')
-  })
-
-  xit('useSafeState on unmount is NOT called, thus not changing state', async () => {
-    act(() => {
-      ReactDOM.render(<TestComponent safeUpdate />, container)
-    })
-
-    const button = container.querySelector('#buttonTest')
-    const h1 = container.querySelector('h1')
-
-    // click button
-    act(() => {
-      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-      // immediately unmount component
-      ReactDOM.unmountComponentAtNode(container)
-    })
-
-    // await promise time
-    await act(() => new Promise((accept): NodeJS.Timeout => setTimeout((): void => accept('done'), 300)))
-
-    // Should be no state change
-    expect(h1.textContent).toBe('')
-  })
-
-  xit('normal useState on unmount IS called, thus changing state', async () => {
-    act(() => {
-      // mount normal state changing component
-      ReactDOM.render(<TestComponent />, container)
-    })
-
-    const button = container.querySelector('#buttonTest')
-    const h1 = container.querySelector('h1')
-
-    // click button
-    act(() => {
-      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-      // immediately unmount component
-      ReactDOM.unmountComponentAtNode(container)
-    })
-
-    // await promise time
-    await act(() => new Promise((accept): NodeJS.Timeout => setTimeout((): void => accept('done'), 300)))
-
-    // Should be no state change
-    expect(h1.textContent).toBe('UNSAFE')
   })
 })
