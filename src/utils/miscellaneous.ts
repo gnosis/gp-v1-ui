f5555cdaa0a850357169d670f413dc9bd5c34fc1
David Sato <david.sato64@gmail.com>
2019-11-21 10:51:49 +0100

    simplified assert

--- src/utils/miscellaneous.ts
+++ src/utils/miscellaneous.ts
@@ -2,7 +2,7 @@ import { TokenDetails } from 'types'
 import { AssertionError } from 'assert'
 
 export function assert<T>(val: T, message: string): asserts val is NonNullable<T> {
-  if (!val || val === undefined || val === null) {
+  if (!val) {
     throw new AssertionError({ message })
   }
 }
