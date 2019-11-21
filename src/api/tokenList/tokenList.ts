28919022b35e4de0b7b72ee0c97760fc675f19a1
David Sato <david.sato64@gmail.com>
2019-11-21 11:23:14 +0100

    tokenList getTokensByNetwork acc.push

--- src/api/tokenList/tokenList.ts
+++ src/api/tokenList/tokenList.ts
@@ -5,14 +5,15 @@ import tokens from './tokenList.json'
 
 export function getTokensByNetwork(networkId: number): TokenDetails[] {
   // Return token details
-  const tokenDetails: TokenDetails[] = tokens.reduce((acc, token) => {
+  const tokenDetails: TokenDetails[] = tokens.reduce((acc: TokenDetails[], token) => {
     const address = token.addressByNetwork[networkId]
     if (address) {
       // There's an address for the current network
       const { name, symbol, decimals = DEFAULT_PRECISION } = token
       const addressMainnet = token.addressByNetwork[Network.Mainnet]
 
-      return [...acc, { name, symbol, decimals, address, addressMainnet }]
+      acc.push({ name, symbol, decimals, address, addressMainnet })
+      return acc
     }
 
     return acc
