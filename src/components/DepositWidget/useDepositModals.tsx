03cde862a2674181ca4b2b98ea47fdb5cf7cdded
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-11-26 13:01:51 -0800

    Further refactoring buttons on useDepositModals

--- src/components/DepositWidget/useDepositModals.tsx
+++ src/components/DepositWidget/useDepositModals.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import BN from 'bn.js'
 
 import { ModalBodyWrapper } from './Styled'
@@ -46,42 +46,39 @@ interface Result {
 export function useDepositModals(params: Params): Result {
   const { amount, tokenAddress, requestWithdraw } = params
 
+  const getButtons = useCallback(
+    (toggleModal: toggleModaliComponent): React.ReactNode[] => {
+      return [
+        <Modali.Button label="Cancel" key="no" isStyleCancel onClick={toggleModal} />,
+        <Modali.Button
+          label="Continue"
+          key="yes"
+          isStyleDefault
+          onClick={async (): Promise<void> => {
+            // On confirm, do the request
+            toggleModal()
+            await requestWithdraw(amount, tokenAddress)
+          }}
+        />,
+      ]
+    },
+    [amount, requestWithdraw, tokenAddress],
+  )
+
   const [withdrawOverwriteModal, toggleWithdrawOverwriteModal] = useModali({
     ...defaultOptions,
     title: 'Confirm withdraw overwrite',
     message: <OverwriteModalBody />,
-    buttons: [
-      <Modali.Button label="Cancel" key="no" isStyleCancel onClick={(): void => toggleWithdrawOverwriteModal()} />,
-      <Modali.Button
-        label="Continue"
-        key="yes"
-        isStyleDefault
-        onClick={async (): Promise<void> => {
-          // On confirm, do the request
-          toggleWithdrawOverwriteModal()
-          await requestWithdraw(amount, tokenAddress)
-        }}
-      />,
-    ],
+    buttons: getButtons((): void => {
+      toggleWithdrawOverwriteModal()
+    }),
   })
 
   const [withdrawAndClaimModal, toggleWithdrawAndClaimModal] = useModali({
     ...defaultOptions,
     title: 'Please note',
     message: <WithdrawAndClaimModalBody />,
-    buttons: [
-      <Modali.Button label="Cancel" key="no" isStyleCancel onClick={(): void => toggleWithdrawAndClaimModal()} />,
-      <Modali.Button
-        label="Continue"
-        key="yes"
-        isStyleDefault
-        onClick={async (): Promise<void> => {
-          // On confirm, do the request
-          toggleWithdrawAndClaimModal()
-          await requestWithdraw(amount, tokenAddress)
-        }}
-      />,
-    ],
+    buttons: getButtons((): void => toggleWithdrawAndClaimModal()),
   })
 
   return { withdrawOverwriteModal, toggleWithdrawOverwriteModal, withdrawAndClaimModal, toggleWithdrawAndClaimModal }
