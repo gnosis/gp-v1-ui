780fb94d630b53137e485ec554eba3a21c0d9297
David Sato <david.sato64@gmail.com>
2019-11-29 14:52:00 +0100

    useGlobalState: better types

--- src/hooks/useGlobalState.tsx
+++ src/hooks/useGlobalState.tsx
@@ -1,22 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { useContext, useReducer } from 'react'
-import { GlobalState } from 'reducers-actions'
+import { GlobalState, TokenLocalState } from 'reducers-actions'
 
 const GlobalStateContext = React.createContext({})
 
-export const withGlobalContext = (
-  WrappedComponent: React.FC<{}> | any,
-  initialStateFunc: { (): { enabling: Set<string>; highlighted: Set<string>; claiming: Set<string> }; (): unknown },
+export function withGlobalContext<P>(
+  WrappedComponent: React.FC<P>,
+  initialStateFunc: { (): { TokenRow: TokenLocalState }; (): Partial<GlobalState> },
   reducer: React.Reducer<Partial<GlobalState>, { type: string; payload: unknown }>,
-) => (props: any) => {
-  const [state, dispatch] = useReducer(reducer, initialStateFunc())
+): (props: P) => JSX.Element {
+  return function WrappedComponentWithGlobalState(props: P): JSX.Element {
+    const [state, dispatch] = useReducer(reducer, initialStateFunc())
 
-  return (
-    <GlobalStateContext.Provider value={[state, dispatch]}>
-      <WrappedComponent {...props} />
-    </GlobalStateContext.Provider>
-  )
+    return (
+      <GlobalStateContext.Provider value={[state, dispatch]}>
+        <WrappedComponent {...props} />
+      </GlobalStateContext.Provider>
+    )
+  }
 }
 
 const useGlobalState = (): [GlobalState, Function] => useContext(GlobalStateContext) as [GlobalState, Function]
