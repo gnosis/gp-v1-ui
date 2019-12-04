dcc46256b780dc8c220351e05b9a4acaed00c141
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-12-04 14:10:26 -0800

    Displaying no orders layout

--- src/components/OrdersWidget/index.tsx
+++ src/components/OrdersWidget/index.tsx
@@ -32,15 +32,39 @@ const CreateButtons = styled.div`
   justify-content: center;
   align-items: flex-start;
 
+
+  button {
+    // resetting button margins to help with alignment
+    margin: 0;
+  }
+
   .strategy {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
 
-    a,
+  }
+
+  &.noOrders {
+    // when no orders, give some extra space
+    margin-top: 4em;
+
+    // turn buttons into a column
+    flex-direction: column
+    // align everything
+    justify-content: center;
+    align-items: center;
+
     button {
-      margin-left: 0.5em;
+      // make buttons the same width
+      width: 15em;
+    }
+
+    .strategy {
+      // align strategy div as well
+      justify-content: center;
+      align-items:center;
     }
   }
 `
@@ -139,7 +163,12 @@ const OrdersWidget: React.FC = () => {
     <OrdersWrapper>
       <div>
         <h2>Your orders</h2>
-        <CreateButtons>
+        <CreateButtons className={noOrders ? 'noOrders' : ''}>
+          {noOrders && (
+            <p>
+              It appears you haven&apos;t place any order yet. <br /> Create one!
+            </p>
+          )}
           <Link to="/trade">
             <ButtonWithIcon>
               <FontAwesomeIcon icon={faExchangeAlt} /> Trade
@@ -155,74 +184,76 @@ const OrdersWidget: React.FC = () => {
           </div>
         </CreateButtons>
       </div>
-      <OrdersForm>
-        <div className="infoContainer">
-          <div>
-            You have <Highlight>3</Highlight> standing orders
+      {!noOrders && (
+        <OrdersForm>
+          <div className="infoContainer">
+            <div>
+              You have <Highlight>{orders.length}</Highlight> standing orders
+            </div>
+            <div className="warning">
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+              <strong> Low balance</strong>
+            </div>
           </div>
-          <div className="warning">
-            <FontAwesomeIcon icon={faExclamationTriangle} />
-            <strong> Low balance</strong>
-          </div>
-        </div>
-        <form action="submit">
-          <div className="ordersContainer">
-            <div className="headerRow">
-              <div></div> {/* Empty div on purpose */}
-              <div className="checked">
-                <input type="checkbox" />
+          <form action="submit">
+            <div className="ordersContainer">
+              <div className="headerRow">
+                <div></div> {/* Empty div on purpose */}
+                <div className="checked">
+                  <input type="checkbox" />
+                </div>
+                <div className="title">Order details</div>
+                <div className="title">
+                  Unfilled <br /> amount
+                </div>
+                <div className="title">
+                  Available <br />
+                  amount
+                </div>
+                <div className="title">Expires</div>
               </div>
-              <div className="title">Order details</div>
-              <div className="title">
-                Unfilled <br /> amount
-              </div>
-              <div className="title">
-                Available <br />
-                amount
-              </div>
-              <div className="title">Expires</div>
+
+              <OrderRow
+                id="1"
+                sellToken={DAI}
+                buyToken={TUSD}
+                price="1.05"
+                unfilledAmount="1,500"
+                availableAmount="1,000"
+                overBalance
+                expiresOn="In 3 min"
+              />
+              <OrderRow id="543" sellToken={TUSD} buyToken={USDC} price="1.03" availableAmount="1,000" unlimited />
+              <OrderRow
+                id="1287"
+                sellToken={USDC}
+                buyToken={DAI}
+                price="1.50"
+                unfilledAmount="876.849"
+                availableAmount="876.849"
+                expiresOn="In 1 days"
+                pending
+              />
+              <OrderRow
+                id="1257"
+                sellToken={PAX}
+                buyToken={DAI}
+                price="1.10"
+                unfilledAmount="5,876.8429"
+                availableAmount="500"
+                expiresOn="In 2 days"
+              />
             </div>
 
-            <OrderRow
-              id="1"
-              sellToken={DAI}
-              buyToken={TUSD}
-              price="1.05"
-              unfilledAmount="1,500"
-              availableAmount="1,000"
-              overBalance
-              expiresOn="In 3 min"
-            />
-            <OrderRow id="543" sellToken={TUSD} buyToken={USDC} price="1.03" availableAmount="1,000" unlimited />
-            <OrderRow
-              id="1287"
-              sellToken={USDC}
-              buyToken={DAI}
-              price="1.50"
-              unfilledAmount="876.849"
-              availableAmount="876.849"
-              expiresOn="In 1 days"
-              pending
-            />
-            <OrderRow
-              id="1257"
-              sellToken={PAX}
-              buyToken={DAI}
-              price="1.10"
-              unfilledAmount="5,876.8429"
-              availableAmount="500"
-              expiresOn="In 2 days"
-            />
-          </div>
-
-          <div className="deleteContainer">
-            <ButtonWithIcon disabled>
-              <FontAwesomeIcon icon={faTrashAlt} /> Delete orders
-            </ButtonWithIcon>
-            <span>Select first the order(s) you want to delete</span>
-          </div>
-        </form>
-      </OrdersForm>
+            <div className="deleteContainer">
+              <ButtonWithIcon disabled>
+                <FontAwesomeIcon icon={faTrashAlt} /> Delete orders
+              </ButtonWithIcon>
+              <span>Select first the order(s) you want to delete</span>
+            </div>
+          </form>
+        </OrdersForm>
+      )}
     </OrdersWrapper>
   )
 }
