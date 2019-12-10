222277eb2f1280a383be4173f2c20a0b5a2a48cd
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-12-10 10:42:44 -0800

    Updating tokenList on useOrders hook whenever orders are updated

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -1,16 +1,21 @@
 import { AuctionElement } from 'types'
 import { useWalletConnection } from './useWalletConnection'
 import useSafeState from './useSafeState'
-import { exchangeApi } from 'api'
+import { exchangeApi, tokenListApi } from 'api'
 import { useEffect } from 'react'
 
 export function useOrders(): AuctionElement[] {
-  const { userAddress } = useWalletConnection()
+  const { userAddress, networkId } = useWalletConnection()
   const [orders, setOrders] = useSafeState<AuctionElement[]>([])
 
   useEffect(() => {
     userAddress && exchangeApi.getOrders(userAddress).then(setOrders)
   }, [setOrders, userAddress])
+
+  useEffect(() => {
+    // whenever orders or network change, update token list
+    networkId && tokenListApi.updateTokenIdsForNetwork(networkId)
+  }, [networkId, orders])
 
   return orders
 }
