330653863ac209c6627843638f0a2b99aa6c1bec
David Sato <david.sato64@gmail.com>
2019-12-13 12:21:23 +0100

    extrapolated .oage css into styled

--- src/components/Layout/PageWrapper.tsx
+++ src/components/Layout/PageWrapper.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 
-<<<<<<< HEAD
 const PageWrapper = styled.div<{ $bgColor?: string; $boxShadow?: string; $width?: string }>`
   background-color: ${({ $bgColor = 'var(--color-background-pageWrapper)' }): string => $bgColor};
   box-shadow: ${({ $boxShadow = 'var(--box-shadow)' }): string => $boxShadow};
@@ -8,15 +7,6 @@ const PageWrapper = styled.div<{ $bgColor?: string; $boxShadow?: string; $width?
   margin: auto;
   padding: 2rem 10vw 2rem 10vw;
   width: ${({ $width = '95vw' }): string => $width};
-=======
-const PageWrapper = styled.div`
-  background-color: white;
-  box-shadow: 1px 1px #e8e8e8;
-
-  margin: auto;
-  padding: 2rem 10vw 2rem 10vw;
-  width: 95vw;
->>>>>>> .page > PageWrapper
   min-height: 25rem;
 `
 
