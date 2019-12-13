4e075e69778bf5341120ee9ce1628cc6efc1b707
David Sato <david.sato64@gmail.com>
2019-12-13 11:45:07 +0100

    .page > PageWrapper

--- src/components/Layout/PageWrapper.tsx
+++ src/components/Layout/PageWrapper.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+<<<<<<< HEAD
 const PageWrapper = styled.div<{ $bgColor?: string; $boxShadow?: string; $width?: string }>`
   background-color: ${({ $bgColor = 'var(--color-background-pageWrapper)' }): string => $bgColor};
   box-shadow: ${({ $boxShadow = 'var(--box-shadow)' }): string => $boxShadow};
@@ -7,6 +8,15 @@ const PageWrapper = styled.div<{ $bgColor?: string; $boxShadow?: string; $width?
   margin: auto;
   padding: 2rem 10vw 2rem 10vw;
   width: ${({ $width = '95vw' }): string => $width};
+=======
+const PageWrapper = styled.div`
+  background-color: white;
+  box-shadow: 1px 1px #e8e8e8;
+
+  margin: auto;
+  padding: 2rem 10vw 2rem 10vw;
+  width: 95vw;
+>>>>>>> .page > PageWrapper
   min-height: 25rem;
 `
 
