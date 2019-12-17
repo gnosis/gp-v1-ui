f3bb95bb7a0b4add75615f74b4d4d8a7eb44037a
David Sato <david.sato64@gmail.com>
2019-12-17 12:17:26 +0100

    useNavigation + useOpenCloseNav in separate files

--- src/components/Layout/Header/useNavigation.ts
+++ src/components/Layout/Header/useNavigation.ts
@@ -25,20 +25,4 @@ const useNavigation = (initialState: HeaderProps['navigation'], responsive: bool
   return navigationArray
 }
 
-export const useOpenCloseNav = (): {
-  isResponsive: boolean
-  openNav: boolean
-  setOpenNav: (action: boolean) => void
-} => {
-  const [openNav, setOpenNav] = useState(false)
-
-  const { innerWidth } = useWindowSizes()
-  const isResponsive = !!(innerWidth && innerWidth < 720)
-  useMemo(() => {
-    if (openNav && !isResponsive) {
-      setOpenNav(false)
-    }
-  }, [isResponsive, openNav])
-
-  return { isResponsive, openNav, setOpenNav }
-}
+export default useNavigation
