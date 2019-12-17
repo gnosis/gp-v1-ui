d3eab27d287bb369f4f23da4e066f1d6c9fe7032
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-12-17 14:08:23 -0800

    Awaiting on erc20 calls

--- src/api/erc20/Erc20ApiImpl.ts
+++ src/api/erc20/Erc20ApiImpl.ts
@@ -36,19 +36,19 @@ export class Erc20ApiImpl implements Erc20Api {
   public async name({ tokenAddress }: { tokenAddress: string }): Promise<string> {
     const erc20 = this._getERC20AtAddress(tokenAddress)
 
-    return erc20.methods.name().call()
+    return await erc20.methods.name().call()
   }
 
   public async symbol({ tokenAddress }: { tokenAddress: string }): Promise<string> {
     const erc20 = this._getERC20AtAddress(tokenAddress)
 
-    return erc20.methods.symbol().call()
+    return await erc20.methods.symbol().call()
   }
 
   public async decimals({ tokenAddress }: { tokenAddress: string }): Promise<number> {
     const erc20 = this._getERC20AtAddress(tokenAddress)
 
-    const decimals = erc20.methods.decimals().call()
+    const decimals = await erc20.methods.decimals().call()
 
     return Number(decimals)
   }
