eccc59d2cafbafb1609630222d78f89e2248809c
Anxo Rodriguez <anxolin@gmail.com>
2019-12-19 20:59:32 +0100

    Fix error and simplify

--- src/api/exchange/batchExchangeAddresses.ts
+++ src/api/exchange/batchExchangeAddresses.ts
@@ -1,22 +1,18 @@
 import { Network } from 'types'
 import networks from '@gnosis.pm/dex-contracts/networks.json'
 
-const CONTRACTS = ['BatchExchange']
-
-function _getAddress(contractName: string, networkId: number): string {
-  const networkInfo = networks[contractName][networkId]
+function _getAddress(networkId: number): string {
+  const networkInfo = networks['BatchExchange'][networkId]
   return networkInfo ? networkInfo.address : ''
 }
 
-const addressesByNetwork = CONTRACTS.reduce((acc, contractName) => {
-  acc[contractName] = {
-    [Network.Rinkeby]: _getAddress(contractName, Network.Rinkeby),
-    [Network.Mainnet]: _getAddress(contractName, Network.Mainnet),
-  }
+const addressesByNetwork = {
+  [Network.Rinkeby]: _getAddress(Network.Rinkeby),
+  [Network.Mainnet]: _getAddress(Network.Mainnet),
+}
 
-  return acc
-}, {})
-
-export const getAddressForNetwork = (networkId: Network): string | null => addressesByNetwork[networkId] || null
+export const getAddressForNetwork = (networkId: Network): string | null => {
+  return addressesByNetwork[networkId] || null
+}
 
 export default addressesByNetwork
