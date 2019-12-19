27ba868a8c1d6e25a96a8c3264162093df096408
Anxo Rodriguez <anxolin@gmail.com>
2019-12-19 15:57:04 +0100

    Load the contract dynamically

--- src/api/exchange/epochList.ts
+++ src/api/exchange/epochList.ts
@@ -1,11 +1,23 @@
 import { Network } from 'types'
+import networks from '@gnosis.pm/dex-contracts/networks.json'
 
-const Network2Epoch = {
-  // TODO get addresses from env or a centralized place (npm package?)
-  [Network.Rinkeby]: '0x89593E017D4A88c60347257DAfB95384a422da09',
-  // TODO fill in when deployed
-  [Network.Mainnet]: '',
+const CONTRACTS = ['BatchExchange']
+
+function _getAddress(contractName: string, networkId: number): string {
+  const networkInfo = networks[contractName][networkId]
+  return networkInfo ? networkInfo.address : ''
 }
+
+const Network2Epoch = CONTRACTS.reduce((acc, contractName) => {
+  acc[contractName] = {
+    [Network.Rinkeby]: _getAddress(contractName, Network.Rinkeby),
+    [Network.Mainnet]: _getAddress(contractName, Network.Mainnet),
+  }
+
+  return acc
+}, {})
+
+console.log('Network2Epoch', Network2Epoch)
 
 export const getEpochAddressForNetwork = (networkId: Network): string | null => Network2Epoch[networkId] || null
 
