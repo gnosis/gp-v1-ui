78385380be884c24d7f390ec1fedc1baa0142f8b
Anxo Rodriguez <anxolin@gmail.com>
2019-12-23 13:50:09 +0100

    Show versions

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,6 @@
 declare let VERSION: string
+declare let DEX_JS_VERSION: string
+declare let CONTRACT_VERSION: string
 
 declare module '*.svg' {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
