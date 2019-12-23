6348e8d9b29efb963de1b0bc8ece1a2c252720da
Anxo Rodriguez <anxolin@gmail.com>
2019-12-23 14:11:03 +0100

    Show version (#346)
    * Remove orders link
    
    * Show versions
    
    * Remove version word

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,11 +5,42 @@ const About: React.FC = () => (
   <PageWrapper>
     <h1>About dFusion</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      The dFusion PoC protocol (working title) introduces a new, fully decentralized exchange mechanism for ERC20
+      tokens: batch-auctions with multidimensional order books (in the form of ring trades) with uniform clearing prices
+      in every batch. <br />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://docs.google.com/document/d/1GjDX1_2RsCCxtwnjBgXaV6yXJ2AXYL4-EEYv7HjptaM/edit"
+      >
+        Read more here
+      </a>
     </p>
+    <p>Versions used in this web:</p>
+    <ul>
+      <li>
+        Web:&nbsp;
+        <a target="_blank" rel="noopener noreferrer" href={'https://github.com/gnosis/dex-react/tree/v' + VERSION}>
+          v{VERSION}
+        </a>{' '}
+      </li>
+      <li>
+        Smart Contract:&nbsp;
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={'https://github.com/gnosis/dex-contracts/tree/v' + CONTRACT_VERSION}
+        >
+          v{CONTRACT_VERSION}
+        </a>
+      </li>
+      <li>
+        dex-js library:&nbsp;
+        <a target="_blank" rel="noopener noreferrer" href={'https://github.com/gnosis/dex-js/tree/v' + DEX_JS_VERSION}>
+          v{DEX_JS_VERSION}
+        </a>
+      </li>
+    </ul>
   </PageWrapper>
 )
 
