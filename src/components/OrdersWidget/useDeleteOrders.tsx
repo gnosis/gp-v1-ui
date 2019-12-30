1eeab483f2a44567ea6796aca65add9aa94fd140
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-12-30 08:50:37 -0800

    Memoizing delete function based on userAddress

--- src/components/OrdersWidget/useDeleteOrders.tsx
+++ src/components/OrdersWidget/useDeleteOrders.tsx
@@ -7,6 +7,7 @@ import { exchangeApi } from 'api'
 
 import { log, assert } from 'utils'
 import { txOptionalParams } from 'utils/transaction'
+import { useCallback } from 'react'
 
 interface Result {
   deleteOrders: (orderIds: string[]) => Promise<boolean>
@@ -23,34 +24,37 @@ export function useDeleteOrders(): Result {
   const [deleting, setDeleting] = useSafeState<boolean>(false)
   const { userAddress } = useWalletConnection()
 
-  async function deleteOrders(uiOrderIds: string[]): Promise<boolean> {
-    log(`Trying to cancel the orderIds ${uiOrderIds} for user ${userAddress}`)
+  const deleteOrders = useCallback(
+    async (uiOrderIds: string[]): Promise<boolean> => {
+      log(`Trying to cancel the orderIds ${uiOrderIds} for user ${userAddress}`)
 
-    try {
-      assert(userAddress, 'User address is missing. Aborting.')
-      assert(uiOrderIds.length > 0, 'No orders to cancel. Aborting.')
+      try {
+        assert(userAddress, 'User address is missing. Aborting.')
+        assert(uiOrderIds.length > 0, 'No orders to cancel. Aborting.')
 
-      setDeleting(true)
+        setDeleting(true)
 
-      const orderIds = extractExchangeOrderIds(uiOrderIds)
+        const orderIds = extractExchangeOrderIds(uiOrderIds)
 
-      const receipt = await exchangeApi.cancelOrders({ senderAddress: userAddress, orderIds }, txOptionalParams)
+        const receipt = await exchangeApi.cancelOrders({ senderAddress: userAddress, orderIds }, txOptionalParams)
 
-      log(`The transaction has been mined: ${receipt.transactionHash}`)
+        log(`The transaction has been mined: ${receipt.transactionHash}`)
 
-      toast.success('The selected orders have been cancelled')
+        toast.success('The selected orders have been cancelled')
 
-      return true
-    } catch (e) {
-      console.error(`Failed to cancel orders ${uiOrderIds} for user ${userAddress}`, e)
+        return true
+      } catch (e) {
+        console.error(`Failed to cancel orders ${uiOrderIds} for user ${userAddress}`, e)
 
-      toast.error('Failed to cancel selected orders')
+        toast.error('Failed to cancel selected orders')
 
-      return false
-    } finally {
-      setDeleting(false)
-    }
-  }
+        return false
+      } finally {
+        setDeleting(false)
+      }
+    },
+    [setDeleting, userAddress],
+  )
 
   return { deleteOrders, deleting }
 }
