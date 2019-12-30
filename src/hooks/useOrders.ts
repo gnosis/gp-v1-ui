8f86de03f024e08c42a55cc797ed92dea49a48af
Leandro Boscariol <leandro.boscariol@gnosis.io>
2019-12-30 14:34:03 -0800

    Filtering out deleted orders at hook level

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -4,12 +4,38 @@ import { exchangeApi } from 'api'
 import { useEffect } from 'react'
 import { AuctionElement } from 'api/exchange/ExchangeApi'
 
+/**
+ * Filter out deleted orders.
+ *
+ * When orders are `deleted` from the contract, they are still returned, but with all fields set to zero.
+ * We will not display such orders.
+ *
+ * @param orders all orders returned by the contract
+ */
+function filterDeletedOrders(orders: AuctionElement[]): AuctionElement[] {
+  return orders.filter(
+    order =>
+      !(
+        !order.buyTokenId &&
+        !order.sellTokenId &&
+        !order.priceDenominator &&
+        !order.priceNumerator &&
+        !order.validFrom &&
+        !order.validUntil
+      ),
+  )
+}
+
 export function useOrders(): AuctionElement[] {
   const { userAddress } = useWalletConnection()
   const [orders, setOrders] = useSafeState<AuctionElement[]>([])
 
   useEffect(() => {
-    userAddress && exchangeApi.getOrders(userAddress).then(setOrders)
+    userAddress &&
+      exchangeApi
+        .getOrders(userAddress)
+        .then(filterDeletedOrders)
+        .then(setOrders)
   }, [setOrders, userAddress])
 
   return orders
