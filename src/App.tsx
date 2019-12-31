98f2eef97dd1fe136b3e6bf451702f41fd97f488
Dmitry <Velenir@users.noreply.github.com>
2019-12-31 11:19:38 +0300

    hack for hosting on ipfs (#351)
    * base.href hack for hosting on ipfs
    
    * no need for origin in base.href
    
    * use HashRouter on ipfs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import 'types'
 
 import { hot } from 'react-hot-loader/root'
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, RouteProps, Redirect } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Switch, RouteProps, Redirect } from 'react-router-dom'
 
 // SCSS
 import GlobalStyles from 'styles/global'
@@ -53,6 +53,9 @@ const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
 }
 
 toast.configure({ position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: false })
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Router: typeof BrowserRouter & typeof HashRouter = (window as any).IS_IPFS ? HashRouter : BrowserRouter
 
 // App
 const App: React.FC = () => (
