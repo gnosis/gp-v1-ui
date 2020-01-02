70397319d15f502cc97ce9c03f99385392c96a27
David Sato <david.sato64@gmail.com>
2020-01-02 15:45:07 +0100

    footer - open/close

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import ThemeToggler from 'components/ThemeToggler'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronCircleDown, faChevronCircleUp } from '@fortawesome/free-solid-svg-icons'
 
-const Wrapper = styled.footer`
+const Wrapper = styled.footer<{ $open: boolean }>`
   position: fixed;
   bottom: 0;
   width: 100%;
-  background: var(--color-background);
   z-index: 2000;
+  height: ${({ $open = true }): string => ($open ? 'auto' : '0px')};
 
   display: flex;
   flex-flow: row wrap;
   align-items: center;
   justify-content: center;
-  padding 1.3em;
+  padding: 1.3rem;
   text-align: center;
-  
+
+  background: var(--color-background);
   color: var(--color-text-secondary);
   font-size: 0.85rem;
 
   > div {
     margin: 0.2rem auto;
     flex: 1 1 15rem;
+    opacity: ${({ $open }): string => ($open ? '1' : '0')};
   }
 
   > .footerLinks {
@@ -44,32 +48,49 @@ const Wrapper = styled.footer`
   }
 `
 
-const Footer: React.FC = () => (
-  <Wrapper>
-    <ThemeToggler />
-    <div className="footerLinks">
-      <div>
-        <Link to="/about">About dFusion</Link>
+const FooterToggler = styled.a`
+  position: absolute;
+  right: 1rem;
+  top: 0.73rem;
+  cursor: pointer;
+`
+
+const Footer: React.FC = () => {
+  const [footerOpen, setFooterOpen] = useState(true)
+  return (
+    <Wrapper $open={footerOpen}>
+      {/* FOOTER ACTION HANDLER */}
+      <FooterToggler onClick={(): void => setFooterOpen(!footerOpen)}>
+        <FontAwesomeIcon size="lg" icon={footerOpen ? faChevronCircleDown : faChevronCircleUp} />
+      </FooterToggler>
+      {/* DARK/LIGHT MODE TOGGLER */}
+      <ThemeToggler />
+      {/* LINKS */}
+      <div className="footerLinks">
+        <div>
+          <Link to="/about">About dFusion</Link>
+        </div>
+        <div>
+          <Link to="/source-code">Source code</Link>
+        </div>
       </div>
-      <div>
-        <Link to="/source-code">Source code</Link>
+      {/* VERSION */}
+      <div className="version">
+        Web{' '}
+        <a target="_blank" rel="noopener noreferrer" href={'https://github.com/gnosis/dex-react/tree/v' + VERSION}>
+          v{VERSION}
+        </a>{' '}
+        - Contracts{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={'https://github.com/gnosis/dex-contracts/tree/v' + CONTRACT_VERSION}
+        >
+          v{CONTRACT_VERSION}
+        </a>
       </div>
-    </div>
-    <div className="version">
-      Web{' '}
-      <a target="_blank" rel="noopener noreferrer" href={'https://github.com/gnosis/dex-react/tree/v' + VERSION}>
-        v{VERSION}
-      </a>{' '}
-      - Contracts{' '}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={'https://github.com/gnosis/dex-contracts/tree/v' + CONTRACT_VERSION}
-      >
-        v{CONTRACT_VERSION}
-      </a>
-    </div>
-  </Wrapper>
-)
+    </Wrapper>
+  )
+}
 
 export default Footer
