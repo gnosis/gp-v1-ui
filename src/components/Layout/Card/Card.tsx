dffeb6a3f22aaebe5925d0fb52fb774e63d895e5
David Sato <david.sato64@gmail.com>
2020-01-14 13:06:55 +0100

    DepositWidget.styled changes + Card.tsx
    1. moved core Card css and styled code into Card.ts
    2. changed div styles to new td styles
    ==
    3. Card.tsx: created + placed core card styles
    4. added CardDrawer

--- src/components/Layout/Card/Card.tsx
+++ src/components/Layout/Card/Card.tsx
@@ -2,6 +2,93 @@ import React from 'react'
 import styled from 'styled-components'
 import { RESPONSIVE_SIZES } from 'const'
 
+export const CardRowDrawer = styled.tr<{ responsive: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: transparent;
+  box-shadow: none;
+
+  // Inner td wrapper
+  > td {
+    position: relative;
+    background-color: var(--color-background-selected-dark);
+    border-bottom: 0.125rem solid #0000000f;
+    border-radius: 0 0 var(--border-radius) var(--border-radius);
+    box-shadow: var(--box-shadow);
+    margin-top: -1.52rem;
+    width: 80%;
+
+    @media only screen and (max-width: ${RESPONSIVE_SIZES.TABLET}em) {
+      width: 95%;
+    }
+
+    > div {
+      margin-top: 2rem;
+    }
+
+    span.symbol {
+      color: #b02ace;
+    }
+
+    h4 {
+      margin: 2.5rem 1rem 1rem;
+      font-size: 1.3em;
+      font-weight: normal;
+      text-align: center;
+    }
+
+    .times {
+    }
+  }
+
+  ${(props): string | false =>
+    props.responsive &&
+    `
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background: #000000b5;
+      z-index: 99;
+
+      > td {
+        border-radius: var(--border-radius);
+      }
+  `}
+`
+const CardDrawerCloser = styled.a`
+  position: absolute;
+  display: inline-block;
+  top: 0.5rem;
+  right: 0.5rem;
+  padding: 0 0.5em 0 0;
+  font-size: 2em;
+  text-decoration: none;
+`
+
+interface CardDrawerProps {
+  children: React.ReactNode
+  closeDrawer: () => void
+  responsive: boolean
+}
+
+export const CardDrawer = React.forwardRef<HTMLTableRowElement, CardDrawerProps>(function ReffedCardDrawer(
+  { children, closeDrawer, responsive },
+  ref,
+) {
+  return (
+    <CardRowDrawer ref={ref} responsive={responsive}>
+      <td>
+        <CardDrawerCloser onClick={closeDrawer}>&times;</CardDrawerCloser>
+        {children}
+      </td>
+    </CardRowDrawer>
+  )
+})
+
 export const CardTable = styled.table<{
   $bgColor?: string
 
@@ -22,12 +109,9 @@ export const CardTable = styled.table<{
   grid-gap: ${({ $headerGap = '0.3rem' }): string => $headerGap};
   width: 100%;
 
-  tbody > tr:hover {
-    background: var(--color-background-selected);
-  }
-
   > thead, tbody {
-    > tr {
+    > tr:not(${CardRowDrawer}) {
+      position: relative;
       display: grid;
       grid-template-columns: ${({ $columns }): string => $columns || `repeat(auto-fit, minmax(3rem, 1fr))`};
       // grid-template-rows
@@ -37,9 +121,7 @@ export const CardTable = styled.table<{
       align-items: ${({ $align = 'center' }): string => $align};
       justify-content: ${({ $justify = 'center' }): string => $justify};
 
-      background-color: ${({ $bgColor = 'var(--color-background-pageWrapper)' }): string => $bgColor};
       border-radius: var(--border-radius);
-      box-shadow: var(--box-shadow);
 
       // How much separation between ROWS
       margin: ${({ $rowSeparation = '1rem' }): string => `${$rowSeparation} 0`};
@@ -48,31 +130,57 @@ export const CardTable = styled.table<{
       transition: all 0.2s ease-in-out;
       z-index: 1;
       
+      &.highlight {
+        background-color: var(--color-background-highlighted);
+        border-bottom-color: #fbdf8f;
+      }
+
+      &.selected {
+        background-color: var(--color-button-disabled);
+        color: var(--color-background-pageWrapper);
+      }
+
       // Separation between CELLS
       > * {
         margin: ${({ $cellSeparation = '0 0.5rem' }): string => $cellSeparation};
       }
     }
   }
-
-  > thead > tr {
-    background-color: transparent;
-    box-shadow: none;
+  
+  // Table Header
+  > thead {
+    // No styling for table header
+    > tr {
+      background-color: transparent;
+      box-shadow: none;
+    }
   }
 
-  > tbody > tr {
-    > td {
-      &.cardOpener {
-        display: none;
+  // Table Body
+  tbody {
+    > tr:not(${CardRowDrawer}) {
+      background-color: ${({ $bgColor = 'var(--color-background-pageWrapper)' }): string => $bgColor};
+      box-shadow: var(--box-shadow);
+
+      > td {
+        &.cardOpener {
+          display: none;
+        }
+      }
+
+      // Don't highlight on hover selected rows or the drawer
+      &:not(.selected):not(${CardRowDrawer}):hover {
+        background: var(--color-background-selected);
       }
     }
   }
+
   // Top level custom CSS
   ${({ $webCSS }): string | undefined => $webCSS}
 
   @media only screen and (max-width: ${RESPONSIVE_SIZES.TABLET}em) {
     > thead, tbody {
-      > tr {
+      > tr:not(${CardRowDrawer}) {
         grid-template-columns: none;
         grid-template-rows: auto;
 
@@ -104,9 +212,9 @@ export const CardTable = styled.table<{
             font-size: 0.7rem;
           }
         }
-  
+
         &.selected {
-          > td {
+          > td:not(:last-child) {
             border-bottom: 0.0625rem solid #ffffff40;
           }
         }
@@ -123,6 +231,7 @@ export const CardTable = styled.table<{
   }
 `
 
+// TODO: remove all below
 const fakeData = [
   { a: 1, b: 2, c: 3 },
   { a: 1, b: 2, c: 3 },
