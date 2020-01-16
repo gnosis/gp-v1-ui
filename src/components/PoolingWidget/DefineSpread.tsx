96b9196cf8a6d0930815c40c048509edef59950f
David Sato <david.sato64@gmail.com>
2020-01-16 18:07:48 +0100

    DefineSpread
    1. added input change handlers
    2. added table

--- src/components/PoolingWidget/DefineSpread.tsx
+++ src/components/PoolingWidget/DefineSpread.tsx
@@ -1,53 +1,60 @@
 import React from 'react'
-import styled from 'styled-components'
 import { TokenDetails } from '@gnosis.pm/dex-js'
-
-const DefineSpreadWrapper = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  > * {
-    margin: 0.5rem auto;
-    width: auto;
-  }
-  > input {
-    border: 0.125rem solid #000;
-    margin: auto;
-  }
-  > div {
-    padding: 1rem;
-  }
-`
-
-const SpreadInformationWrapper = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: space-evenly;
-  align-items: center;
-
-  background: var(--color-background-selected-dark);
-
-  width: 50%;
-`
+import useSafeState from 'hooks/useSafeState'
+import { SpreadInformationWrapper, DefineSpreadWrapper } from './DefineSpread.styled'
+import { DEFAULT_DECIMALS } from 'const'
 
 interface SpreadInformationProps {
   selectedTokens: TokenDetails[]
+  spread: number
 }
 
-const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokens }) => {
-  return (
-    <SpreadInformationWrapper>
-      {selectedTokens.map((token, index: number) => (
-        <div key={index}>Sell {token.symbol || token.name} for at least $1.00</div>
-      ))}
-    </SpreadInformationWrapper>
-  )
-}
+const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokens, spread }) => (
+  <SpreadInformationWrapper>
+    {/* [{ name: 'DAI', ... }, ...] */}
+    <table>
+      <thead>
+        <tr>
+          <th>Sell</th>
+          <th>Buy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {selectedTokens.map((token, index: number, arr) =>
+          arr.map(otherToken => {
+            if (otherToken.id === token.id) return
+            return (
+              <tr key={index}>
+                <td>
+                  Sell {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for{' '}
+                  <strong>at least</strong> ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
+                </td>
+                <td>
+                  Buy {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for{' '}
+                  <strong>at most</strong> ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
+                </td>
+              </tr>
+            )
+          }),
+        )}
+      </tbody>
+    </table>
+  </SpreadInformationWrapper>
+)
 
 const DefineSpread: React.FC<SpreadInformationProps> = ({ selectedTokens }) => {
+  const [spread, setSpread] = useSafeState(0.2)
+
+  const handleSpreadChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
+    if (+value < 0) return
+
+    setSpread(+value)
+  }
+
   return (
     <DefineSpreadWrapper>
-      <input type="number" placeholder="0.5%" />
-      <SpreadInformation selectedTokens={selectedTokens} />
+      <input type="number" step="0.1" value={spread} onChange={handleSpreadChange} />
+      <SpreadInformation selectedTokens={selectedTokens} spread={spread} />
     </DefineSpreadWrapper>
   )
 }
