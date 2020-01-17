d3be20dff085e0bbbb9a3660b3b031bec11065c3
David Sato <david.sato64@gmail.com>
2020-01-17 14:52:09 +0100

    DefineSpread
    1. added CardTable formatting
    2. styled responsive and web cells

--- src/components/PoolingWidget/DefineSpread.styled.ts
+++ src/components/PoolingWidget/DefineSpread.styled.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { ProgressStepText } from './PoolingWidget.styled'
+import { RESPONSIVE_SIZES } from 'const'
 
 export const DefineSpreadWrapper = styled.div`
   display: flex;
@@ -8,8 +10,20 @@ export const DefineSpreadWrapper = styled.div`
     width: auto;
   }
   > input {
-    border: 0.125rem solid #000;
+    background: var(--color-background-highlighted);
+    border: 0.2rem solid var(--color-button-primary);
+    font-size: larger;
     margin: auto;
+    width: 20%;
+    min-width: 5rem;
+    text-align: center;
+
+    &:focus {
+      background: #bbfdbb87;
+    }
+}
+
+
   }
   > div {
     padding: 1rem;
@@ -22,7 +36,31 @@ export const SpreadInformationWrapper = styled.div`
   justify-content: space-evenly;
   align-items: center;
 
-  background: var(--color-background-selected-dark);
+  background: var(--color-background);
+  border-radius: var(--border-radius);
 
-  width: 50%;
+  width: 82%;
+
+  .responsive-cell {
+    display: none;
+  }
+
+  @media only screen and (max-width: ${RESPONSIVE_SIZES.TABLET}em) {
+    width: 95%;
+
+    .web-cell {
+      display: none;
+    }
+
+    .responsive-cell {
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+`
+
+export const RedBoldText = styled(ProgressStepText)`
+  color: red;
 `
--- src/components/PoolingWidget/DefineSpread.tsx
+++ src/components/PoolingWidget/DefineSpread.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { TokenDetails } from '@gnosis.pm/dex-js'
 import useSafeState from 'hooks/useSafeState'
-import { SpreadInformationWrapper, DefineSpreadWrapper } from './DefineSpread.styled'
+import { SpreadInformationWrapper, DefineSpreadWrapper, RedBoldText } from './DefineSpread.styled'
 import { DEFAULT_DECIMALS } from 'const'
+import { CardTable } from 'components/Layout/Card'
 
 interface DefineSpreadProps {
   selectedTokensMap: Map<number, TokenDetails>
@@ -15,11 +16,34 @@ interface SpreadInformationProps extends DefineSpreadProps {
 
 const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokensMap, spread }) => (
   <SpreadInformationWrapper>
-    <table>
+    <CardTable
+      $columns="1fr 1fr"
+      $cellSeparation="0 1rem"
+      // TODO: change CardTable so that header row margins
+      // is a props and doesn't need !important
+      $webCSS={`
+        thead > tr { margin: 0 !important; };
+        tbody > tr { 
+          min-height: 3.5rem;
+        } 
+      `}
+      $responsiveCSS={`
+        tbody > tr { 
+          > td:first-child {
+            font-weight: bolder;
+            margin: auto;
+            width: 90%;
+            &::before {
+              content: none;
+            }
+          } 
+        }
+      `}
+    >
       <thead>
         <tr>
-          <th>Sell</th>
-          <th>Buy</th>
+          <th>Sell Spread</th>
+          <th>Buy Spread</th>
         </tr>
       </thead>
       <tbody>
@@ -28,20 +52,42 @@ const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokensMap
             if (otherId === id) return
             return (
               <tr key={`${token.address}-${otherToken.address}`} data-label={`${token.symbol}-${otherToken.symbol}`}>
-                <td>
-                  Sell {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for{' '}
-                  <strong>at least</strong> ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
+                <td className="responsive-cell">
+                  {token.symbol || token.name} - {otherToken.symbol || otherToken.name}
                 </td>
-                <td>
-                  Buy {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for{' '}
-                  <strong>at most</strong> ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
+                <td data-label="Sell (at least)">
+                  <div className="responsive-cell">
+                    <RedBoldText as="span" $bold="normal">
+                      ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
+                    </RedBoldText>
+                  </div>
+                  <div className="web-cell">
+                    {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for{' '}
+                    <strong>at least</strong>{' '}
+                    <RedBoldText as="span" $bold="normal">
+                      ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
+                    </RedBoldText>
+                  </div>
+                </td>
+                <td data-label="Buy (at most)">
+                  <div className="responsive-cell">
+                    <RedBoldText as="span" $bold="normal">
+                      ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
+                    </RedBoldText>
+                  </div>
+                  <div className="web-cell">
+                    {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for <strong>at most</strong>{' '}
+                    <RedBoldText as="span" $bold="normal">
+                      ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
+                    </RedBoldText>
+                  </div>
                 </td>
               </tr>
             )
           }),
         )}
       </tbody>
-    </table>
+    </CardTable>
   </SpreadInformationWrapper>
 )
 
