e71faf3b4a57be690110d24c247376f5104a1f04
David Sato <david.sato64@gmail.com>
2020-01-20 10:39:59 +0100

    PR requested changes
    1. DefineSpread no longer renders rows (infinite rows) and instead renders string sentence in 2 rows
    2. fixes spread percentage >0 and <= 100

--- src/components/PoolingWidget/DefineSpread.tsx
+++ src/components/PoolingWidget/DefineSpread.tsx
@@ -14,20 +14,25 @@ interface SpreadInformationProps extends DefineSpreadProps {
   spread: number
 }
 
-const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokensMap, spread }) => (
-  <SpreadInformationWrapper>
-    <CardTable
-      $columns="1fr 1fr"
-      $cellSeparation="0 1rem"
-      // TODO: change CardTable so that header row margins
-      // is a props and doesn't need !important
-      $webCSS={`
+const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokensMap, spread }) => {
+  const tokenSymbolsString = React.useMemo(() => Array.from(selectedTokensMap).map(([, token]) => token.symbol), [
+    selectedTokensMap,
+  ])
+
+  return (
+    <SpreadInformationWrapper>
+      <CardTable
+        $columns="1fr 1fr"
+        $cellSeparation="0 1rem"
+        // TODO: change CardTable so that header row margins
+        // is a props and doesn't need !important
+        $webCSS={`
         thead > tr { margin: 0 !important; };
         tbody > tr { 
           min-height: 3.5rem;
         } 
       `}
-      $responsiveCSS={`
+        $responsiveCSS={`
         tbody > tr { 
           > td:first-child {
             font-weight: bolder;
@@ -39,63 +44,54 @@ const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokensMap
           } 
         }
       `}
-    >
-      <thead>
-        <tr>
-          <th>Sell Spread</th>
-          <th>Buy Spread</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Array.from(selectedTokensMap).map(([id, token], _, arr) =>
-          arr.map(([otherId, otherToken]) => {
-            if (otherId === id) return
-            return (
-              <tr key={`${token.address}-${otherToken.address}`} data-label={`${token.symbol}-${otherToken.symbol}`}>
-                <td className="responsive-cell">
-                  {token.symbol || token.name} - {otherToken.symbol || otherToken.name}
-                </td>
-                <td data-label="Sell (at least)">
-                  <div className="responsive-cell">
-                    <RedBoldText as="span" $bold="normal">
-                      ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
-                    </RedBoldText>
-                  </div>
-                  <div className="web-cell">
-                    {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for{' '}
-                    <strong>at least</strong>{' '}
-                    <RedBoldText as="span" $bold="normal">
-                      ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
-                    </RedBoldText>
-                  </div>
-                </td>
-                <td data-label="Buy (at most)">
-                  <div className="responsive-cell">
-                    <RedBoldText as="span" $bold="normal">
-                      ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
-                    </RedBoldText>
-                  </div>
-                  <div className="web-cell">
-                    {token.symbol || token.name} and {otherToken.symbol || otherToken.name} for <strong>at most</strong>{' '}
-                    <RedBoldText as="span" $bold="normal">
-                      ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
-                    </RedBoldText>
-                  </div>
-                </td>
-              </tr>
-            )
-          }),
-        )}
-      </tbody>
-    </CardTable>
-  </SpreadInformationWrapper>
-)
+      >
+        <thead>
+          <tr>
+            <th>Sell Spread</th>
+            <th>Buy Spread</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="responsive-cell">{tokenSymbolsString.join(' - ')}</td>
+            <td data-label="Sell (at least)">
+              <div className="responsive-cell">
+                <RedBoldText as="span" $bold="normal">
+                  ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
+                </RedBoldText>
+              </div>
+              <div className="web-cell">
+                {tokenSymbolsString.join(', ')} for <strong>at least</strong>{' '}
+                <RedBoldText as="span" $bold="normal">
+                  ${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}
+                </RedBoldText>
+              </div>
+            </td>
+            <td data-label="Buy (at most)">
+              <div className="responsive-cell">
+                <RedBoldText as="span" $bold="normal">
+                  ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
+                </RedBoldText>
+              </div>
+              <div className="web-cell">
+                {tokenSymbolsString.join(', ')} for <strong>at most</strong>{' '}
+                <RedBoldText as="span" $bold="normal">
+                  ${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}
+                </RedBoldText>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </CardTable>
+    </SpreadInformationWrapper>
+  )
+}
 
 const DefineSpread: React.FC<DefineSpreadProps> = ({ selectedTokensMap, defaultSpread = 0.2 }) => {
   const [spread, setSpread] = useSafeState(defaultSpread)
 
   const handleSpreadChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
-    if (+value < 0) return
+    if (+value < 0 || +value >= 100) return
 
     setSpread(+value)
   }
