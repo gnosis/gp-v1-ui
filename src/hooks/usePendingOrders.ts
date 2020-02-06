464fb6d6e252d3d20607be1b24c3b0ae191ff5fc
David Sato <david.sato64@gmail.com>
2020-02-06 12:02:45 +0100

    created usePendingOrders separate from useOrders

---
+++ src/hooks/usePendingOrders.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import { PendingTxArray } from 'api/exchange/ExchangeApi'
+
+import useGlobalState from './useGlobalState'
+import useSafeState from './useSafeState'
+import { useWalletConnection } from './useWalletConnection'
+
+function usePendingOrders(): PendingTxArray {
+  const { userAddress, networkId } = useWalletConnection()
+
+  const [{ pendingOrders: pendingOrdersGlobal }] = useGlobalState()
+  const [pendingOrders, setPendingOrders] = useSafeState<PendingTxArray>([])
+
+  useEffect(() => {
+    userAddress && networkId && setPendingOrders(pendingOrdersGlobal[networkId][userAddress] || [])
+    // updating list of orders on every block by listening on `blockNumber`
+  }, [networkId, pendingOrdersGlobal, setPendingOrders, userAddress])
+
+  return pendingOrders
+}
+
+export default usePendingOrders
