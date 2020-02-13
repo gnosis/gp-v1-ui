49939d9d6a2fe5021c9a00ecdb02b9bae862609e
Leandro Boscariol <alfetopito@users.noreply.github.com>
2020-02-13 13:39:02 -0300

    477/ notifications displayed longer (#524)
    * Making toast notifications stay twice longer (from 5s to 10s)
    
    * Moving toast notification duration to const

--- src/setupToastify.ts
+++ src/setupToastify.ts
@@ -1,6 +1,8 @@
 import 'react-toastify/dist/ReactToastify.min.css'
 import { toast } from 'react-toastify'
 
-toast.configure({ position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: false })
+import { TOAST_NOTIFICATION_DURATION } from 'const'
+
+toast.configure({ position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: false, autoClose: TOAST_NOTIFICATION_DURATION })
 
 export { toast }
