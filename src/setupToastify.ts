20fedc9dc1214146b4ea09c05ee96e20225c1d7c
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-02-13 08:45:45 -0300

    Making toast notifications stay twice longer (from 5s to 10s)

--- src/setupToastify.ts
+++ src/setupToastify.ts
@@ -1,6 +1,6 @@
 import 'react-toastify/dist/ReactToastify.min.css'
 import { toast } from 'react-toastify'
 
-toast.configure({ position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: false })
+toast.configure({ position: toast.POSITION.BOTTOM_RIGHT, closeOnClick: false, autoClose: 10000 })
 
 export { toast }
