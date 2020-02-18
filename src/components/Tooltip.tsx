b5ffb5de73e3dca167986183c22d7ba69908739e
Velenir <velenir@gmail.com>
2020-02-18 16:39:03 +0300

    extend Wrapper to allow for custom tag and better ref propagation

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,6 +3,7 @@ import Portal from './Portal'
 import { usePopperDefault, TOOLTIP_OFFSET } from 'hooks/usePopper'
 import { State, Placement } from '@popperjs/core'
 import styled from 'styled-components'
+import { isElement, isFragment } from 'react-is'
 
 const TooltipOuter = styled.div<Pick<TooltipBaseProps, 'isShown'>>`
   visibility: ${(props): string | false => !props.isShown && 'hidden'};
@@ -79,16 +80,36 @@ export const Tooltip = React.memo(React.forwardRef<HTMLDivElement, TooltipProps>
 interface WrapperProps {
   tooltip: ReactNode
   placement?: Placement
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>
 }
 
-export const TooltipWrapper: React.FC<WrapperProps> = ({ children, tooltip, placement }) => {
+const Wrapper: React.FC<WrapperProps> = ({ children, tooltip, placement, as }) => {
   const { targetProps, tooltipProps } = usePopperDefault<HTMLDivElement>(placement)
 
+  const chilrenNumber = React.Children.count(children)
+
+  // can attach ref to element
+  // if children is a single element, not just text, and `as` tag|Component not specified
+  if (chilrenNumber === 1 && typeof as === 'undefined' && isElement(children) && !isFragment(children)) {
+    const TargetComponent = React.cloneElement(children, targetProps)
+    return (
+      <>
+        {TargetComponent}
+        <Tooltip {...tooltipProps}>{tooltip}</Tooltip>
+      </>
+    )
+  }
+
+  //  if as not provided and can't clone single element
+  //  use div
+  const TargetComponent = as || 'div'
+
   return (
-    <div {...targetProps}>
+    <TargetComponent {...targetProps}>
       {children}
       <Tooltip {...tooltipProps}>{tooltip}</Tooltip>
-    </div>
+    </TargetComponent>
   )
 }
 
