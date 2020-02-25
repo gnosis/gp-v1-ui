8082ee1fa6daf63d9d2c4747900b96c187cf9e3d
David Sato <david.sato64@gmail.com>
2020-02-25 19:03:02 +0530

    order validity try 1

--- src/components/TradeWidget/OrderValidity.tsx
+++ src/components/TradeWidget/OrderValidity.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useCallback, Dispatch, SetStateAction } from 'react'
 import styled from 'styled-components'
 import { useFormContext } from 'react-hook-form'
 
-import { TradeFormTokenId, TradeFormData } from './'
-import { adjustPrecision } from '@gnosis.pm/dex-js'
+import { TradeFormTokenId, TradeFormData, PriceInputBox } from './'
+import { adjustPrecision, ZERO } from '@gnosis.pm/dex-js'
 import cog from 'assets/img/cog.svg'
+import useSafeState from 'hooks/useSafeState'
+import { validInputPattern, validatePositive, formatValidity } from 'utils'
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,6 +15,25 @@ const Wrapper = styled.div`
   width: 100%;
   flex-flow: row wrap;
   border-bottom: 0.1rem solid #dfe6ef;
+
+  > input {
+    width: 100%;
+  }
+
+  .radio-container {
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-content: center;
+
+    > small {
+      margin: auto 0.5rem;
+    }
+  }
 
   > button {
     width: 100%;
@@ -55,6 +76,48 @@ const Wrapper = styled.div`
   > button > b {
     color: #218dff;
     margin: 0 0.4rem;
+  }
+`
+
+const OrderValidityInputsWrapper = styled.div<{ $visible: boolean }>`
+  opacity: ${({ $visible }): string => ($visible ? '1' : '0')};
+  position: fixed;
+
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  background: var(--color-background-pageWrapper);
+  color: var(--color-text-primary);
+  z-index: 500;
+  box-shadow: 0 999vh 0 999vw rgba(47, 62, 78, 0.5);
+
+  max-width: 50rem;
+  min-width: 30rem;
+  height: 12.7rem;
+  padding: 1rem 4rem;
+  border-radius: 0.8rem;
+
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+
+  transition: all 0.5s ease-in-out;
+  > strong {
+    text-transform: capitalize;
+    color: #2f3e4e;
+    width: 100%;
+    margin: 0 0 1rem;
+    padding: 0 1rem;
+    box-sizing: border-box;
+  }
+  > button {
+    background: #218dff;
+    border-radius: 3rem;
+    padding: 0.6rem 1.2rem;
+    color: white;
+    margin: 1rem auto;
   }
 `
 
@@ -128,67 +191,145 @@ const Wrapper = styled.div`
 // }
 
 interface Props {
-  inputId: TradeFormTokenId
+  validFromInputId: TradeFormTokenId
+  validUntilInputId: TradeFormTokenId
   isDisabled: boolean
+  isAsap: boolean
   tabIndex: number
   isUnlimited: boolean
+  setAsap: Dispatch<SetStateAction<boolean>>
   setUnlimited: Dispatch<SetStateAction<boolean>>
 }
 
-const OrderValidity: React.FC<Props> = ({ inputId }) => {
-  const { setValue, watch } = useFormContext<TradeFormData>()
-  // const error = errors[inputId]
-  const inputValue = watch(inputId)
-  // const overMax = ZERO
-  // const className = error ? 'error' : overMax.gt(ZERO) ? 'warning' : ''
-  // const errorOrWarning = error && <WalletDetail className="error">{error.message}</WalletDetail>
+const OrderValidity: React.FC<Props> = ({
+  isDisabled,
+  isUnlimited,
+  setAsap,
+  setUnlimited,
+  isAsap,
+  tabIndex,
+  validFromInputId,
+  validUntilInputId,
+}) => {
+  const [showOrderConfig, setShowOrderConfig] = useSafeState(false)
+  const { setValue, errors, register, getValues, watch } = useFormContext<TradeFormData>()
+  const { validFrom, validUntil } = getValues()
 
-  const handleChange = useCallback(() => {
-    const newValue = adjustPrecision(inputValue, 0)
-    if (inputValue !== newValue) {
-      setValue(inputId, newValue, true)
+  const validFromError = errors[validFromInputId]
+  const validUntilError = errors[validUntilInputId]
+  const validFromInputValue = watch(validFromInputId)
+  const validUntilInputValue = watch(validUntilInputId)
+  const overMax = ZERO
+  const validFromClassName = validFromError ? 'error' : overMax.gt(ZERO) ? 'warning' : ''
+  const validUntilClassName = validUntilError ? 'error' : overMax.gt(ZERO) ? 'warning' : ''
+
+  // const validFromErrorComp = validFromError && <WalletDetail className="error">{validFromError.message}</WalletDetail>
+  // const validUntilErrorComp = validUntilError && (
+  //   <WalletDetail className="error">{validUntilError.message}</WalletDetail>
+  // )
+
+  const handleShowConfig = (): void => setShowOrderConfig(!showOrderConfig)
+
+  const handleValidFromChange = useCallback(() => {
+    const newValue = adjustPrecision(validFromInputValue, 0)
+    if (validFromInputValue !== newValue) {
+      setValue(validFromInputId, newValue, true)
     }
-  }, [inputValue, setValue, inputId])
+  }, [validFromInputValue, setValue, validFromInputId])
+
+  const handleValidUntilChange = useCallback(() => {
+    const newValue = adjustPrecision(validUntilInputValue, 0)
+    if (validUntilInputValue !== newValue) {
+      setValue(validUntilInputId, newValue, true)
+    }
+  }, [validUntilInputValue, setValue, validUntilInputId])
 
   useEffect(() => {
-    handleChange()
-  }, [handleChange])
+    handleValidFromChange()
+  }, [handleValidFromChange])
 
-  // function handleUnlimitedClick(): void {
-  //   setUnlimited(!isUnlimited)
-  //   !isUnlimited ? setValue(inputId, '', true) : setValue(inputId, '30', true)
-  // }
+  useEffect(() => {
+    handleValidUntilChange()
+  }, [handleValidUntilChange])
+
+  function handleUnlimitedClick(): void {
+    setUnlimited(!isUnlimited)
+    !isUnlimited ? setValue(validUntilInputId, '', true) : setValue(validUntilInputId, '30', true)
+  }
+  function handleASAPClick(): void {
+    setAsap(!isAsap)
+    !isAsap ? setValue(validFromInputId, '', true) : setValue(validFromInputId, '30', true)
+  }
 
   //TODO: re-enable input logic
   return (
     <Wrapper>
-      <button>
-        Order starts: <b>ASAP</b> - expires in: <b>30 minutes</b>
+      <button onClick={handleShowConfig}>
+        Order starts: <b>{formatValidity(validFrom, 'ASAP')}</b> - expires in:{' '}
+        <b>{formatValidity(validUntil, 'Never')}</b>
       </button>
-      {/* <InputBox>
-        <div className="main-input-container">
-          <input
-            className={className}
-            name={inputId}
-            type="number"
-            step="5"
-            disabled={isDisabled || isUnlimited}
-            required
-            ref={register({
-              pattern: { value: validInputPattern, message: 'Expiration time cannot be negative' },
-              validate: { positive: validatePositive },
-            })}
-            onChange={handleChange}
-            onFocus={(e): void => e.target.select()}
-            tabIndex={tabIndex + 2}
-          />
-          <div className="radio-container">
-            <input type="checkbox" disabled={isDisabled} defaultChecked={isUnlimited} onClick={handleUnlimitedClick} />
-            <small>Never</small>
-          </div>
-        </div>
-        {errorOrWarning}
-      </InputBox> */}
+
+      <OrderValidityInputsWrapper $visible={showOrderConfig}>
+        <PriceInputBox>
+          <strong>Order starts in (min)</strong>
+          <label>
+            <input
+              className={validFromClassName}
+              name={validFromInputId}
+              type="number"
+              step="5"
+              disabled={isDisabled || isAsap}
+              required
+              ref={register({
+                pattern: {
+                  value: validInputPattern,
+                  message: 'Order from time cannot be negative or less than 15 minutes',
+                },
+                validate: value => Number(value) === 0 || Number(value) >= 15,
+              })}
+              onChange={handleValidFromChange}
+              onFocus={(e): void => e.target.select()}
+              tabIndex={tabIndex + 2}
+            />
+            <div className="radio-container">
+              <input type="checkbox" disabled={isDisabled} defaultChecked={isAsap} onClick={handleASAPClick} />
+              <small>ASAP</small>
+            </div>
+            {/* {validFromErrorComp} */}
+          </label>
+        </PriceInputBox>
+        <PriceInputBox>
+          <strong>Order expires in (min)</strong>
+          <label>
+            <input
+              className={validUntilClassName}
+              name={validUntilInputId}
+              type="number"
+              step="5"
+              disabled={isDisabled || isUnlimited}
+              required
+              ref={register({
+                pattern: { value: validInputPattern, message: 'Expiration time cannot be negative' },
+                validate: { positive: validatePositive },
+              })}
+              onChange={handleValidUntilChange}
+              onFocus={(e): void => e.target.select()}
+              tabIndex={tabIndex + 2}
+            />
+            <div className="radio-container">
+              <input
+                type="checkbox"
+                disabled={isDisabled}
+                defaultChecked={isUnlimited}
+                onClick={handleUnlimitedClick}
+              />
+              <small>Never</small>
+            </div>
+            {/* {validUntilErrorComp} */}
+          </label>
+        </PriceInputBox>
+        <button onClick={(): void => setShowOrderConfig(false)}>Set order parameters</button>
+      </OrderValidityInputsWrapper>
     </Wrapper>
   )
 }
