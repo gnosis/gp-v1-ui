fd9a4025fcfa94de63d3d6afe28cab3b13f1624a
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-02-27 15:10:02 -0300

    Spread input validation onKeypress, onChange and onBlur

--- src/components/PoolingWidget/DefineSpread.tsx
+++ src/components/PoolingWidget/DefineSpread.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { TokenDetails } from '@gnosis.pm/dex-js'
+import React, { useCallback } from 'react'
+import { TokenDetails, adjustPrecision } from '@gnosis.pm/dex-js'
 
 import { SpreadInformationWrapper, DefineSpreadWrapper } from './DefineSpread.styled'
 
 import { DEFAULT_DECIMALS } from 'const'
+import { formatPartialNumber, preventInvalidChars } from 'utils'
 
 interface DefineSpreadProps {
   selectedTokensMap: Map<number, TokenDetails>
@@ -37,17 +38,45 @@ export const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTo
   )
 }
 
-  const handleSpreadChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
-    if (+value < 0 || +value >= 100) return
 const DefineSpread: React.FC<DefineSpreadProps> = ({ selectedTokensMap, spread, setSpread, nextStep }) => {
+  const onChange = useCallback(
+    ({ currentTarget: { value } }: React.SyntheticEvent<HTMLInputElement>): void => {
+      if (+value === NaN || +value <= 0 || +value >= 100) {
+        return
+      }
 
-    setSpread(+value)
-  }
+      const newValue = formatPartialNumber(value)
+
+      setSpread(Number(newValue))
+    },
+    [setSpread],
+  )
+
+  const onBlur = useCallback(
+    ({ currentTarget: { value } }: React.SyntheticEvent<HTMLInputElement>): void => {
+      const newValue = adjustPrecision(value, 1)
+
+      setSpread(Number(newValue))
+    },
+    [setSpread],
+  )
+
+  const onKeyPress = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>): void => {
+      if (event.key === 'Enter') {
+        onBlur(event)
+        nextStep()
+      } else {
+        preventInvalidChars(event)
+      }
+    },
+    [nextStep, onBlur],
+  )
 
   return (
     <DefineSpreadWrapper>
       <strong>Spread %</strong>
-      <input type="number" step="0.1" value={spread} onChange={handleSpreadChange} />
+      <input type="number" step="0.1" value={spread} onChange={onChange} onBlur={onBlur} onKeyPress={onKeyPress} />
       <SpreadInformation selectedTokensMap={selectedTokensMap} spread={spread} />
     </DefineSpreadWrapper>
   )
