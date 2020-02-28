09fbb6ceb5505b56aabaf6680a28992790c84f72
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-02-28 09:15:30 -0300

    connectWallet/disconnectWallet now return a boolean indicating success

--- src/hooks/useConnectWallet.ts
+++ src/hooks/useConnectWallet.ts
@@ -2,32 +2,42 @@ import { walletApi } from 'api'
 import { toast } from 'toastify'
 
 interface Result {
-  connectWallet: () => Promise<void>
-  disconnectWallet: () => Promise<void>
+  connectWallet: () => Promise<boolean>
+  disconnectWallet: () => Promise<boolean>
 }
 
 export function useConnectWallet(): Result {
-  const connectWallet = async (): Promise<void> => {
+  const connectWallet = async (): Promise<boolean> => {
     try {
       const success = await walletApi.connect()
 
       // user closed Provider selection modal
-      if (!success) return
+      if (!success) {
+        return false
+      }
 
       toast.success('Wallet connected')
+
+      return true
     } catch (error) {
       console.error('[WalletComponent] Connect wallet error', error)
       toast.error('Error connecting wallet')
+
+      return false
     }
   }
 
-  const disconnectWallet = async (): Promise<void> => {
+  const disconnectWallet = async (): Promise<boolean> => {
     try {
       await walletApi.disconnect()
       toast.info('Wallet disconnected')
+
+      return true
     } catch (error) {
       console.error('[WalletComponent] Disconnect wallet error', error)
       toast.error('Error disconnecting wallet')
+
+      return false
     }
   }
 
