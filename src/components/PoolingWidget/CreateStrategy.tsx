292a2505a738ac9870cfa929bc44cc2756b9ea97
David Sato <david.sato64@gmail.com>
2020-03-04 19:23:40 +0530

    CreateStrategy
    1. now holds DefineSpread and is a state change page
    2. Now is step 2 (final step)

--- src/components/PoolingWidget/CreateStrategy.tsx
+++ src/components/PoolingWidget/CreateStrategy.tsx
@@ -1,18 +1,46 @@
 import React from 'react'
-import { TokenDetails } from '@gnosis.pm/dex-js'
+import { TokenDetails, DEFAULT_DECIMALS } from '@gnosis.pm/dex-js'
 
-import { BlueBoldText } from './DefineSpread.styled'
-import { SpreadInformation } from './DefineSpread'
+import { BlueBoldText, SpreadInformationWrapper } from './DefineSpread.styled'
+import DefineSpread from './DefineSpread'
 import { CreateStrategyWrapper } from './CreateStrategy.styled'
 import AddFunding from './AddFunding'
 import { Receipt } from 'types'
 
 export interface CreateStrategyProps {
+  isSubmitting: boolean
   selectedTokensMap: Map<number, TokenDetails>
   spread: number
+  setSpread: (spread: number) => void
   txIdentifier: string
   txReceipt?: Receipt
   txError?: Error
+}
+
+interface SpreadInformationProps {
+  selectedTokensMap: Map<number, TokenDetails>
+  spread: number
+}
+
+const SpreadInformation: React.FC<SpreadInformationProps> = ({ selectedTokensMap, spread }) => {
+  const tokenSymbolsString = React.useMemo(() => Array.from(selectedTokensMap.values()).map(token => token.symbol), [
+    selectedTokensMap,
+  ])
+
+  return (
+    <SpreadInformationWrapper>
+      <strong>Sell Spread</strong>
+      <p>
+        {tokenSymbolsString.join(', ')} for <b>at least</b> <br />
+        <i>${(1 + spread / 100).toFixed(DEFAULT_DECIMALS)}</i>
+      </p>
+      <strong>Buy Spread</strong>
+      <p>
+        {tokenSymbolsString.join(', ')} for <b>at most</b> <br />
+        <i>${(1 - spread / 100).toFixed(DEFAULT_DECIMALS)}</i>
+      </p>
+    </SpreadInformationWrapper>
+  )
 }
 
 export const CreateStrategy: React.FC<CreateStrategyProps> = ({
@@ -21,6 +49,7 @@ export const CreateStrategy: React.FC<CreateStrategyProps> = ({
   txIdentifier,
   txReceipt,
   txError,
+  isSubmitting,
 }) => {
   const ordersCount = selectedTokensMap.size * (selectedTokensMap.size - 1)
 
@@ -32,6 +61,7 @@ export const CreateStrategy: React.FC<CreateStrategyProps> = ({
         <div>ERROR! {txError}</div>
       ) : (
         <>
+          <DefineSpread spread={spread} isSubmitting={isSubmitting} />
           <p>
             Once the transaction is sent and mined, <BlueBoldText as="span">{ordersCount} orders</BlueBoldText> will be
             created.
