cc6e794c31fb0055888f64f6042bcab3017b3546
David Sato <david.sato64@gmail.com>
2020-03-12 19:55:22 +0530

    removed TODO comment

--- src/components/Layout/Card/Card.tsx
+++ src/components/Layout/Card/Card.tsx
@@ -128,8 +128,6 @@ export const CardTable = styled.table<{
     position: sticky;
     background: #ecf2f7;
     top: 0;
-    // TODO: why is this 5? see no problems in testing
-    // z-index: 5;
     font-size: 1.1rem;
     color: #2F3E4E;
     letter-spacing: 0;
