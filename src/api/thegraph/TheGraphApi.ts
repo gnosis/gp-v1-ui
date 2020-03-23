4d6245205dae89aa6b1e875162e9d41120593808
Leandro Boscariol <alfetopito@users.noreply.github.com>
2020-03-23 08:39:34 -0700

    TheGraphApi and getPriceEstimation service (#733)
    * First attempt to get a very simple TheGraphApi implementation.
    Spoiler alert: fails due to CORS
    
    * more fetch options
    
    * proper types
    
    * implement typeguards
    
    * fix api endpoints
    
    * use staging graph for mainnet (revert later)
    
    * Fetching a list of prices at once from the Graph
    
    * Returning prices in WEI or units
    
    * Adding TheGraphApiProxy, for caching calls
    
    * New service getPriceEstimation
    
    * Simplifying string replacement
    
    Co-Authored-By: Velenir <Velenir@users.noreply.github.com>
    
    * Further optimization suggested by Dima
    
    * Renaming getPrice parameters
    
    * Making code clearer by (gasp) using ONE EXTRA VARIABLE 😲
    
    * De-optmizing :(
    
    * Returning null instead of 0 for empty values from TheGraph
    
    * Returning null instead of 0 when there are no prices for getPriceEstimation service
    
    Co-authored-by: Leandro Boscariol <leandro.boscariol@gnosis.io>
    Co-authored-by: Velenir <velenir@gmail.com>
    Co-authored-by: Velenir <Velenir@users.noreply.github.com>

---
+++ src/api/thegraph/TheGraphApi.ts
@@ -0,0 +1,157 @@
+import { assert, DEFAULT_PRECISION } from '@gnosis.pm/dex-js'
+import BigNumber from 'bignumber.js'
+
+import { TEN_BIG_NUMBER } from 'const'
+
+export interface TheGraphApi {
+  getPrice(params: GetPriceParams): Promise<BigNumber | null>
+  getPrices(params: GetPricesParams): Promise<GetPricesResult>
+}
+
+export interface GetPriceParams {
+  networkId: number
+  tokenId: number
+  inWei?: boolean
+}
+
+export interface GetPricesParams {
+  networkId: number
+  tokenIds: number[]
+  inWei?: boolean
+}
+
+interface GetPricesResult {
+  [tokenId: number]: BigNumber | null
+}
+
+interface UrlByNetwork {
+  [network: number]: string
+}
+
+interface PriceEntry {
+  priceInOwl: string
+  token: {
+    decimals?: number
+  }
+}
+
+interface PricesResponse {
+  data: {
+    [tokenKey: string]: PriceEntry[]
+  }
+}
+
+interface GqlError {
+  errors: {
+    locations: {
+      line: number
+      column: number
+    }[]
+    message: string
+  }[]
+}
+
+type GqlResult = PricesResponse | GqlError
+
+const isGqlError = (gqlResult: GqlResult): gqlResult is GqlError => 'errors' in gqlResult
+
+export interface Params {
+  urls: UrlByNetwork
+}
+
+export class TheGraphApiImpl {
+  private urlByNetwork: UrlByNetwork
+
+  public constructor(params: Params) {
+    this.urlByNetwork = params.urls
+  }
+
+  public async getPrice({ tokenId, ...params }: GetPriceParams): Promise<BigNumber | null> {
+    // syntactic sugar
+    const prices = this.getPrices({ ...params, tokenIds: [tokenId] })
+    return prices[tokenId]
+  }
+
+  /**
+   * Fetches prices for token based on TokenIds, in relation to OWL.
+   * Price is returned in units by default. To return in WEI, set `inWei` to true.
+   */
+  public async getPrices({ networkId, tokenIds, inWei = false }: GetPricesParams): Promise<GetPricesResult> {
+    const tokens = tokenIds.map(tokenId => this.buildPartialPriceQuery(tokenId)).join('\n')
+    const queryString = `{${tokens}}`
+
+    try {
+      const response = await this.query<PricesResponse>(networkId, queryString)
+
+      return this.parsePricesResponse(response, inWei)
+    } catch (e) {
+      console.error(e)
+      throw new Error(`Failed to query prices: ${e.message}`)
+    }
+  }
+
+  private async query<T>(networkId: number, queryString: string): Promise<T> {
+    const url = this.urlByNetwork[networkId]
+    assert(url, `TheGraph instance is not available for network id ${networkId}`)
+
+    const data = {
+      query: queryString,
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'omit',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Request failed: [${response.status}] ${response.body}`)
+    }
+
+    const gqlResult = await response.json()
+
+    if (isGqlError(gqlResult)) {
+      throw new Error(`Query failed: ${gqlResult.errors[0].message}`)
+    }
+
+    return gqlResult
+  }
+
+  private buildPartialPriceQuery(tokenId: number): string {
+    return `Token${tokenId}: prices(
+  first: 1, 
+  orderBy: batchId, 
+  orderDirection: desc, 
+  where: {token: "${tokenId}"}
+) {
+  priceInOwl
+  token {
+    decimals
+  }
+}`
+  }
+
+  private parsePricesResponse({ data }: PricesResponse, inWei: boolean): GetPricesResult {
+    return Object.keys(data).reduce((acc, tokenKey) => {
+      // not possible to have a key with only integers, thus `Token` prefix was added
+      const tokenId = +tokenKey.replace('Token', '')
+
+      // When there's no data for a token, return is null
+      acc[tokenId] = data[tokenKey].length > 0 ? this.calculatePrice(data[tokenKey][0], inWei) : null
+      return acc
+    }, {})
+  }
+
+  private calculatePrice(
+    { priceInOwl, token: { decimals = DEFAULT_PRECISION } }: PriceEntry,
+    inWei: boolean,
+  ): BigNumber {
+    const price = new BigNumber(priceInOwl)
+
+    return inWei ? price : price.dividedBy(TEN_BIG_NUMBER.exponentiatedBy(decimals))
+  }
+}
