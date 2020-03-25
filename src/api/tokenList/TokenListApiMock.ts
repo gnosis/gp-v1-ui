bc4a4ffc39061e5f23bf88545acd1a9ec4252e32
Velenir <velenir@gmail.com>
2020-03-25 16:05:41 +0300

    adapt TokenListApiMock

--- src/api/tokenList/TokenListApiMock.ts
+++ src/api/tokenList/TokenListApiMock.ts
@@ -1,5 +1,6 @@
 import { TokenDetails } from 'types'
-import { TokenList } from './TokenListApi'
+import { TokenList, AddTokenParams, AddTokenResult, AddTokenToExchangeParams } from './TokenListApi'
+
 export class TokenListApiMock implements TokenList {
   private _tokenList: TokenDetails[]
 
@@ -10,6 +11,26 @@ export class TokenListApiMock implements TokenList {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public getTokens(_networkId: number): TokenDetails[] {
     return this._tokenList
+  }
+
+  public async addTokenToList({ tokenAddress, networkId }: AddTokenParams): Promise<AddTokenResult> {
+    this._tokenList.push({
+      id: this._tokenList.length + 1,
+      name: 'New Token',
+      symbol: 'NTK',
+      decimals: 18,
+      addressMainnet: '0x12345',
+      image: `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x12345/logo.png`,
+      address: tokenAddress,
+    })
+    return {
+      success: true,
+      tokenList: this.getTokens(networkId),
+    }
+  }
+
+  public addTokenToExchange(params: AddTokenToExchangeParams): Promise<AddTokenResult> {
+    return this.addTokenToList(params)
   }
 }
 
