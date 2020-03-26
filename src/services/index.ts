bfa70fdfbdd78eb4300c66d711c3737a00f721b5
Velenir <velenir@gmail.com>
2020-03-26 12:52:07 +0300

    fix circular dependency

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,6 +6,8 @@ import {
   addTokenToExchangeFactory,
   getPriceEstimationFactory,
 } from './factories'
+import { logDebug } from 'utils'
+import { TokenDetails } from '@gnosis.pm/dex-js'
 
 const apis = {
   tokenListApi,
@@ -19,6 +21,53 @@ export const getTokenFromExchangeByAddress = getTokenFromExchangeByAddressFactor
 
 export const getTokenFromExchangeById = getTokenFromExchangeByIdFactory(apis)
 
-export const addTokenToExchange = addTokenToExchangeFactory(apis)
+export const addTokenToExchangeContract = addTokenToExchangeFactory(apis)
 
 export const getPriceEstimation = getPriceEstimationFactory(apis)
+
+interface AddTokenToListParams {
+  networkId: number
+  tokenAddress: string
+}
+interface AddTokenToExchangeParams extends AddTokenToListParams {
+  userAddress: string
+}
+interface AddTokenResult {
+  success: boolean
+  tokenList: TokenDetails[]
+}
+
+export const addTokenToList = async ({ networkId, tokenAddress }: AddTokenToListParams): Promise<AddTokenResult> => {
+  const token = await getTokenFromExchangeByAddress({ networkId, tokenAddress })
+  if (token) {
+    logDebug('Added new Token to userlist', token)
+
+    tokenListApi.addToken({ token, networkId })
+  } else {
+    logDebug('Token at address', tokenAddress, 'not available in Exchange contract')
+  }
+  return {
+    success: !!token,
+    tokenList: tokenListApi.getTokens(networkId),
+  }
+}
+;(window as any).addTokenToList = addTokenToList
+
+export const addTokenToExchange = async ({
+  userAddress,
+  networkId,
+  tokenAddress,
+}: AddTokenToExchangeParams): Promise<AddTokenResult> => {
+  const token = await addTokenToExchangeContract({ userAddress, networkId, tokenAddress })
+  if (token) {
+    logDebug('Added new Token to userlist', token)
+
+    tokenListApi.addToken({ token, networkId })
+  } else {
+    logDebug('Token at address', tokenAddress, 'could not be added to Exchange contract')
+  }
+  return {
+    success: !!token,
+    tokenList: tokenListApi.getTokens(networkId),
+  }
+}
