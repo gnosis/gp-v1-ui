c8bb73d22baf9f866a465ea8accef138026b46dd
Velenir <velenir@gmail.com>
2020-03-26 13:53:26 +0300

    add TokenListApi tests

--- test/api/ExchangeApi/TokenListApiMock.test.ts
+++ test/api/ExchangeApi/TokenListApiMock.test.ts
@@ -12,18 +12,59 @@ beforeEach(() => {
   instanceReal = new TokenListApiImpl([Network.Mainnet, Network.Rinkeby])
 })
 
+const NEW_TOKEN = {
+  id: 7,
+  name: 'New Token',
+  symbol: 'NTK',
+  decimals: 18,
+  addressMainnet: '0x1234...',
+  image: '',
+  address: '0x1234...',
+}
+
 // TODO: These tests are dumb. Either do something meaningful or remove them entirely
 
-describe('MOCK: Basic view functions', () => {
+describe('MOCK: Basic functions', () => {
   test('Mock API Token list has length 7', () => {
     const tokens = instanceMock.getTokens(1)
     expect(tokens.length).toBe(7)
   })
+  test('Mock API Token list has expected token', () => {
+    const tokens = instanceMock.getTokens(1)
+    expect(instanceMock.hasToken({ tokenAddress: tokens[0].address, networkId: 1 })).toBe(true)
+  })
+  test('Mock API Token list can add tokens', () => {
+    const length = instanceMock.getTokens(1).length
+    instanceMock.addToken({
+      token: NEW_TOKEN,
+      networkId: 1,
+    })
+    expect(instanceMock.getTokens(1)).toHaveLength(length + 1)
+  })
 })
 
-describe('REAL: Basic view functions', () => {
+describe('REAL: Basic functions', () => {
   test('API Token list has length 12', () => {
     const tokens = instanceReal.getTokens(1)
     expect(tokens.length).toBe(12)
+  })
+  test('API Token list has expected token', () => {
+    const tokens = instanceReal.getTokens(1)
+    expect(instanceReal.hasToken({ tokenAddress: tokens[0].address, networkId: 1 })).toBe(true)
+  })
+  test('API Token list can add tokens', () => {
+    const length = instanceReal.getTokens(1).length
+    instanceReal.addToken({
+      token: NEW_TOKEN,
+      networkId: 1,
+    })
+    expect(instanceReal.getTokens(1)).toHaveLength(length + 1)
+
+    const userListStringified = localStorage.getItem('USER_TOKEN_LIST_1')
+    expect(userListStringified).toBeTruthy()
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const userList = JSON.parse(userListStringified!)
+    expect(userList).toEqual([NEW_TOKEN])
   })
 })
