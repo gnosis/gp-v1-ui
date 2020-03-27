a2aa5469cb265d06e79ef5eacea0b835603ab42e
Velenir <velenir@gmail.com>
2020-03-27 13:39:49 +0300

    stable ref for empty array

--- src/hooks/useTokeList.ts
+++ src/hooks/useTokeList.ts
@@ -3,11 +3,15 @@ import { tokenListApi } from 'api'
 import { TokenDetails } from 'types'
 import useSafeState from './useSafeState'
 
+// for stable reference
+// to avoid updates on setState([])
+const emptyArray: TokenDetails[] = []
+
 export const useTokenList = (networkId?: number): TokenDetails[] => {
-  const [tokens, setTokens] = useSafeState(networkId === undefined ? [] : tokenListApi.getTokens(networkId))
+  const [tokens, setTokens] = useSafeState(networkId === undefined ? emptyArray : tokenListApi.getTokens(networkId))
 
   useEffect(() => {
-    if (networkId === undefined) return setTokens([])
+    if (networkId === undefined) return setTokens(emptyArray)
     return tokenListApi.subscribe(setTokens)
   }, [networkId, setTokens])
 
