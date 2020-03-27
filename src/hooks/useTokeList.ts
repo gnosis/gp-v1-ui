656aa10f4a219b02d3e659c2079acf4844b4f04b
Velenir <velenir@gmail.com>
2020-03-27 13:33:41 +0300

    allow networkId undefined in useTokenList

--- src/hooks/useTokeList.ts
+++ src/hooks/useTokeList.ts
@@ -3,10 +3,11 @@ import { tokenListApi } from 'api'
 import { TokenDetails } from 'types'
 import useSafeState from './useSafeState'
 
-export const useTokenList = (networkId: number): TokenDetails[] => {
-  const [tokens, setTokens] = useSafeState(tokenListApi.getTokens(networkId))
+export const useTokenList = (networkId?: number): TokenDetails[] => {
+  const [tokens, setTokens] = useSafeState(networkId === undefined ? [] : tokenListApi.getTokens(networkId))
 
   useEffect(() => {
+    if (networkId === undefined) return setTokens([])
     return tokenListApi.subscribe(setTokens)
   }, [networkId, setTokens])
 
