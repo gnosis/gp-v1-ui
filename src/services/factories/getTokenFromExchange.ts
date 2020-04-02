e68748e4a75b1a5233c9233026637a9a440bb374
Velenir <velenir@gmail.com>
2020-04-02 14:21:13 +0300

    more comprehensive return from getTokenFromExchangeByAddress

--- src/services/factories/getTokenFromExchange.ts
+++ src/services/factories/getTokenFromExchange.ts
@@ -49,6 +49,18 @@ interface GetByAddressParams {
   tokenAddress: string
 }
 
+export const enum TokenFromExchange {
+  FROM_TOKEN_LIST = 'FROM TOKEN LIST',
+  NOT_REGISTERED_ON_CONTRACT = 'NOT REGISTERED ON CONTRACT',
+  UNREGISTERED_ERC20 = 'UNREGISTERED ERC20',
+  NOT_ERC20 = 'NOT ERC20',
+}
+
+export interface TokenFromExchangeResult {
+  token: TokenDetails | null
+  reason: TokenFromExchange
+}
+
 /**
  * Factory of getTokenFromExchangeByAddress
  * Takes as input API instances
@@ -56,16 +68,19 @@ interface GetByAddressParams {
  */
 function getTokenFromExchangeByAddressFactory(
   factoryParams: FactoryParams,
-): (params: GetByAddressParams) => Promise<TokenDetails | null> {
+): (params: GetByAddressParams) => Promise<TokenFromExchangeResult> {
   const { tokenListApi, exchangeApi } = factoryParams
 
-  return async ({ networkId, tokenAddress }: GetByAddressParams): Promise<TokenDetails | null> => {
+  return async ({ networkId, tokenAddress }: GetByAddressParams): Promise<TokenFromExchangeResult> => {
     const tokens = tokenListApi.getTokens(networkId)
 
     // Try from our current list of tokens, by address
     let token = getToken('address', tokenAddress, tokens)
     if (token) {
-      return token
+      return {
+        token,
+        reason: TokenFromExchange.FROM_TOKEN_LIST,
+      }
     }
 
     let tokenId: number
@@ -77,17 +92,34 @@ function getTokenFromExchangeByAddressFactory(
         tokenAddress,
         e,
       )
-      return null
+      return {
+        token: null,
+        reason: TokenFromExchange.NOT_REGISTERED_ON_CONTRACT,
+      }
     }
 
     // Try from our current list of tokens, by id
     token = getToken('id', tokenId, tokens)
     if (token) {
-      return { ...token, address: tokenAddress }
+      return {
+        token: { ...token, address: tokenAddress },
+        reason: TokenFromExchange.FROM_TOKEN_LIST,
+      }
     }
 
     // Not there, get it from the ERC20 contract
-    return getTokenFromErc20({ ...factoryParams, tokenAddress, tokenId, networkId })
+    token = (await getTokenFromErc20({ ...factoryParams, tokenAddress, tokenId, networkId })) || undefined
+    if (token) {
+      return {
+        token,
+        reason: TokenFromExchange.UNREGISTERED_ERC20,
+      }
+    }
+
+    return {
+      token: null,
+      reason: TokenFromExchange.NOT_ERC20,
+    }
   }
 }
 
