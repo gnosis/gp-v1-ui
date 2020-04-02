f14a0c9b21bd3517db7630ff01e2ef5ad1c80c53
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-04-02 15:55:39 -0700

    Updating unit tests

--- test/api/ExchangeApi/TokenListApiMock.test.ts
+++ test/api/ExchangeApi/TokenListApiMock.test.ts
@@ -4,6 +4,8 @@ import TokenListApiMock from 'api/tokenList/TokenListApiMock'
 import { TokenListApiImpl, TokenList } from 'api/tokenList/TokenListApi'
 import { tokenList as testTokenList } from '../../data'
 import SubscriptionBase from 'api/tokenList/Subscriptions'
+import ExchangeApiImpl from 'api/exchange/ExchangeApi'
+import Web3 from 'web3'
 
 class GenericSubscriptions<T> extends SubscriptionBase<T> {
   // expose triggerSubscriptions for testing
@@ -12,12 +14,26 @@ class GenericSubscriptions<T> extends SubscriptionBase<T> {
   }
 }
 
+// Setup to mock exchangeApi
+// Mocking web3
+jest.mock('web3')
+const web3 = new Web3()
+
+// Mocking fetchGasPrice
+const fetchGasPrice = jest.fn(async (): Promise<string | undefined> => undefined)
+
+// Mocking ExchangeApi
+jest.mock('api/exchange/ExchangeApi')
+const exchangeApi = new ExchangeApiImpl({ web3, fetchGasPrice })
+// patching getTokenIdByAddress to always return the same id
+exchangeApi.getTokenIdByAddress = async (_: any): Promise<number> => 87
+
 let instanceMock: TokenList
 let instanceReal: TokenList
 
 beforeEach(() => {
   instanceMock = new TokenListApiMock(testTokenList)
-  instanceReal = new TokenListApiImpl([Network.Mainnet, Network.Rinkeby])
+  instanceReal = new TokenListApiImpl({ networkIds: [Network.Mainnet, Network.Rinkeby], exchangeApi })
 })
 
 const NEW_TOKEN = {
@@ -64,22 +80,29 @@ describe('REAL: Basic functions', () => {
     expect(instanceReal.hasToken({ tokenAddress: tokens[0].address, networkId: 1 })).toBe(true)
   })
   test('API Token list can add tokens', () => {
-    const tokens = instanceReal.getTokens(1)
+    // patching ids, updated async on first getTokens invocation
+    const tokens = instanceReal.getTokens(1).map(t => {
+      t.id = 87
+      return t
+    })
+
     instanceReal.addToken({
       token: NEW_TOKEN,
       networkId: 1,
     })
 
+    const newListOfTokens = tokens.concat(NEW_TOKEN)
+
     expect(instanceReal.hasToken({ tokenAddress: NEW_TOKEN.address, networkId: 1 })).toBe(true)
     expect(instanceReal.getTokens(1)).toHaveLength(tokens.length + 1)
-    expect(instanceReal.getTokens(1)).toEqual(tokens.concat(NEW_TOKEN))
+    expect(instanceReal.getTokens(1)).toEqual(newListOfTokens)
 
     const userListStringified = localStorage.getItem('USER_TOKEN_LIST_1')
     expect(userListStringified).toBeTruthy()
 
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const userList = JSON.parse(userListStringified!)
-    expect(userList).toEqual([NEW_TOKEN])
+    expect(userList).toEqual(newListOfTokens)
   })
 })
 
