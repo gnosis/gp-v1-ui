058a069ee7964cb7154b557cf25fd7388b5e7edf
Michel <31534717+biocom@users.noreply.github.com>
2020-04-09 13:39:35 +0200

    Order book: Touch up design / modal size. (#856)
    * Touch up design / modal size.
    
    * Mobile fixes.
    
    * Mobile fixes.

--- src/components/OrderBookBtn.tsx
+++ src/components/OrderBookBtn.tsx
@@ -9,8 +9,69 @@ import { safeTokenName } from '@gnosis.pm/dex-js'
 import TokenSelector from './TokenSelector'
 import useSafeState from 'hooks/useSafeState'
 import { useTokenList } from 'hooks/useTokenList'
+import { MEDIA } from 'const'
 
-const ModalWrapper = styled(ModalBodyWrapper)``
+const ModalWrapper = styled(ModalBodyWrapper)`
+  display: flex;
+  /* min-height: 40rem; */
+  /* height: calc(100vh - 30rem); */
+  text-align: center;
+  height: 100%;
+  min-width: 100%;
+  width: 100%;
+  align-items: center;
+  align-content: flex-start;
+  flex-flow: row wrap;
+  padding: 0;
+  justify-content: center;
+  
+  /* @media ${MEDIA.tabletPortrait} {
+    min-height:
+  } */
+
+  > span {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    margin: 1.6rem 0 1rem;
+  }
+
+  > span:first-of-type::after {
+    content: '⟶';
+    margin: 0 1rem;
+
+    @media ${MEDIA.mobile} {
+      display: none;
+    }
+  }
+
+  > span:first-of-type > p {
+    margin: 0 1rem 0 0;
+  }
+
+  > span:last-of-type > p {
+    margin: 0 0 0 1rem;
+  }
+
+  .amcharts-Sprite-group {
+    font-size: 1rem;
+  }
+
+  .amcharts-Container .amcharts-Label {
+    text-transform: uppercase;
+    font-size: 1.2rem;
+  }
+
+  .amcharts-ZoomOutButton-group > .amcharts-RoundedRectangle-group {
+    fill: var(--color-text-active);
+    opacity: 0.6;
+    transition: 0.3s ease-in-out;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`
 
 interface OrderBookBtnProps {
   baseToken: TokenDetails
@@ -32,8 +93,12 @@ export const OrderBookBtn: React.FC<OrderBookBtnProps> = (props: OrderBookBtnPro
     title: `${safeTokenName(baseToken)}-${safeTokenName(quoteToken)} Order book`,
     message: (
       <ModalWrapper>
-        <TokenSelector tokens={tokenList} selected={baseToken} onChange={setBaseToken} />
-        <TokenSelector tokens={tokenList} selected={quoteToken} onChange={setQuoteToken} />
+        <span>
+          <p>Bid</p> <TokenSelector tokens={tokenList} selected={baseToken} onChange={setBaseToken} />
+        </span>
+        <span>
+          <TokenSelector tokens={tokenList} selected={quoteToken} onChange={setQuoteToken} /> <p>Ask</p>
+        </span>
         <OrderBookWidget baseToken={baseToken} quoteToken={quoteToken} networkId={networkId} />
       </ModalWrapper>
     ),
