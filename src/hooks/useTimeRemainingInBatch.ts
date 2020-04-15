8fdb80fadbf8566f57dac74cdd569b209dd97482
Velenir <velenir@gmail.com>
2020-04-15 14:41:27 +0300

    add useCheckWhenTimeRemainingInBatch hook

--- src/hooks/useTimeRemainingInBatch.ts
+++ src/hooks/useTimeRemainingInBatch.ts
@@ -1,6 +1,7 @@
 import useSafeState from './useSafeState'
 import { getSecondsRemainingInBatch } from 'utils'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+import { BATCH_TIME } from 'const'
 
 export function useTimeRemainingInBatch(): number {
   const [timeRemaining, setTimeRemaining] = useSafeState(getSecondsRemainingInBatch())
@@ -12,4 +13,76 @@ export function useTimeRemainingInBatch(): number {
   }, [setTimeRemaining])
 
   return timeRemaining
+}
+
+interface SecondsRemainingResult {
+  secondsRemaining: number
+  checkTime: boolean
+}
+
+const checkIfTime = (seconds: number): SecondsRemainingResult => {
+  const secondsRemaining = getSecondsRemainingInBatch()
+  console.log('CHECK:secondsRemaining', secondsRemaining)
+  return {
+    secondsRemaining,
+    checkTime: secondsRemaining < seconds,
+  }
+}
+
+export function useCheckWhenTimeRemainingInBatch(
+  seconds: number,
+  callback: (SResult: SecondsRemainingResult) => void,
+): void {
+  // allow for fluid callback change without resetting interval
+  const callbackRef = useRef(callback)
+  callbackRef.current = callback
+
+  useEffect(() => {
+    // how often to check
+    // when there are ${seconds} seconds left in the batch
+    const checkIntervalSeconds = BATCH_TIME - seconds
+    if (checkIntervalSeconds <= 0) {
+      console.warn(`seconds given ${seconds} > BATCH_TIME ${BATCH_TIME}`)
+      return
+    }
+
+    const currentCheck = checkIfTime(seconds)
+    let id: number
+
+    if (currentCheck.checkTime) {
+      // setCheckResult(currentCheck)
+      callbackRef.current(currentCheck)
+    }
+
+    console.log('CHECK:checkIntervalSeconds', checkIntervalSeconds)
+
+    // const nextCheckInSeconds = currentCheck.secondsRemaining + checkIntervalSeconds
+    // first check on timeout not interval
+    // as we start at random time in the BATCH
+    const nextCheckInSeconds =
+      (currentCheck.checkTime
+        ? //  if currently past check time -- interval + seconds remaining in BATCH
+          currentCheck.secondsRemaining + checkIntervalSeconds
+        : // if not yet check time -- what's left to that time
+          currentCheck.secondsRemaining - seconds) + 1 // plus one second
+    // because condition in contract is >= 1 minutes and we check for <
+    // so we don't fall on the exact second and query blockchain
+    console.log('CHECK:check again in', nextCheckInSeconds)
+
+    const checkAndReport = (): void => {
+      const currentCheck = checkIfTime(seconds)
+      callbackRef.current(currentCheck)
+    }
+
+    id = window.setTimeout(() => {
+      // not can go on interval
+      id = window.setInterval(checkAndReport, checkIntervalSeconds * 1000)
+
+      checkAndReport()
+    }, nextCheckInSeconds * 1000)
+
+    return (): void => {
+      clearInterval(id)
+    }
+  }, [seconds])
 }
