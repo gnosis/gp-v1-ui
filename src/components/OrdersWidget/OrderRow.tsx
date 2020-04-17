d507dfbfe9e7a6e7faa2d7a6ba49036d48aeaca4
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-04-17 13:17:02 -0700

    Renamed `Looking for match` to `Waiting for settlement`

--- src/components/OrdersWidget/OrderRow.tsx
+++ src/components/OrdersWidget/OrderRow.tsx
@@ -207,7 +207,7 @@ const Status: React.FC<Pick<Props, 'order' | 'isOverBalance' | 'transactionHash'
       ) : isActiveNextBatch ? (
         `Active in next batch: ${secondsCountdown}`
       ) : isFirstActiveBatch ? (
-        `Looking for match: ${secondsCountdown}`
+        `Waiting for settlement: ${secondsCountdown}`
       ) : isScheduled ? (
         <>
           Scheduled
