db72d3bd55eb76bd49a7add480a5e00253c6c51a
Anxo Rodriguez <anxolin@gmail.com>
2020-05-05 20:39:53 +0200

    Add config loader

---
+++ src/loadConfig.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const fs = require('fs')
+const YAML = require('yaml')
+
+const CONFIG_FILE_OVERRIDE = 'custom/config.yaml'
+const CONFIG_FILE = 'config.yaml'
+
+function parseJsonOrYaml(filePath) {
+  const content = fs.readFileSync(filePath, 'utf-8')
+  if (filePath.endsWith('.json')) {
+    return JSON.parse(content)
+  } else if (filePath.endsWith('.yaml')) {
+    return YAML.parse(content)
+  } else {
+    throw new Error('Unknown file extension. Supported JSON or YAML: ' + filePath)
+  }
+}
+
+function loadConfig() {
+  const configPath = path.resolve(CONFIG_FILE)
+  let config = parseJsonOrYaml(configPath)
+  const configOverridePath = path.resolve(CONFIG_FILE_OVERRIDE)
+  if (fs.existsSync(configOverridePath)) {
+    const configOverride = parseJsonOrYaml(configOverridePath)
+    config = { ...config, ...configOverride }
+  } else {
+    console.warn('Using default config from %s. If you want to override, use %s', configPath, configOverridePath)
+  }
+
+  return config
+}
+
+module.exports = loadConfig
