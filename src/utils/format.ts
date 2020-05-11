5264979d8942a23306f2816f7963cb127a31a045
David Sato <david.sato64@gmail.com>
2020-05-11 12:35:50 +0200

    Utils: wrap formatAmount in gnosis safe impl

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
-
+import { formatAmount as formatAmountDexJS } from '@gnosis.pm/dex-js'
+import BN from 'bn.js'
 export {
-  formatAmount,
   formatAmountFull,
   adjustPrecision,
   parseAmount,
@@ -98,3 +98,53 @@ export const formatTimeToFromBatch = (
   returnType: 'TIME' | 'BATCH' = 'TIME',
   batchLength = 5,
 ): number => (returnType === 'TIME' ? Number(value) * batchLength : Number(value) / batchLength)
+
+// TODO: move
+// Locale is an empty array because we want it to use user's locale
+const lt1kFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 5 })
+const lt10kFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 4 })
+const lt100kFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 3 })
+const lt1mFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 2 })
+const lt10mFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 1 })
+const lt100mFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 0 })
+// same format for billions and trillions
+const lt1000tFormatter = new Intl.NumberFormat([], { maximumFractionDigits: 3, notation: 'compact' })
+
+export const formatAmountForDisplay = (number: string): string => {
+  let numberFloat: number | string = parseFloat(number)
+
+  if (numberFloat === 0) {
+    numberFloat = '0.000'
+  } else if (numberFloat < 0.001) {
+    numberFloat = '< 0.001'
+  } else if (numberFloat < 1000) {
+    numberFloat = lt1kFormatter.format(numberFloat)
+  } else if (numberFloat < 10000) {
+    numberFloat = lt10kFormatter.format(numberFloat)
+  } else if (numberFloat < 100000) {
+    numberFloat = lt100kFormatter.format(numberFloat)
+  } else if (numberFloat < 1000000) {
+    numberFloat = lt1mFormatter.format(numberFloat)
+  } else if (numberFloat < 10000000) {
+    numberFloat = lt10mFormatter.format(numberFloat)
+  } else if (numberFloat < 100000000) {
+    numberFloat = lt100mFormatter.format(numberFloat)
+  } else if (numberFloat < 10 ** 15) {
+    numberFloat = lt1000tFormatter.format(numberFloat)
+  } else {
+    numberFloat = '> 1000T'
+  }
+
+  return numberFloat
+}
+
+export const formatAmount = (amount: BN, amountPrecision: number): string => {
+  const stringAmount = formatAmountDexJS({
+    amount,
+    precision: amountPrecision,
+    isLocaleAware: false,
+    thousandSeparator: false,
+  })
+
+  return formatAmountForDisplay(stringAmount)
+}
