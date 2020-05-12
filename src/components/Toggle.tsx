1603b3b4c0282edeff1d946260278032b0e1a720
Velenir <velenir@gmail.com>
2020-05-12 16:11:39 +0300

    parametrize Toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ToggleBase: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className, ...props }) => {
+interface ToggleSize {
+  width: string
+  height: string
+}
+
+type ToggleProps = React.InputHTMLAttributes<HTMLInputElement> & Partial<ToggleSize>
+
+// get rid of width, height
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ToggleBase: React.FC<ToggleProps> = ({ className, width, height, ...props }) => {
   return (
     <label className={className}>
       <input type="checkbox" {...props} hidden />
@@ -10,14 +19,20 @@ const ToggleBase: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ cla
   )
 }
 
-const ToggleStyled = styled(ToggleBase)`
+const defaultSize: ToggleSize = { width: '2.8em', height: '1em' }
+
+// don't pass on theme object, it'll get spread over input anyway
+const ToggleStyled = styled(ToggleBase).attrs(props => ({ ...defaultSize, ...props, theme: undefined }))<ToggleSize>`
+  --span-width: ${(props): string => props.width};
+  --span-height: ${(props): string => props.height};
+  /* easier to set vers once, then copy the whole func into each calc() */
   padding: 1em;
   cursor: pointer;
 
   span {
     background-color: #e0e6ee;
-    width: 2.8em;
-    height: 1em;
+    width: var(--span-width);
+    height: var(--span-height);
     display: flex;
     align-items: center;
     border-radius: 0.45em;
@@ -25,14 +40,15 @@ const ToggleStyled = styled(ToggleBase)`
   }
 
   span::after {
+    --check-size: calc(var(--span-height) * 1.3);
     content: '';
     border: 1px solid #cbcbcb;
-    width: 1.3em;
-    height: 1.3em;
+    width: var(--check-size);
+    height: var(--check-size);
     border-radius: 50%;
     background-color: #fff;
     box-shadow: 1px 1px 3px 1px grey;
-    transform: translateX(calc(-1.3em / 2));
+    transform: translateX(-50%);
     transition: transform 0.3s;
   }
 
@@ -43,7 +59,7 @@ const ToggleStyled = styled(ToggleBase)`
   input:checked + span {
     background-color: #428ef7;
     &::after {
-      transform: translateX(calc(100% + 1.3em / 2));
+      transform: translateX(calc(var(--span-width) - 50%));
     }
   }
 `
