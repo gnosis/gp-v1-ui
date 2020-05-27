38f81d8107de129deafb218524bc39b13a131ffc
Leandro Boscariol <leandro.boscariol@gnosis.io>
2020-05-27 14:56:06 -0700

    Refreshing TradeRow and updating component to remove countdown when done

--- src/components/TradesWidget/TradeRow.tsx
+++ src/components/TradesWidget/TradeRow.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 import { formatPrice, formatSmart } from '@gnosis.pm/dex-js'
-import { EtherscanLink } from 'components/EtherscanLink'
 
 import { Trade } from 'api/exchange/ExchangeApi'
 
-import { isTradeFilled } from 'utils'
+import { EtherscanLink } from 'components/EtherscanLink'
 import { StatusCountdown } from 'components/StatusCountdown'
+
+import { isTradeFilled } from 'utils'
 import { displayTokenSymbolOrLink } from 'utils/display'
+
+import useSafeState from 'hooks/useSafeState'
 
 function classifyTrade(trade: Trade): string {
   return isTradeFilled(trade) ? 'Full' : 'Partial'
@@ -24,6 +27,28 @@ interface TradeRowProps {
 
 export const TradeRow: React.FC<TradeRowProps> = params => {
   const { trade, networkId, now } = params
+
+  // Dima's trick to force component update
+  const [, forceUpdate] = useSafeState({})
+
+  const refreshTimeout = useRef<null | NodeJS.Timeout>(null)
+
+  // Refresh component when countdown reaches 0, if any
+  useEffect(() => {
+    function clear(): void {
+      if (refreshTimeout.current) clearTimeout(refreshTimeout.current)
+      refreshTimeout.current = null
+    }
+
+    clear()
+
+    if (trade.settlingDate > now) {
+      const timeout = trade.settlingDate.getTime() - now.getTime()
+      refreshTimeout.current = setTimeout(() => forceUpdate({}), timeout)
+    }
+
+    return clear
+  }, [forceUpdate, now, trade.settlingDate])
 
   return (
     <tr key={trade.id} data-order-id={trade.orderId} data-batch-id={trade.batchId}>
@@ -45,10 +70,7 @@ export const TradeRow: React.FC<TradeRowProps> = params => {
       <td>
         <EtherscanLink type={'event'} identifier={trade.txHash} networkId={networkId} />
       </td>
-      <td>
-        {/* TODO: refresh component when countdown reaches 0 */}
-        {trade.settlingDate > now ? <StatusCountdown timeoutDelta={60} /> : <FontAwesomeIcon icon={faCheck} />}
-      </td>
+      <td>{trade.settlingDate > now ? <StatusCountdown timeoutDelta={60} /> : <FontAwesomeIcon icon={faCheck} />}</td>
     </tr>
   )
 }
