41b66f3555bd9f4a51dcabec79a90b5599fc04ea
David Sato <david.sato64@gmail.com>
2020-05-28 11:20:07 +0200

    Card: removed dead code + added svg css

--- src/components/Layout/Card/Card.tsx
+++ src/components/Layout/Card/Card.tsx
@@ -149,8 +149,6 @@ export const CardTable = styled.table<{
     > tr:not(${CardRowDrawer}) {
       position: relative;
       display: grid;
-      // grid-template-columns: ${({ $columns }): string => $columns || `repeat(auto-fit, minmax(3rem, 1fr))`};
-      // grid-template-columns: minmax(2rem,.4fr) minmax(7rem,16rem) minmax(4rem,11rem) minmax(5rem, 7.5rem) minmax(3rem,9rem);
       grid-template-columns: 3.2rem 1fr 1fr minmax(3rem,8rem) minmax(5rem,9rem);
       // grid-template-rows
       ${({ $rows }): string => ($rows ? `grid-template-rows: ${$rows};` : '')}
@@ -208,6 +206,14 @@ export const CardTable = styled.table<{
         line-height: 1;
         font-size: 1.1rem;
         padding: 1.3rem 0;
+
+        &.sortable {
+          cursor: pointer;
+        }
+
+        > svg {
+          margin: 0 0 0.04rem 0.2rem;
+        }
       }
       
       > th.filled {
