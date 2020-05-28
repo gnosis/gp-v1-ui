2a2863fc263d7d1fdf429f79d4642da3e3000181
Velenir <velenir@gmail.com>
2020-05-28 17:38:38 +0300

    format trades as CSV

--- src/components/TradesWidget/index.tsx
+++ src/components/TradesWidget/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { ContentPage } from 'components/Layout/PageWrapper'
 import { CardTable } from 'components/Layout/Card'
@@ -6,26 +6,66 @@ import { CardTable } from 'components/Layout/Card'
 import { useWalletConnection } from 'hooks/useWalletConnection'
 import { useTrades } from 'hooks/useTrades'
 
-import { TradeRow } from 'components/TradesWidget/TradeRow'
+import { TradeRow, classifyTrade } from 'components/TradesWidget/TradeRow'
+
+import { formatPrice, formatSmart } from '@gnosis.pm/dex-js'
+
+import { Trade } from 'api/exchange/ExchangeApi'
+
+import { displayTokenSymbolOrLink } from 'utils/display'
+import { FileDownloaderLink } from 'components/FileDownloaderLink'
+
+const formatTradeAsCSV = (trade: Trade, now: Date): string => {
+  return [
+    displayTokenSymbolOrLink(trade.sellToken) + '/' + displayTokenSymbolOrLink(trade.buyToken),
+    formatSmart({ amount: trade.sellAmount, precision: trade.sellToken.decimals as number }) +
+      ' ' +
+      displayTokenSymbolOrLink(trade.sellToken),
+    formatPrice(trade.limitPrice),
+    formatPrice(trade.fillPrice),
+    formatSmart({ amount: trade.buyAmount, precision: trade.buyToken.decimals as number }) +
+      ' ' +
+      displayTokenSymbolOrLink(trade.buyToken),
+    classifyTrade(trade),
+    new Date(trade.timestamp).toLocaleString(),
+    trade.txHash,
+    trade.settlingDate > now ? 'NOT SETTLED' : 'SETTLED',
+  ]
+    .map(value => {
+      // " is a string delimeter
+      // if already included in e.g. symbol
+      // must be replaced by double ""
+      value = value.replace(/"/g, '""')
+
+      // if there's a field delimeter comma in e.g. symbol or date
+      // need to enclose whole string in quotes
+      if (value.includes(',')) value = `"${value}"`
+      return value
+    })
+    .join(',')
+}
+
+const tableHeads = ['Market', 'Amount', 'Limit Price', 'Fill Price', 'Received', 'Type', 'Time', 'Tx', 'Settled']
+const tableHeader = tableHeads.join(',') + '\n'
 
 const Trades: React.FC = () => {
   const { networkId } = useWalletConnection()
   const trades = useTrades()
 
+  const csvString = useMemo(() => {
+    const now = new Date()
+    return tableHeader + trades.map(trade => formatTradeAsCSV(trade, now)).join('\n')
+  }, [trades])
+
   return (
     <ContentPage>
+      {trades.length > 0 && <FileDownloaderLink data={csvString} options={{ type: 'text/csv;charset=utf-8;' }} />}
       <CardTable $columns="1fr 1.2fr repeat(2, 0.8fr) 1.2fr 0.7fr 0.8fr 1fr 0.5fr" $rowSeparation="0">
         <thead>
           <tr>
-            <th>Market</th>
-            <th>Amount</th>
-            <th>Limit Price</th>
-            <th>Fill Price</th>
-            <th>Received</th>
-            <th>Type</th>
-            <th>Time</th>
-            <th>Tx</th>
-            <th>Settled</th>
+            {tableHeads.map(head => (
+              <th key={head}>{head}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
