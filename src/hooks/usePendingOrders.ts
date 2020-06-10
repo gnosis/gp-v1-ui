8c0a94ea7d311142af9158a7ccc48c132effdb9e
David Sato <david.sato64@gmail.com>
2020-06-10 10:36:13 +0200

    get detailed info from pendingOrders hook

--- src/hooks/usePendingOrders.ts
+++ src/hooks/usePendingOrders.ts
@@ -3,18 +3,34 @@ import { useEffect } from 'react'
 import useGlobalState from './useGlobalState'
 import useSafeState from './useSafeState'
 import { useWalletConnection } from './useWalletConnection'
-import { PendingTxObj } from 'api/exchange/ExchangeApi'
+import { DetailedAuctionElement } from 'api/exchange/ExchangeApi'
+import { getTokenFromExchangeById } from 'services'
 
-function usePendingOrders(): PendingTxObj[] {
+export interface DetailedPendingOrder extends DetailedAuctionElement {
+  txHash?: string
+}
+
+function usePendingOrders(): DetailedPendingOrder[] {
   const { userAddress, networkId } = useWalletConnection()
 
   const [{ pendingOrders: pendingOrdersGlobal }] = useGlobalState()
-  const [pendingOrders, setPendingOrders] = useSafeState<PendingTxObj[]>([])
+  const [pendingOrders, setPendingOrders] = useSafeState<DetailedPendingOrder[]>([])
 
   useEffect(() => {
-    if (userAddress && networkId) {
-      setPendingOrders(pendingOrdersGlobal[networkId][userAddress] || [])
+    async function getDetailedPendingOrders(): Promise<void> {
+      if (userAddress && networkId) {
+        const pendingOrdersByNetwork = pendingOrdersGlobal[networkId][userAddress] || []
+        const ordersPromises = pendingOrdersByNetwork.map(async order => ({
+          ...order,
+          sellToken: await getTokenFromExchangeById({ tokenId: order.sellTokenId, networkId }),
+          buyToken: await getTokenFromExchangeById({ tokenId: order.buyTokenId, networkId }),
+        }))
+
+        const orders: DetailedPendingOrder[] = await Promise.all(ordersPromises)
+        setPendingOrders(orders)
+      }
     }
+    getDetailedPendingOrders()
   }, [networkId, pendingOrdersGlobal, setPendingOrders, userAddress])
   return pendingOrders
 }
