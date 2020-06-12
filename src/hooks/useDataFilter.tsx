382a8def1dbca5af45428b8d59731d71d1fa4410
David Sato <david.sato64@gmail.com>
2020-06-12 11:26:34 +0200

    useDataFilter: PR comments addressed
    1. memoised all the things (necessary)
    2. export handlers obj
    3. logic opt - cleaner readability

--- src/hooks/useDataFilter.tsx
+++ src/hooks/useDataFilter.tsx
@@ -1,64 +1,77 @@
 import { useMemo, useState } from 'react'
 import { useDebounce } from './useDebounce'
 
+interface InternalState<T> {
+  debouncedSearch?: string
+  showFilter?: boolean
+  data?: T[]
+}
+
 interface HookResults<T> {
   filteredData: T[]
   search: string
   showFilter: boolean
-  handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void
-  handleToggleFilter: (e: React.ChangeEvent<HTMLInputElement>) => void
-  clearFilters: () => void
+  handlers: {
+    handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void
+    handleToggleFilter: (e: React.ChangeEvent<HTMLInputElement>) => void
+    clearFilters: () => void
+  }
 }
 
 export interface HookParams<T> {
   data: T[]
   searchDebounceTime?: number
   isSearchFilter?: boolean
-  customStopCheck?: (...any: unknown[]) => boolean
+  userConditionalCheck?: (internalState?: InternalState<T>) => boolean
   filterFnFactory: (searchTxt: string) => (params: T, index: number, array: T[]) => boolean
 }
 
 function useDataFilter<T>({
   data,
-  searchDebounceTime = 500,
   isSearchFilter = true,
-  customStopCheck,
+  searchDebounceTime = 500,
   filterFnFactory,
+  userConditionalCheck,
 }: HookParams<T>): HookResults<T> {
   const [search, setSearch] = useState('')
   // searchFilter prop turns on/off by default. If using search filter, we dont want to use toggle
   const [showFilter, setShowFilter] = useState(isSearchFilter)
   const { value: debouncedSearch, setImmediate: setDebouncedSearch } = useDebounce(search, searchDebounceTime)
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => setSearch(e.target.value)
-  const handleToggleFilter = (): void => setShowFilter(state => !state)
-  const clearFilters = (): void => {
-    setSearch('')
-    setDebouncedSearch('')
-    setShowFilter(false)
-  }
+  const handlers = useMemo(() => {
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => setSearch(e.target.value)
+    const handleToggleFilter = (): void => setShowFilter(state => !state)
+    const clearFilters = (): void => {
+      setSearch('')
+      setDebouncedSearch('')
+      setShowFilter(false)
+    }
+
+    return {
+      handleSearch,
+      handleToggleFilter,
+      clearFilters,
+    }
+  }, [setDebouncedSearch])
 
   const filteredData = useMemo(() => {
-    // showFilter: if we want to hide zero balances for example
-    // debouncedSearch or custom conditional check is used here
-    // no data
-    if (!showFilter || (!debouncedSearch && customStopCheck && customStopCheck()) || !data || data.length === 0)
-      return data
+    const failsBasicSearchReq = !showFilter || (isSearchFilter && !debouncedSearch) || !data || data.length === 0
+    // pass internal state to user's userConditionalCheck fn for use outside
+    const failsUserReq = userConditionalCheck && userConditionalCheck({ debouncedSearch, data, showFilter })
+
+    if (failsBasicSearchReq || failsUserReq) return data
 
     const searchTxt = debouncedSearch.trim().toLowerCase()
-
     const customFilterFn = filterFnFactory(searchTxt)
 
     return data.filter(customFilterFn)
-  }, [debouncedSearch, data, showFilter, customStopCheck, filterFnFactory])
+  }, [debouncedSearch, data, isSearchFilter, showFilter, userConditionalCheck, filterFnFactory])
 
   return {
     filteredData,
     search,
     showFilter,
-    handleSearch,
-    handleToggleFilter,
-    clearFilters,
+    handlers,
   }
 }
 
