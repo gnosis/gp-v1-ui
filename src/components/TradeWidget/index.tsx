48589375211d7b3abe10bae3977f90bd8c712cf3
Velenir <velenir@gmail.com>
2020-06-22 13:01:08 +0300

    fix receiveAmount not updating on priceInverse change

--- src/components/TradeWidget/index.tsx
+++ src/components/TradeWidget/index.tsx
@@ -636,7 +636,8 @@ const TradeWidget: React.FC = () => {
         setValue(priceInputId, newPrice)
         setValue(priceInverseInputId, invertPriceFromString(newPrice))
 
-        setValue(receiveInputId, calculateReceiveAmount(priceValue, sellValue))
+        // now that we don't register priceValue, we don't have it inside FormContext
+        setValue(receiveInputId, calculateReceiveAmount(invertPriceFromString(priceInverseValue), sellValue))
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -644,7 +645,7 @@ const TradeWidget: React.FC = () => {
 
   // Update receive amount
   useEffect(() => {
-    setValue(receiveInputId, calculateReceiveAmount(priceValue, sellValue))
+    setValue(receiveInputId, calculateReceiveAmount(invertPriceFromString(priceInverseValue), sellValue))
   }, [priceValue, priceInverseValue, setValue, receiveInputId, sellValue])
 
   const url = buildUrl({
