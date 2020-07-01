6102872c9e1e79d0e1bc706293a975369bdac9fa
Velenir <velenir@gmail.com>
2020-07-01 12:19:07 +0300

    simplify excluding deprecated in useTokenList

--- src/hooks/useTokenList.ts
+++ src/hooks/useTokenList.ts
@@ -1,29 +1,26 @@
 import { useEffect, useMemo } from 'react'
-import { TokenDetails, Network } from 'types'
+import { TokenDetails } from 'types'
 import useSafeState from './useSafeState'
 import { getTokens, subscribeToTokenList } from 'services'
-import { DISABLED_TOKEN_MAPS } from 'const'
-import { AddressToOverrideMap } from 'types/config'
 
 // for stable reference
 // to avoid updates on setState([])
 const emptyArray: TokenDetails[] = []
-const emptyObject: AddressToOverrideMap = {}
 
-interface UseTokenListResult {
-  tokenList: TokenDetails[]
-  disabledTokensMap: AddressToOverrideMap
+export interface UseTokenListParams {
+  networkId?: number
+  excludeDeprecated?: boolean
 }
 
-export const useTokenList = (networkId?: number, excludeDisabled?: boolean): UseTokenListResult => {
+export const useTokenList = ({ networkId, excludeDeprecated }: UseTokenListParams = {}): TokenDetails[] => {
   // sync get tokenList
   const unfilteredTokens = networkId === undefined ? emptyArray : getTokens(networkId)
-  const disabledTokensMap = DISABLED_TOKEN_MAPS[networkId || Network.Mainnet] || emptyObject
 
   const tokens = useMemo(() => {
-    if (!excludeDisabled) return unfilteredTokens
-    return unfilteredTokens.filter(token => !disabledTokensMap[token.address])
-  }, [disabledTokensMap, excludeDisabled, unfilteredTokens])
+    if (!excludeDeprecated) return unfilteredTokens
+
+    return unfilteredTokens.filter(token => token.overrideReason !== 'DEPRECATED')
+  }, [excludeDeprecated, unfilteredTokens])
 
   // force update with a new value each time
   const [, forceUpdate] = useSafeState({})
@@ -32,5 +29,5 @@ export const useTokenList = (networkId?: number, excludeDisabled?: boolean): Use
     return subscribeToTokenList(() => forceUpdate({}))
   }, [forceUpdate])
 
-  return { tokenList: tokens, disabledTokensMap }
+  return tokens
 }
