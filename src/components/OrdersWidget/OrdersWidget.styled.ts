1a51265df817e267957f4174a71dc39f3eadfd8f
Anxo Rodriguez <anxolin@gmail.com>
2020-07-08 17:56:00 +0200

    Remove fixed height that breaks IOS (#1209)
    * Remove fixed height that breaks IOS
    
    * Do not use fixed height in mobile

--- src/components/OrdersWidget/OrdersWidget.styled.ts
+++ src/components/OrdersWidget/OrdersWidget.styled.ts
@@ -90,10 +90,13 @@ export const OrdersForm = styled.div`
   > form {
     display: flex;
     flex-flow: column nowrap;
-    height: 59rem;
+    height: 71rem;
 
     @media ${MEDIA.tablet} {
-      height: auto;
+      height: inherit;
+    }
+    @media ${MEDIA.mobile} {
+      height: inherit;
     }
   }
 
@@ -220,6 +223,10 @@ export const OrdersForm = styled.div`
     padding: 0 0 5rem;
     box-sizing: border-box;
     overflow-y: auto;
+
+    @media ${MEDIA.tablet} {
+      overflow-y: none;
+    }
   }
 
   .checked {
