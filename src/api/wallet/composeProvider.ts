c11e26f2c7ffbc9529218ead0ecadad0a31a9f9c
Velenir <velenir@gmail.com>
2020-07-16 15:44:35 +0300

    add LoggerMiddleware

--- src/api/wallet/composeProvider.ts
+++ src/api/wallet/composeProvider.ts
@@ -11,6 +11,7 @@ import { numberToHex } from 'web3-utils'
 import { isWalletConnectProvider, Provider } from './providerUtils'
 import { logDebug } from 'utils'
 import { web3 } from 'api'
+import { createLoggerMiddleware } from './loggerMiddleware'
 
 // custom providerAsMiddleware
 function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
@@ -95,6 +96,11 @@ export const composeProvider = <T extends Provider>(
 ): T => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const engine = new (RpcEngine as any)() as JsonRpcEngine
+
+  if (process.env.NODE_ENV === 'development') {
+    // Logger middleware
+    engine.push(createLoggerMiddleware())
+  }
 
   engine.push(
     createConditionalMiddleware<[]>(
---
+++ src/api/wallet/loggerMiddleware.ts
@@ -0,0 +1,24 @@
+import { JsonRpcMiddleware } from 'json-rpc-engine'
+
+export const createLoggerMiddleware = (): JsonRpcMiddleware => {
+  let logRequestResponse = true
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ;(window as any).logMiddleware = (enableLogging: boolean): void => {
+    logRequestResponse = enableLogging
+  }
+
+  // Logger middleware
+  return (req, res, next): void => {
+    if (!logRequestResponse) return next()
+    console.log('CPROV::Logger middleware')
+    console.log('CPROV::req', req)
+    console.log('CPROV::res', res)
+
+    next(done => {
+      // triggered at the end of all middlewares
+      console.log('CPROV::FINAL res', res)
+      done()
+    })
+  }
+}
