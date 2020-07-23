304d0c4694774e64abcd8a61c33a713036c0603a
Leandro Boscariol <alfetopito@users.noreply.github.com>
2020-07-23 09:35:18 -0700

    Resetting prices when token selection changes (#1255)
    Co-authored-by: Leandro Boscariol <leandro.boscariol@gnosis.io>

--- src/components/TradeWidget/index.tsx
+++ src/components/TradeWidget/index.tsx
@@ -669,10 +669,17 @@ const TradeWidget: React.FC = () => {
 
   const { placeOrder, placeMultipleOrders, isSubmitting, setIsSubmitting } = usePlaceOrder()
 
+  const resetPrices = useCallback((): void => {
+    setValue(priceInputId, '0')
+    setValue(priceInverseInputId, '0')
+  }, [setValue])
+
   const swapTokens = useCallback((): void => {
     setSellToken(receiveTokenBalance)
     setReceiveToken(sellTokenBalance)
-  }, [receiveTokenBalance, sellTokenBalance])
+    // selected price no longer has meaning, reset and force user pick/insert new one
+    resetPrices()
+  }, [receiveTokenBalance, resetPrices, sellTokenBalance])
 
   const onSelectChangeFactory = useCallback(
     (
@@ -684,10 +691,12 @@ const TradeWidget: React.FC = () => {
           swapTokens()
         } else {
           setToken(selected)
+          // selected price no longer has meaning, reset and force user pick/insert new one
+          resetPrices()
         }
       }
     },
-    [swapTokens],
+    [swapTokens, resetPrices],
   )
 
   const sameToken = sellToken === receiveToken
