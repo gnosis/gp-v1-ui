f095ccfa523c1c92ec45713b87f39823cc735eb0
Velenir <velenir@gmail.com>
2020-08-04 17:37:33 +0300

    use ethPriceInOwl in calcMinTradableAmountInOwl

--- src/utils/minFee.ts
+++ src/utils/minFee.ts
@@ -1,7 +1,6 @@
 import { BigNumber } from 'bignumber.js'
 
 export const DEFAULT_GAS_PRICE = 40e9 // 40 Gwei
-export const ETH_PRICE_IN_OWL = 240 * 1000
 export const SUBSIDIZE_FACTOR = 10
 
 // account for a 20% change in the time it takes to mine the tx, and start the batch
@@ -11,9 +10,22 @@ export const SETTLEMENT_FACTOR = 1.5
 export const FEE_FACTOR = 1000
 export const TRADE_TX_GASLIMIT = 120000
 
-export const calcMinTradableAmountInOwl = (gasPrice: number): BigNumber => {
-  const MIN_ECONOMICAL_VIABLE_FEE_IN_OWL = TRADE_TX_GASLIMIT * gasPrice * ETH_PRICE_IN_OWL
+const OWL_DECIMAL_UNITS = 1e18
 
-  const MIN_FEE = (BUFFER_MULTIPLIER * MIN_ECONOMICAL_VIABLE_FEE_IN_OWL) / SUBSIDIZE_FACTOR
-  return new BigNumber(MIN_FEE * FEE_FACTOR * SETTLEMENT_FACTOR)
+interface CalcMinTradableAmountInOwlParams {
+  gasPrice: number
+  ethPriceInOwl: BigNumber
+}
+
+export const calcMinTradableAmountInOwl = ({
+  gasPrice,
+  ethPriceInOwl,
+}: CalcMinTradableAmountInOwlParams): BigNumber => {
+  const MIN_ECONOMICAL_VIABLE_FEE_IN_OWL = ethPriceInOwl
+    .multipliedBy(TRADE_TX_GASLIMIT * gasPrice)
+    .dividedBy(OWL_DECIMAL_UNITS)
+  console.log('MIN_ECONOMICAL_VIABLE_FEE_IN_OWL', MIN_ECONOMICAL_VIABLE_FEE_IN_OWL.toString(10))
+
+  const MIN_FEE = MIN_ECONOMICAL_VIABLE_FEE_IN_OWL.multipliedBy(BUFFER_MULTIPLIER).dividedBy(SUBSIDIZE_FACTOR)
+  return MIN_FEE.multipliedBy(FEE_FACTOR * SETTLEMENT_FACTOR)
 }
