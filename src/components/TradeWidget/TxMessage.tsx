20e72b05ef4c4e728640b7b49e7fc460a9444364
David Sato <david.sato64@gmail.com>
2020-08-04 13:15:46 +0200

    pr review comments
    1. added how order works
    2. tooltips

--- src/components/TradeWidget/TxMessage.tsx
+++ src/components/TradeWidget/TxMessage.tsx
@@ -5,7 +5,8 @@ import { TokenDetails } from 'types'
 import styled from 'styled-components'
 import { TradeFormData } from '.'
 import { displayTokenSymbolOrLink } from 'utils/display'
-import { UnderlinedText } from 'components/PoolingWidget/CreateStrategy.styled'
+import { HelpTooltip } from 'components/Tooltip'
+import useSafeState from 'hooks/useSafeState'
 
 interface TxMessageProps {
   sellToken: TokenDetails
@@ -14,9 +15,35 @@ interface TxMessageProps {
 
 const TxMessageWrapper = styled.div`
   padding: 1em;
+
+  > div.message {
+    div.sectionTitle {
+      margin: 0 0 0.2rem 0;
+    }
+
+    div:not(.sectionTitle) {
+      margin-left: 1rem;
+    }
+  }
 `
+const ReceiveTooltip: React.FC<{ amount: string; buyToken: string | React.ReactNode; linkURL?: string }> = ({
+  amount,
+  linkURL = 'https://docs.gnosis.io/protocol/',
+  buyToken,
+}) => (
+  <>
+    You will receive at least {amount} {buyToken} if your order is completely executed. <br />
+    ⚠️ Please remember that your order can be also partially executed, or not executed at all. <br />
+    Read more about how Gnosis Protocol works{' '}
+    <a target="_blank" rel="noreferrer" href={linkURL}>
+      here
+    </a>
+    .
+  </>
+)
 
 export const TxMessage: React.FC<TxMessageProps> = ({ sellToken, receiveToken }) => {
+  const [orderHelpVisible, showOrderHelp] = useSafeState(false)
   const { getValues } = useFormContext<TradeFormData>()
   const {
     price,
@@ -34,37 +61,80 @@ export const TxMessage: React.FC<TxMessageProps> = ({ sellToken, receiveToken })
       <div className="intro-text">
         <div>Carefully review the information below to make sure everything looks correct.</div>
       </div>
+      <p>
+        How is the order executed?
+        <a onClick={(): void => showOrderHelp(!orderHelpVisible)}>
+          {orderHelpVisible ? '[-] Show less...' : '[+] Show more...'}
+        </a>
+        {orderHelpVisible && (
+          <>
+            <ul>
+              <li>
+                After you confirm and send the ethereum transaction, your order will be active during the expecified
+                validity time.
+              </li>
+              <li>
+                During that time, it will be matched against other orders as long as there is an overlap in the limit
+                prices.
+              </li>
+              <li>
+                The limit price specified in this order will be respected, and{' '}
+                <strong>no fee will be applied on top of it</strong>.
+              </li>
+              <li>You can cancel the order at any point after its creation.</li>
+            </ul>
+            <p>
+              Check{' '}
+              <a href="https://docs.gnosis.io/protocol/" target="_blank" rel="noreferrer">
+                here
+              </a>{' '}
+              for more information on Gnosis Protocol.
+            </p>
+          </>
+        )}
+      </p>
       <div className="message">
         {/* Details */}
-        <div>
+        <div className="sectionTitle">
           <strong>Order Details</strong>
         </div>
         <div>
-          Sell: <span>{sellTokenAmount}</span> <strong>{displaySellToken}</strong> for{' '}
-          <strong>{displayReceiveToken}</strong>
+          Sell: <span>{sellTokenAmount}</span> <strong>{displaySellToken}</strong>
         </div>
         <div>
-          Receive: <UnderlinedText>at least</UnderlinedText> {receiveTokenAmount} <strong>{displayReceiveToken}</strong>
+          Receive: {receiveTokenAmount} <strong>{displayReceiveToken}</strong>{' '}
+          <HelpTooltip tooltip={<ReceiveTooltip amount={receiveTokenAmount} buyToken={displayReceiveToken} />} />
         </div>
 
         {/* Prices */}
         <p>
-          <div>
+          <div className="sectionTitle">
             <strong>Prices</strong>
           </div>
           <div>
-            Direct: <span>{priceInverse}</span> <strong>{displaySellToken}</strong> per{' '}
-            <strong>{displayReceiveToken}</strong>
+            Direct: <span>{price}</span> <strong>{displayReceiveToken}</strong> per <strong>{displaySellToken}</strong>
           </div>
           <div>
-            Inverse: <span>{price}</span> <strong>{displayReceiveToken}</strong> per <strong>{displaySellToken}</strong>
+            Inverse: <span>{priceInverse}</span> <strong>{displaySellToken}</strong> per{' '}
+            <strong>{displayReceiveToken}</strong>
           </div>
         </p>
 
         {/* Order Validity */}
         <p>
-          <div>
-            <strong>Order Validity Details</strong>
+          <div className="sectionTitle">
+            <strong>Order Validity Details</strong>{' '}
+            <HelpTooltip
+              tooltip={
+                <>
+                  ⚠️ Learn more about the validity of orders in the Gnosis Protocol{' '}
+                  <a target="_blank" rel="noreferrer" href="https://docs.gnosis.io/protocol/docs/intro-batches/#orders">
+                    here
+                  </a>
+                  .
+                </>
+              }
+            />
           </div>
           <div>
             Starts: <span>{formatTimeInHours(validFrom || 0, 'Now')}</span>
