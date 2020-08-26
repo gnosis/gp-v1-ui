c08a6d660c2181e8496845a088e0041f232b23ce
David Sato <david.sato64@gmail.com>
2020-08-26 10:58:55 +0200

    update usePriortiseToken hook to return bool
    1. use bool to set inversePrice
    2. no changing base/quote token

--- src/hooks/usePrioritiseTokensForPrice.ts
+++ src/hooks/usePrioritiseTokensForPrice.ts
@@ -1,51 +1,36 @@
-import { useEffect } from 'react'
-
-import useSafeState from './useSafeState'
+import { useMemo } from 'react'
 import { TokenDetails } from 'types'
-
-interface HookReturn {
-  baseToken: TokenDetails
-  quoteToken: TokenDetails
-}
 
 interface HookProps {
   sellToken: TokenDetails
   receiveToken: TokenDetails
+  startInverse: boolean
 }
 
-export default ({ sellToken, receiveToken }: HookProps): HookReturn => {
+export default ({ sellToken, receiveToken, startInverse }: HookProps): boolean => {
   // baseToken = receiveToken
   // quoteToken = sellToken
   // baseToken/quoteToken
-  const [baseToken, setBaseToken] = useSafeState(receiveToken)
-  const [quoteToken, setQuoteToken] = useSafeState(sellToken)
-
-  useEffect(() => {
+  const showInverse = useMemo(() => {
     // use defaults if no tokens or both are volatile (non-priority) tokens
-    if (!sellToken || !receiveToken || (!sellToken.priority && !receiveToken.priority)) return
+    if (!sellToken || !receiveToken || (!sellToken.priority && !receiveToken.priority)) return false
 
-    let base = receiveToken
-    let quote = sellToken
+    let showInversePrice = startInverse
 
-    const buyIsWeth = base.priority === 3
-    const sellIsWeth = quote.priority === 3
-    const sellIsStable = quote.priority === 1 || quote.priority === 2
-    const buyIsVolatile = !base.priority
+    const receiveIsWeth = receiveToken.priority === 3
+    const sellIsWeth = sellToken.priority === 3
+    const sellIsStable = sellToken.priority === 1 || sellToken.priority === 2
+    const receiveIsVolatile = !receiveToken.priority
 
-    // Volatile/stable -> Always stable as quote
-    // volatile/WETH -> WETH as quote token
-    if ((sellIsStable || sellIsWeth) && (buyIsVolatile || buyIsWeth)) {
-      base = sellToken
-      quote = receiveToken
+    // IF: [WETH]/[STABLE] OR [VOLATILE]/[STABLE] OR [VOLATILE]/[WETH]
+    if ((sellIsStable || sellIsWeth) && (receiveIsVolatile || receiveIsWeth)) {
+      showInversePrice = true
+    } else {
+      showInversePrice = false
     }
-    // stable/stable -> As user inputs, respecting base/quote system
-    // volatile/volatile -> As user inputs
-    setBaseToken(base)
-    setQuoteToken(quote)
-  }, [baseToken, receiveToken, sellToken, setBaseToken, setQuoteToken])
 
-  return {
-    baseToken,
-    quoteToken,
-  }
+    return showInversePrice
+  }, [receiveToken, sellToken, startInverse])
+
+  return showInverse
 }
