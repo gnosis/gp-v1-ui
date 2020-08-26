ca7fd0bb5d3536818ebceab0ef50139ba25f01b2
Velenir <velenir@gmail.com>
2020-08-26 16:50:51 +0300

    merge webpack configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+// your app's webpack.config.js
+const custom = require("../webpack.config.js")();
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +9,24 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        // enable rules override sparingly
+        // only if storybook can't transpile something
+        // rules: custom.module.rules,
+      },
+      resolve: {
+        ...config.resolve,
+        ...custom.resolve,
+      },
+      plugins: [
+        ...config.plugins,
+        ...custom.plugins // always need at least Env plugins
+      ],
+    }
+  }
 }
