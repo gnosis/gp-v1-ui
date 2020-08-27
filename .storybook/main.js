e3db5abe23f8c14f7424b6b2e24d4c7e4f4d057d
Velenir <velenir@gmail.com>
2020-08-27 12:29:32 +0300

    fix prod build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
+const { EnvironmentPlugin, DefinePlugin } = require('webpack')
 // your app's webpack.config.js
 const custom = require("../webpack.config.js")();
+
+// the minimum plugins required
+// to inline CONFIG and other env vars
+const customPlugins = custom.plugins.filter(pl => pl instanceof EnvironmentPlugin || pl instanceof DefinePlugin)
 
 module.exports = {
   "stories": [
@@ -21,11 +26,20 @@ module.exports = {
       },
       resolve: {
         ...config.resolve,
-        ...custom.resolve,
+        modules: custom.resolve.modules,  // important for absolute path resolution
+        // enable resolve override sparingly
+        // storybook config includes many resolve.alias and resolve.plugins
+        // ...custom.resolve,
       },
       plugins: [
         ...config.plugins,
-        ...custom.plugins // always need at least Env plugins
+        ...customPlugins,
+        // enable plugins override sparingly
+        // only if storybook doesn't work as is
+        // pulling in every plugin currently breaks production build
+        // because of some incompatibility with DLLPlugin from storybook config
+        // with `storybook_docs_dll is not defined` runtime error
+        // ...custom.plugins
       ],
     }
   }
