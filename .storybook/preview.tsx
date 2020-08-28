a23efe9455978f547368ba3b011082889dfc6c83
Velenir <velenir@gmail.com>
2020-08-28 16:48:16 +0300

    .storybook/preview in TS

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import GlobalStyles from 'styles/global'
+import { BaseDecorators } from '@storybook/addons'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-export const decorators = [
+export const decorators: BaseDecorators<JSX.Element> = [
   (Story) => (
     <>
       <GlobalStyles />
