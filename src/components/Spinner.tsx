febe5d5f316163af78eab46977fcf4a413a1cbe2
Anxo Rodriguez <anxolin@gmail.com>
2020-08-28 08:27:34 +0200

    Allow to change the icon

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { SizeProp } from '@fortawesome/fontawesome-svg-core'
 
 export interface SpinnerProps {
   spin?: boolean
   style?: React.CSSProperties
   size?: SizeProp
+  icon?: IconDefinition
 }
 
-export const Spinner: React.FC<SpinnerProps> = ({ style, spin = true, size }) => (
-  <FontAwesomeIcon icon={faSpinner} style={style} spin={spin} size={size} />
+export const Spinner: React.FC<SpinnerProps> = ({ style, spin = true, size, icon = faSpinner }) => (
+  <FontAwesomeIcon icon={icon} style={style} spin={spin} size={size} />
 )
 
 export default Spinner
