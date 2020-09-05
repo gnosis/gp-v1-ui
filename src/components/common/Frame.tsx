e897085ad0de98ac67d3178ad638c7d0cb102137
Anxo Rodriguez <anxolin@gmail.com>
2020-09-05 20:37:40 +0200

    Separate component and dumb component

--- src/components/common/Frame.tsx
+++ src/components/common/Frame.tsx
@@ -3,8 +3,7 @@ import React, { CSSProperties } from 'react'
 const CSS_DEFAULT: CSSProperties = {
   margin: '2rem auto',
   border: '2px dashed gray',
-  padding: '2rem',
-  minHeight: '20rem',
+  padding: '1.5rem',
 }
 
 interface Props {
