50c1d0f237bd81ce88895ca89773c0229c5f69dc
Anxo Rodriguez <anxolin@gmail.com>
2020-09-07 10:28:17 +0200

    Remove unused logic in Swap component

--- src/components/common/SwapPrice.tsx
+++ src/components/common/SwapPrice.tsx
@@ -16,33 +16,17 @@ export interface Props {
   baseToken: TokenDetails
   quoteToken: TokenDetails
   isPriceInverted: boolean
-  separator?: string
-  showOnlyQuoteToken?: boolean
+  separator?: SVGAnimatedString
   onSwapPrices: () => void
 }
 
-export const SwapPrice: React.FC<Props> = ({
-  baseToken,
-  quoteToken,
-  separator = '/',
-  isPriceInverted,
-  showOnlyQuoteToken = false,
-  onSwapPrices,
-}) => {
-  const displayBaseToken = !isPriceInverted ? quoteToken : baseToken
-  const displayQuoteToken = isPriceInverted ? quoteToken : baseToken
-  const displayBtName = displayTokenSymbolOrLink(displayBaseToken)
-  const displayQtName = displayTokenSymbolOrLink(displayQuoteToken)
+export const SwapPrice: React.FC<Props> = ({ baseToken, quoteToken, isPriceInverted, onSwapPrices }) => {
+  const displayQuoteToken = isPriceInverted ? baseToken : quoteToken
+  const quoteTokenName = displayTokenSymbolOrLink(displayQuoteToken)
 
   return (
     <SwapPriceWrapper onClick={onSwapPrices}>
-      {!showOnlyQuoteToken && (
-        <>
-          <EllipsisText title={safeTokenName(displayBaseToken)}>{displayBtName}</EllipsisText>
-          <div className="separator">{separator}</div>
-        </>
-      )}
-      <EllipsisText title={safeTokenName(displayQuoteToken)}>{displayQtName}</EllipsisText>
+      <EllipsisText title={safeTokenName(displayQuoteToken)}>{quoteTokenName}</EllipsisText>
       <SwapIcon />
     </SwapPriceWrapper>
   )
