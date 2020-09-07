bb81ceefdd6de3d05668e104359a4f1764da7a28
Anxo Rodriguez <anxolin@gmail.com>
2020-09-07 19:09:25 +0200

    Add component to stories of FormMessage

--- src/components/common/FormMessage.stories.tsx
+++ src/components/common/FormMessage.stories.tsx
@@ -6,6 +6,7 @@ import { Meta, Story } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Common/FormMessage',
+  component: FormMessage,
 } as Meta
 
 const Template: Story<Props> = (args) => <FormMessage {...args}>This is an example of a form message</FormMessage>
