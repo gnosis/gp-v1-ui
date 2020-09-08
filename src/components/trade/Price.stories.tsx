6de295c1e693088c8a8386758a104ff3763bc600
Anxo Rodriguez <anxolin@gmail.com>
2020-09-08 15:51:40 +0200

    Fic wrong component

--- src/components/trade/Price.stories.tsx
+++ src/components/trade/Price.stories.tsx
@@ -5,11 +5,10 @@ import { useForm, FormProvider } from 'react-hook-form'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from '@storybook/react/types-6-0'
 import { Price, Props } from './Price'
-import { SwapPrice } from 'components/common/SwapPrice'
 
 export default {
   title: 'Trade/Price',
-  component: SwapPrice,
+  component: Price,
 } as Meta
 
 const Template: Story<Props> = (props) => {
