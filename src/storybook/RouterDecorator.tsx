d9194a9dea16aa8e7a4a48ff9fdcc3c50b2c4dbe
Anxo Rodriguez <anxolin@gmail.com>
2020-09-14 18:20:12 +0200

    Do not use internal types

--- src/storybook/RouterDecorator.tsx
+++ src/storybook/RouterDecorator.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types'
 import { MemoryRouter } from 'react-router'
 
-export const RouterDecorator = (DecoratedStory: () => StoryFnReactReturnType): JSX.Element => (
+export const RouterDecorator = (DecoratedStory: () => JSX.Element): JSX.Element => (
   <MemoryRouter initialEntries={['/']}>{DecoratedStory()}</MemoryRouter>
 )
