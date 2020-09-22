f50c8a47a4f87b75ba4551008780ef7ba26c3b45
Anxo Rodriguez <anxolin@gmail.com>
2020-09-22 18:57:25 +0200

    Add new simplified tokenImage compoenent

---
+++ src/components/common/TokenImg2.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { TokenImg2, Props } from './TokenImg2'
+import { WETH_ADDRESS_MAINNET, WETH_ADDRESS_RINKEBY } from 'const'
+
+const ADDRESS_GNO = '0x6810e776880C02933D47DB1b9fc05908e5386b96'
+const ADDRESS_WXDAI = '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d'
+
+export default {
+  title: 'Common/TokenImg',
+  component: TokenImg2,
+} as Meta
+
+const Template: Story<Props> = (args) => <TokenImg2 {...args} />
+
+export const WethMainnet = Template.bind({})
+WethMainnet.args = {
+  address: WETH_ADDRESS_MAINNET,
+}
+
+export const WethRinkeby = Template.bind({})
+WethRinkeby.args = {
+  address: WETH_ADDRESS_RINKEBY,
+  addressMainnet: WETH_ADDRESS_MAINNET,
+}
+
+export const WrappedXdai = Template.bind({})
+WrappedXdai.args = {
+  address: ADDRESS_WXDAI,
+  faded: true,
+}
+
+export const Unknown = Template.bind({})
+Unknown.args = {
+  address: '0x1',
+}
+
+export const Gno = Template.bind({})
+Gno.args = {
+  address: ADDRESS_GNO,
+}
+
+export const Faded = Template.bind({})
+Faded.args = {
+  address: ADDRESS_GNO,
+  faded: true,
+}
---
+++ src/components/common/TokenImg2.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { getImageUrl, safeTokenName } from 'utils'
+import unknownTokenImg from 'assets/img/unknown-token.png'
+
+const Wrapper = styled.img<WrapperProps>`
+  width: 2.8rem;
+  height: 2.8rem;
+  border-radius: 3.6rem;
+  object-fit: contain;
+  margin: 0 1rem 0 0;
+  background-color: white;
+  padding: 2px;
+  opacity: ${(props): number => (props.faded ? 0.4 : 1)};
+`
+
+function _loadFallbackTokenImage(event: React.SyntheticEvent<HTMLImageElement>): void {
+  const image = event.currentTarget
+  image.src = unknownTokenImg
+}
+
+export interface Props {
+  symbol?: string
+  name?: string
+  address: string
+  addressMainnet?: string
+  faded?: boolean
+}
+
+export interface WrapperProps {
+  faded?: boolean
+}
+
+export const TokenImg2: React.FC<Props> = (props) => {
+  const { address, addressMainnet, symbol, name, faded = false } = props
+
+  // TODO: Probably a good idea to allow to see in etherscan (probably wrapping this in BlockExplorerLink component)
+
+  // TODO: Simplify safeTokenName signature, it doesn't need the addressMainnet or id!
+  const safeName = safeTokenName({ id: 0, address, addressMainnet, symbol, name })
+  return (
+    <Wrapper
+      alt={safeName}
+      src={getImageUrl(addressMainnet || address)}
+      onError={_loadFallbackTokenImage}
+      faded={faded}
+    />
+  )
+}
+
+export default TokenImg2
