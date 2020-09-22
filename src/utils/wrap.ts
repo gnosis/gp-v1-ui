fbd85c2a67f864a7ed15046d25d7a180d5c1a015
Anxo Rodriguez <anxolin@gmail.com>
2020-09-22 15:50:12 +0200

    Add util to get the native token name

---
+++ src/utils/wrap.ts
@@ -0,0 +1,34 @@
+import { WETH_ADDRESS_MAINNET, WETH_ADDRESS_RINKEBY, WXDAI_ADDRESS_XDAI } from 'const'
+import { Network } from 'types'
+
+export interface NativeTokenInfo {
+  nativeToken: string
+  wrappedToken: string
+}
+
+export function getIsWrappable(networkId: number, address: string): boolean {
+  switch (networkId) {
+    case Network.Mainnet:
+      return address === WETH_ADDRESS_MAINNET
+    case Network.Rinkeby:
+      return address === WETH_ADDRESS_RINKEBY
+    case Network.xDai:
+      return address === WXDAI_ADDRESS_XDAI
+    default:
+      return false
+  }
+}
+
+export function getNativeTokenName(networkId?: number): NativeTokenInfo {
+  if (networkId === Network.xDai) {
+    return {
+      nativeToken: 'xDAI',
+      wrappedToken: 'wxDAI',
+    }
+  } else {
+    return {
+      nativeToken: 'ETH',
+      wrappedToken: 'WETH',
+    }
+  }
+}
