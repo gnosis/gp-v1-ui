424c33576c81142e4b0c87d4b592ec32fd5ff0be
Anxo Rodriguez <anxolin@gmail.com>
2020-09-24 20:53:39 +0200

    Init token images

--- src/components/common/TokenImg.tsx
+++ src/components/common/TokenImg.tsx
@@ -33,26 +33,37 @@ export interface WrapperProps {
 
 const tokensIconsRequire =
   process.env.NODE_ENV === 'test' ? RequireContextMock : require.context('assets/img/tokens', false)
-const tokensIconsFiles = tokensIconsRequire.keys()
+
+const tokensIconsFilesByAddress = tokensIconsRequire.keys().reduce((acc, file) => {
+  let address
+  const match = file.match(/(0x\w{40})/)
+  if (match && match.length > 0) {
+    address = match[match.length - 1]
+  }
+  if (!address) {
+    throw new Error(
+      "Error initializing 'assets/img/tokens' images. The image doesn't have the expected format: " + file,
+    )
+  }
+  acc[address.toLowerCase()] = file
+
+  return acc
+}, {})
 
 export const TokenImg: React.FC<Props> = (props) => {
   const { address, addressMainnet, symbol, name } = props
 
-  const iconFile = tokensIconsFiles.find(
-    (file) => file.includes(address) || (addressMainnet && file.includes(addressMainnet)),
-  )
-  const iconFileUrl = iconFile ? tokensIconsRequire(iconFile) : undefined
+  let iconFile = tokensIconsFilesByAddress[address.toLowerCase()]
+  if (!iconFile && addressMainnet) {
+    iconFile = tokensIconsFilesByAddress[addressMainnet.toLowerCase()]
+  }
+
+  const iconFileUrl = iconFile ? tokensIconsRequire(iconFile) : getImageUrl(addressMainnet || address)
   // TODO: Simplify safeTokenName signature, it doesn't need the addressMainnet or id!
   // https://github.com/gnosis/dex-react/issues/1442
   const safeName = safeTokenName({ id: 0, address, addressMainnet, symbol, name })
-  return (
-    <Wrapper
-      alt={safeName}
-      src={iconFileUrl || getImageUrl(addressMainnet || address)}
-      onError={_loadFallbackTokenImage}
-      {...props}
-    />
-  )
+
+  return <Wrapper alt={safeName} src={iconFileUrl} onError={_loadFallbackTokenImage} {...props} />
 }
 
 export const TokenImgWrapper = styled(TokenImg)`
