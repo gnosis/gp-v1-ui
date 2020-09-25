89d7d9c0f17cf361443887159ec84644193216e3
Velenir <Velenir@users.noreply.github.com>
2020-09-25 18:04:31 +0300

    Extra gas per order for order cancellation (#1445)
    * extra gas * orders for order cancellation
    
    * earmarkTX is responsible for tx.gas++

--- src/api/earmark.ts
+++ src/api/earmark.ts
@@ -1,4 +1,5 @@
 import { logDebug } from 'utils'
+import { hexToNumber, numberToHex } from 'web3-utils'
 
 // 0.1 Gwei, reasonable gasPrice that would still allow flags replacement
 export const MIN_GAS_PRICE = (1e8).toString(10)
@@ -22,3 +23,17 @@ export const earmarkGasPrice = (gasPrice: string, userPrint: string): string => 
 
 // simple concatenation, with '0x' for empty data to have `0x<userPrint>` at the least
 export const earmarkTxData = (data = '0x', userPrint: string): string => data + userPrint
+
+const GAS_PER_DATA_CHAR = 8 // wei
+
+// increases pre-estimated gas by the amount needed to save extra earmark data to chain
+// 16 wei per 2 characters
+export const calcEarmarkedGas = (gas: string | number, userPrint: string): string => {
+  const gasNumber = hexToNumber(gas)
+
+  const earmarkGas = GAS_PER_DATA_CHAR * userPrint.length
+
+  const newGas = gasNumber + earmarkGas
+
+  return numberToHex(newGas)
+}
