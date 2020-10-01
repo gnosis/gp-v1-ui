8e2caf372f57dd69b7cb8acd6e7486bce96a4129
Anxo Rodriguez <anxolin@gmail.com>
2020-10-01 08:54:00 +0200

    Add initial default list as a config

--- src/types/config.ts
+++ src/types/config.ts
@@ -3,6 +3,7 @@ import { MultiTcrApiParams } from 'api/tcr/MultiTcrApi'
 import { DexPriceEstimatorParams } from 'api/dexPriceEstimator/DexPriceEstimatorApi'
 import { ContractDeploymentBlock } from 'api/exchange/ExchangeApi'
 import { Network } from 'types'
+import { TokenDetailsConfig } from '@gnosis.pm/dex-js'
 
 export interface MultiTcrConfig {
   type: 'multi-tcr'
@@ -61,6 +62,7 @@ export interface Config {
   name: string
   logoPath: string
   templatePath: string
+  initialTokenList: TokenDetailsConfig[]
   tcr: MultiTcrConfig | NoTcrConfig
   dexPriceEstimator: DexPriceEstimatorConfig
   theGraphApi: TheGraphApiConfig
