e5b6f33371c96755d6e0c114e4de4c18f3bf4162
David <david.sato64@gmail.com>
2020-10-07 21:00:31 +0200

    useForm / react-hook-form storybook decorator (#1482)
    * react-hook-form storybook decorator
    
    * add decorator to PriceSuggestions & Price
    
    1. fix broken Price story
    
    * Remove maxWidth in Form decorator
    
    Remove maxWidth in Form decorator
    
    * yarn
    
    * use curried Form and FormEmpty decorators in price

--- src/components/trade/Price.stories.tsx
+++ src/components/trade/Price.stories.tsx
@@ -1,30 +1,27 @@
 import React from 'react'
-// import { Price } from 'components/trade/PriceP'
-import { useForm, FormProvider } from 'react-hook-form'
 
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from '@storybook/react/types-6-0'
 import { Price, Props } from './Price'
 import { DAI, GNO } from 'storybook/data'
+import { FormEmpty } from 'storybook/decorators'
 
 export default {
   title: 'Trade/Price',
   component: Price,
+  decorators: [FormEmpty],
 } as Meta
 
-const Template: Story<Props> = (props) => {
-  const methods = useForm()
-  return (
-    <FormProvider {...methods}>
-      <form>
-        <Price {...props} />
-      </form>
-    </FormProvider>
-  )
+const Template: Story<Props> = (props) => <Price {...props} />
+
+const defaultProps = {
+  priceInputId: 'price',
+  priceInverseInputId: 'inversePrice',
 }
 
 export const Basic = Template.bind({})
 Basic.args = {
+  ...defaultProps,
   quoteToken: GNO,
   baseToken: DAI,
 }
