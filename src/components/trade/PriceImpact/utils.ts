9a3f9f14447299f249e5948ecede39abc3afcbb6
David Sato <david.sato64@gmail.com>
2020-10-23 11:18:48 +0200

    utils: decouple some of the impact level determining logic
    1. add midPrice and high price impact thres
    2. warning logic fix

--- src/components/trade/PriceImpact/utils.ts
+++ src/components/trade/PriceImpact/utils.ts
@@ -6,16 +6,36 @@ import { ONE_HUNDRED_BIG_NUMBER, ONE_BIG_NUMBER, ZERO_BIG_NUMBER } from 'const'
 import { PriceImpactArgs, PriceImpactArgsBase } from '../PriceImpact'
 
 // Limit as to where price colour changes
-const HIGH_PRICE_IMPACT_THRESHOLD = new BigNumber(3)
+const HIGH_PRICE_IMPACT_THRESHOLD = new BigNumber(5)
+const MID_PRICE_IMPACT_THRESHOLD = new BigNumber(3)
 const LOW_PRICE_IMPACT_THRESHOLD = ONE_BIG_NUMBER
+
+const determineImpactLevel = (impact: BigNumber | null): number => {
+  let impactLevel = 0
+  if (impact) {
+    if (impact.gte(HIGH_PRICE_IMPACT_THRESHOLD)) {
+      impactLevel = 3
+    } else if (impact.gte(MID_PRICE_IMPACT_THRESHOLD)) {
+      impactLevel = 2
+    } else if (impact.lt(LOW_PRICE_IMPACT_THRESHOLD)) {
+      impactLevel = 1
+    }
+  }
+
+  return impactLevel
+}
 
 // sets colour of strong tag presenting price impact
 const getImpactColourClass = (impact: BigNumber | null): string => {
+  const impactLevel = determineImpactLevel(impact)
+
   let className = ''
-  if (impact) {
-    if (impact.gt(HIGH_PRICE_IMPACT_THRESHOLD)) {
+  if (impactLevel) {
+    if (impactLevel === 3) {
       className = 'highImpact'
-    } else if (impact.lt(LOW_PRICE_IMPACT_THRESHOLD)) {
+    } else if (impactLevel === 2) {
+      className = 'midImpact'
+    } else if (impactLevel === 1) {
       className = 'lowImpact'
     }
   }
@@ -23,22 +43,15 @@ const getImpactColourClass = (impact: BigNumber | null): string => {
   return className
 }
 
-function determinePriceWarning(params: PriceImpactArgs): string | null {
+function determinePriceWarning(params: PriceImpactArgs, impact: BigNumber | null): string | null {
   const { limitPrice, fillPrice, bestAskPrice } = params
 
   const limitPriceBigNumber = limitPrice && parseBigNumber(limitPrice)
-  const fillPriceRounded = fillPrice?.toFixed(5)
+  // const fillPriceRounded = fillPrice?.toFixed(5)
 
   // CASE 1 & 2: no warning
   // round fillPrice to 5 decimals points and string compare against limit
-  if (
-    !limitPriceBigNumber ||
-    limitPriceBigNumber.isZero() ||
-    !fillPrice ||
-    !bestAskPrice ||
-    limitPrice === fillPriceRounded
-  )
-    return null
+  if (!limitPriceBigNumber || limitPriceBigNumber.isZero() || !fillPrice || !bestAskPrice) return null
 
   const isLimitPriceLessThanFillPrice = limitPriceBigNumber.lt(fillPrice)
   const isLimitPriceLessThanFillAndBestAsk = isLimitPriceLessThanFillPrice && limitPriceBigNumber.lt(bestAskPrice)
@@ -54,7 +67,12 @@ function determinePriceWarning(params: PriceImpactArgs): string | null {
     }
   } else {
     // CASE 6: Limit price is GREATER THAN Fill price (and Best Ask)
-    warning = 'Deep in the market: Your order might be fully executed at this price.'
+    const impactLevel = determineImpactLevel(impact)
+    if (impactLevel === 3) {
+      warning = 'Deep in the market: Your order might be fully executed at this price.'
+    } else {
+      warning = 'Your order might be fully executed at this price.'
+    }
   }
 
   return warning
