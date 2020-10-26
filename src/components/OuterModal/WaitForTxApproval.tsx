da4b56de6a96d6a428715fd6ff3fa7e6ee968888
Velenir <velenir@gmail.com>
2020-10-26 18:49:33 +0300

    style WaitForTxApproval a bit

--- src/components/OuterModal/WaitForTxApproval.tsx
+++ src/components/OuterModal/WaitForTxApproval.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { openGlobalModal } from './SingletonModal'
 
+const WaitForTxWrapper = styled.div`
+  font-size: 1.3em;
+`
+
 export const WaitForTxApprovalMessage: React.FC<{ timePassed: number }> = ({ timePassed }) => {
   return (
-    <p>
-      It has been {timePassed} seconds since transaction was submitted for approval. Some wallets do not respond to{' '}
-      <code>eth_sendTransaction</code> calls. Stop waiting for transaction approval from Wallet?
-    </p>
+    <WaitForTxWrapper>
+      <p>
+        It has been {timePassed} seconds since transaction was submitted for approval. Some wallets do not respond to{' '}
+        <code>eth_sendTransaction</code> calls.
+      </p>
+      <p>Stop waiting for transaction approval from Wallet?</p>
+    </WaitForTxWrapper>
   )
 }
 export const openWaitForTxApprovalModal = (timePassed: number): Promise<boolean> =>
