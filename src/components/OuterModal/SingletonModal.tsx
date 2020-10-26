437751e125d7fad7d906fc8753a594faf26b1fc2
Velenir <velenir@gmail.com>
2020-10-26 18:45:15 +0300

    separate code for Singleton modal

---
+++ src/components/OuterModal/SingletonModal.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useRef, useMemo } from 'react'
+import Modal from 'components/common/Modal'
+import { useGlobalModal, UseGlobalModalParamsResult } from './GeneralModal'
+import { Deferred, createDeferredPromise } from 'utils'
+
+interface FillAndToggleModal {
+  message: React.ReactNode
+  title?: React.ReactNode
+  leftButton?: typeof Modal.Button
+  rightButton?: typeof Modal.Button
+}
+
+interface OuterModalOptions {
+  message: React.ReactNode
+  title?: React.ReactNode
+  leftButton: typeof Modal.Button
+  rightButton: typeof Modal.Button
+  resolve?: (result: boolean) => void
+}
+
+const defaultButtons: Pick<OuterModalOptions, 'leftButton' | 'rightButton'> = {
+  leftButton: Modal.Button,
+  rightButton: Modal.Button,
+}
+
+const defaultModalOptions: OuterModalOptions = {
+  message: null,
+  ...defaultButtons,
+}
+
+export let openGlobalModal: (params: FillAndToggleModal) => Promise<boolean> = () => Promise.resolve(false)
+export let closeGlobalModal: () => void = () => void 0
+
+const useOuterModalHook = (): UseGlobalModalParamsResult => {
+  const [modalOptions, setModalOptions] = useState<OuterModalOptions>(defaultModalOptions)
+
+  const { modalProps, toggleModal } = useGlobalModal({
+    ...modalOptions,
+    buttons: [modalOptions.leftButton, modalOptions.rightButton],
+    message: modalOptions.message,
+  })
+
+  // toggleModal recreated every time, keep ref to use in Promise.then
+  const toggleRef = useRef(toggleModal)
+  toggleRef.current = toggleModal
+  // same for modalProps.isShown
+  const isShownRef = useRef(modalProps.isShown)
+  isShownRef.current = modalProps.isShown
+
+  useMemo(() => {
+    let deferred: Deferred<boolean>
+    const fillAndOpenModal = (components: FillAndToggleModal): Promise<boolean> => {
+      // guard against double-open
+      // only one such Modal allowed at a time
+      if (isShownRef.current) return Promise.resolve(false)
+
+      // will be resolved on Confirm/Cancelin Modal
+      deferred = createDeferredPromise<boolean>()
+
+      setModalOptions({
+        ...defaultButtons,
+        ...components,
+        resolve: deferred.resolve,
+      })
+      toggleRef.current()
+
+      return deferred
+    }
+    openGlobalModal = fillAndOpenModal
+    closeGlobalModal = (): void => {
+      // guard agains double-close
+      if (!isShownRef.current) return
+
+      toggleRef.current()
+      deferred.resolve(false)
+    }
+
+    return fillAndOpenModal
+  }, [])
+
+  return {
+    toggleModal,
+    modalProps: {
+      ...modalProps,
+      ...modalOptions,
+    },
+  }
+}
+
+const GlobalModal: React.FC = () => {
+  const { modalProps } = useOuterModalHook()
+
+  return <Modal.Modal {...modalProps} />
+}
+
+// singleton to have and escape hatch
+// for toggling modal outside of react
+export const GlobalModalInstance = <GlobalModal />
