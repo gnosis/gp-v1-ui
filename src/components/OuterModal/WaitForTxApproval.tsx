07d287902631707e3c1dcae0e45d4852bc70087e
Velenir <velenir@gmail.com>
2020-10-27 11:43:54 +0300

    custom buttons for WaitForTxApproval modal

--- src/components/OuterModal/WaitForTxApproval.tsx
+++ src/components/OuterModal/WaitForTxApproval.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Modal from 'components/common/Modal'
 import { openGlobalModal } from './SingletonModal'
 
 const WaitForTxWrapper = styled.div`
@@ -18,5 +19,13 @@ export const WaitForTxApprovalMessage: React.FC<{ timePassed: number }> = ({ tim
     </WaitForTxWrapper>
   )
 }
+
+const leftButton: typeof Modal.Button = (props) => <Modal.Button {...props} label="No" />
+const rightButton: typeof Modal.Button = (props) => <Modal.Button {...props} label="Yes" />
+
 export const openWaitForTxApprovalModal = (timePassed: number): Promise<boolean> =>
-  openGlobalModal({ message: <WaitForTxApprovalMessage timePassed={timePassed} /> })
+  openGlobalModal({
+    message: <WaitForTxApprovalMessage timePassed={timePassed} />,
+    leftButton,
+    rightButton,
+  })
