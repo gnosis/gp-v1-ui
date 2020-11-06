63bf41cd6629ab941851f64475aee5052063c693
Anxo Rodriguez <anxolin@gmail.com>
2020-11-06 14:31:04 +0100

    Add about test

--- cypress/integration/about.test.ts
+++ cypress/integration/about.test.ts
@@ -1,8 +1,21 @@
 describe('About', () => {
-  beforeEach(() => {
-    cy.visit('/about')
+  it('Navigates from link in the footer', () => {
+    // GIVEN: home page
+    cy.visit('/')
+
+    // WHEN: Click on the "About" link in the footer
+    cy.get('footer').contains('About').click()
+
+    // THEN: We navigate to the about page
+    cy.url().should('contain', '/about')
   })
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+
+  it('Navigates using URL', () => {
+    // GIVEN: about page
+    cy.visit('/about')
+
+    // WHEN: -
+    // THEN: There's the heading we expect
+    cy.get('main h1:first').contains('About Gnosis Protocol Web')
   })
 })
