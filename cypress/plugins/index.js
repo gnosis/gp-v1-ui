baa9008958f6b4726be20016acc0b05dac1708cd
Anxo Rodriguez <anxolin@gmail.com>
2020-11-06 15:52:13 +0100

    Remove lint issues

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,8 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+module.exports = (_on, _config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
