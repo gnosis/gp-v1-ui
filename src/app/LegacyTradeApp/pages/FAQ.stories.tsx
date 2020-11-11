e73efabd0f4d95f387bee72fbe5402373504f067
Anxo Rodriguez <anxolin@gmail.com>
2020-11-11 18:56:26 +0100

    Refactor legacy app (#1599)
    * Add global styles
    
    * Add global styles
    
    * Rename TradeLayout to generic layout
    
    * Create its own structure per app
    
    * Refactor legacy app

--- src/app/LegacyTradeApp/pages/FAQ.stories.tsx
+++ src/app/LegacyTradeApp/pages/FAQ.stories.tsx
@@ -1,11 +1,11 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta } from '@storybook/react/types-6-0'
 
-import About from 'pages/About'
+import FAQ from './FAQ'
 
 export default {
-  title: 'Pages/About',
-  component: About,
+  title: 'Pages/FAQ',
+  component: FAQ,
 } as Meta
 
-export const Primary = About
+export const Primary = FAQ
