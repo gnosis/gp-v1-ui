f66eb4d7ee22475c8c307fdf99c360d2ca35d9fe
Velenir <velenir@gmail.com>
2020-11-12 17:19:14 +0300

    reassign error.data

--- src/api/wallet/composeProvider.ts
+++ src/api/wallet/composeProvider.ts
@@ -20,6 +20,15 @@ import {
   removeTxPendingApproval,
 } from 'components/OuterModal'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const sanitizeErrorData = (jsonRpcError?: JsonRpcError<any>): void => {
+  if (!jsonRpcError) return
+
+  if (jsonRpcError.data?.originalError) {
+    jsonRpcError.data = jsonRpcError.data.originalError.data
+  }
+}
+
 // custom providerAsMiddleware
 function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
   // WalletConnectProvider.sendAsync is web3-provider-engine.sendAsync
@@ -46,10 +55,17 @@ function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
         return
       }
 
-      provider.request(req).then((providerRes) => {
-        Object.assign(res, providerRes)
-        end()
-      }, end)
+      provider.request(req).then(
+        (providerRes) => {
+          Object.assign(res, providerRes)
+          sanitizeErrorData(res.error)
+          end()
+        },
+        (error) => {
+          sanitizeErrorData(error)
+          end(error)
+        },
+      )
     }
   }
 
@@ -62,7 +78,11 @@ function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
 
     provider[sendFName](req, (err: JsonRpcError<unknown>, providerRes: JsonRpcResponse<unknown>) => {
       // forward any error
-      if (err) return end(err)
+      if (err) {
+        sanitizeErrorData(err)
+        return end(err)
+      }
+      sanitizeErrorData(providerRes.error)
       // copy provider response onto original response
       Object.assign(res, providerRes)
       end()
