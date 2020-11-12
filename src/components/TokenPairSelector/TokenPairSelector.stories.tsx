50cb3e83cc04a25c37b3dfc64e91849926bfc1fe
Anxo Rodriguez <anxolin@gmail.com>
2020-11-12 15:05:59 +0100

    Create global style decorator (#1613)
    * Add global styles decorator
    
    * Simplify stories, use decorator

--- src/components/TokenPairSelector/TokenPairSelector.stories.tsx
+++ src/components/TokenPairSelector/TokenPairSelector.stories.tsx
@@ -3,19 +3,14 @@ import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta } from '@storybook/react/types-6-0'
 
-import GlobalStyles from 'components/layout/GenericLayout/GlobalStyles'
+import { GlobalStyles } from 'storybook/decorators'
 import { RouterDecorator } from 'storybook/RouterDecorator'
 import { TokenPairSelector } from 'components/TokenPairSelector'
 
 export default {
   title: 'component/TokenPairSelector',
   component: TokenPairSelector,
-  decorators: [RouterDecorator],
+  decorators: [RouterDecorator, GlobalStyles],
 } as Meta
 
-export const Normal: React.FC = () => (
-  <div>
-    <GlobalStyles />
-    <TokenPairSelector selectedPair="ETH/USDC" selectLabel="Select Pair" />
-  </div>
-)
+export const Normal: React.FC = () => <TokenPairSelector selectedPair="ETH/USDC" selectLabel="Select Pair" />
