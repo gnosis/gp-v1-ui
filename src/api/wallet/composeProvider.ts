03da3c989732f8999668537a054c9111fb5ed19b
Anxo Rodriguez <anxolin@gmail.com>
2020-11-12 18:21:17 +0100

    Sanitize error data (#1615)
    * Sanitize error data
    
    * Remove unneded import
    
    Co-authored-by: Velenir <velenir@gmail.com>

--- src/api/wallet/composeProvider.ts
+++ src/api/wallet/composeProvider.ts
@@ -12,6 +12,15 @@ import { isWalletConnectProvider, Provider } from './providerUtils'
 import { logDebug } from 'utils'
 import { web3 } from 'api'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const sanitizeErrorData = (jsonRpcError?: JsonRpcError<any>): void => {
+  if (!jsonRpcError) return
+
+  if (jsonRpcError.data?.originalError) {
+    jsonRpcError.data = jsonRpcError.data.originalError.data
+  }
+}
+
 // custom providerAsMiddleware
 function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
   // WalletConnectProvider.sendAsync is web3-provider-engine.sendAsync
@@ -38,10 +47,17 @@ function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
         return
       }
 
-      provider.request(req).then((providerRes) => {
-        Object.assign(res, providerRes)
-        end()
-      }, end)
+      provider.request(req).then(
+        (providerRes) => {
+          Object.assign(res, providerRes)
+          sanitizeErrorData(res.error)
+          end()
+        },
+        (error) => {
+          sanitizeErrorData(error)
+          end(error)
+        },
+      )
     }
   }
 
@@ -54,7 +70,11 @@ function providerAsMiddleware(provider: Provider): JsonRpcMiddleware {
 
     provider[sendFName](req, (err: JsonRpcError<unknown>, providerRes: JsonRpcResponse<unknown>) => {
       // forward any error
-      if (err) return end(err)
+      if (err) {
+        sanitizeErrorData(err)
+        return end(err)
+      }
+      sanitizeErrorData(providerRes.error)
       // copy provider response onto original response
       Object.assign(res, providerRes)
       end()
