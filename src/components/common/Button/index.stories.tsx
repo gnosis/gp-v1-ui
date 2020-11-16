29c2ebe0596a69ed996679e1ab169d66fb2c462c
David Sato <david.sato64@gmail.com>
2020-11-16 22:24:32 +0100

    Button story

---
+++ src/components/common/Button/index.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { ButtonBase } from '.'
+import { DarkModeThemeToggler } from 'storybook/decorators'
+
+export default {
+  title: 'Common/Button',
+  component: ButtonBase,
+  decorators: [DarkModeThemeToggler],
+  argTypes: {
+    label: { control: 'text' },
+    theme: {
+      control: null,
+    },
+    as: {
+      control: null,
+    },
+    forwardedAs: {
+      control: null,
+    },
+  },
+} as Meta
+
+const Template: Story = (args) => <ButtonBase {...args}>{args.label}</ButtonBase>
+
+export const PrimaryButton = Template.bind({})
+PrimaryButton.args = {
+  label: 'Main Button',
+}
+
+export const SecondaryButton = Template.bind({})
+SecondaryButton.args = {
+  label: 'SecondaryButton',
+  $alt: true,
+  $border: true,
+}
