171760f38379af50eb96811799cf778dc842a301
Velenir <velenir@gmail.com>
2020-11-16 13:23:42 +0300

    conditionally compile provider

--- playwright/utils/provider_setup.ts
+++ playwright/utils/provider_setup.ts
@@ -1,5 +1,6 @@
 import webpack from 'webpack'
 import path from 'path'
+import fs from 'fs'
 import Web3EthAccounts, { Accounts as AccountsType } from 'web3-eth-accounts'
 
 import { context } from './test_setup'
@@ -9,15 +10,42 @@ const { config } = CONFIG.defaultProviderConfig
 const providerURL = 'ethNodeUrl' in config ? config.ethNodeUrl : 'https://rinkeby.infura.io/v3/' + config.infuraId
 console.log('url', providerURL)
 
-console.log('path.resolve', path.resolve(__dirname, '../build/'))
-const compiler = webpack({
-  entry: path.resolve(__dirname, '../inject_provider.js'),
-  output: {
-    path: path.resolve(__dirname, '../build/'),
-    filename: 'inject_provider.js',
-  },
-  cache: true,
-})
+const fileExists = (path: string): Promise<boolean> => {
+  return fs.promises.access(path).then(
+    () => true,
+    () => false,
+  )
+}
+
+const compileInjects = async (): Promise<void> => {
+  console.log('process.env.REBUILD_PW', process.env.REBUILD_PW)
+  // only rebuild when no file found
+  // or when passed REBUILD_PW=1 env var
+  if (!process.env.REBUILD_PW && (await fileExists(path.resolve(__dirname, '../build/inject_provider.js')))) {
+    return
+  }
+
+  console.log('path.resolve', path.resolve(__dirname, '../build/'))
+  const compiler = webpack({
+    entry: path.resolve(__dirname, '../inject_provider.js'),
+    output: {
+      path: path.resolve(__dirname, '../build/'),
+      filename: 'inject_provider.js',
+    },
+    cache: true,
+  })
+
+  await new Promise((resolve, reject) => {
+    compiler.run((error, stats) => {
+      console.log('error', error)
+      if (error) {
+        return reject(error)
+      }
+
+      resolve(stats)
+    })
+  })
+}
 
 declare global {
   interface Window {
@@ -33,16 +61,7 @@ export const account = accountCreator.create()
 
 beforeAll(async () => {
   console.log('PROVIDER SETUP')
-  await new Promise((resolve, reject) => {
-    compiler.run((error, stats) => {
-      console.log('error', error)
-      if (error) {
-        return reject(error)
-      }
-
-      resolve(stats)
-    })
-  })
+  await compileInjects()
 
   await context.addInitScript({
     path: path.resolve(__dirname, '../build/inject_provider.js'),
