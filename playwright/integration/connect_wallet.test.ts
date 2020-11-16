64ba5313d1206c2b949528b3c8dad461851755f6
Velenir <velenir@gmail.com>
2020-11-16 13:55:57 +0300

    Revert "better xpath query"
    This reverts commit 01ee1b9a22f1cb25003f1bb15156ed72ef185fd5.

--- playwright/integration/connect_wallet.test.ts
+++ playwright/integration/connect_wallet.test.ts
@@ -16,11 +16,8 @@ describe('Connect Wallet', () => {
     await page.click('header >> text=Web3')
 
     // THEN: wallet connected & address available
-    // XPATH: most specific element with textContent starting with 0x and >= address.length
-    // matches account address inside Wallet popup
-    const textWithAddress = await page.textContent(
-      '(//html/body//*[starts-with(.,"0x") and string-length(.)>=42])[last()]',
-    )
+    //                          XPATH:    closest div ancestor of element with text "Copy address to clipboard"
+    const textWithAddress = await page.textContent('//*[text()="Copy address to clipboard"]//ancestor::div[1]')
 
     expect(textWithAddress).toContain(account.address)
   })
