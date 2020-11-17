9f3ec66941c013408d96037f96d108d8b019b6d4
David Sato <david.sato64@gmail.com>
2020-11-17 18:38:14 +0100

    StyledButton base

---
+++ src/styles/common/StyledButton.ts
@@ -0,0 +1,169 @@
+import styled, { css } from 'styled-components'
+import { variants } from 'styled-theming'
+
+import ColourSheet from '../colours'
+import StyleSheet from '../styles'
+
+const {
+  white,
+  whiteDark,
+  blue,
+  blueDark,
+  successLight,
+  successDark,
+  warningLight,
+  warningDark,
+  dangerLight,
+  dangerDark,
+  bgLight,
+  bgDark,
+} = ColourSheet
+
+const { buttonBorder } = StyleSheet
+
+// Used in stories
+// Good to keep around altough not required
+export enum ButtonVariations {
+  'default',
+  'primary',
+  'secondary',
+  'danger',
+  'success',
+  'warning',
+  'cancel',
+}
+
+// Create our variated Button Theme
+// 'kind' refers to a prop on button
+// <ButtonBase kind="danger" />
+export const ButtonTheme = variants('mode', 'kind', {
+  default: {
+    light: css`
+      color: ${white};
+      background-color: ${blue};
+
+      &:hover {
+        background-color: ${blueDark};
+      }
+    `,
+    dark: css`
+      color: ${blue};
+      background-color: ${bgDark};
+
+      &:hover {
+        color: ${white};
+        background-color: ${blueDark};
+      }
+    `,
+  },
+  get primary() {
+    return this.default
+  },
+  secondary: {
+    light: css`
+      color: ${blue};
+      background-color: ${bgLight};
+      border-color: ${blue};
+
+      &:hover {
+        background-color: ${bgDark};
+      }
+    `,
+    dark: css`
+      color: ${blue};
+      background-color: ${bgDark};
+      border-color: ${blue};
+
+      &:hover {
+        color: ${white};
+        background-color: ${blue};
+      }
+    `,
+  },
+  success: {
+    light: css`
+      color: ${white};
+      background-color: ${successLight};
+
+      &:hover {
+        background-color: ${successDark};
+        border-color: ${successDark};
+      }
+    `,
+    dark: css`
+      color: ${white};
+      background-color: ${successDark};
+
+      &:hover {
+        background-color: ${successLight};
+        border-color: ${successLight};
+      }
+    `,
+  },
+  danger: {
+    light: css`
+      color: ${white};
+      background-color: ${dangerLight};
+
+      &:hover {
+        background-color: ${dangerDark};
+        border-color: ${dangerDark};
+      }
+    `,
+    dark: css`
+      color: ${white};
+      background-color: ${dangerDark};
+
+      &:hover {
+        background-color: ${dangerLight};
+        border-color: ${dangerLight};
+      }
+    `,
+  },
+  warning: {
+    light: css`
+      color: ${white};
+      background-color: ${warningLight};
+
+      &:hover {
+        background-color: ${warningDark};
+        border-color: ${warningDark};
+      }
+    `,
+    dark: css`
+      color: ${white};
+      background-color: ${warningDark};
+
+      &:hover {
+        background-color: ${warningLight};
+        border-color: ${warningLight};
+      }
+    `,
+  },
+  cancel: {
+    light: css`
+      color: ${white};
+      background: transparent;
+
+      &:hover {
+        background-color: ${blueDark};
+      }
+    `,
+    dark: css`
+      color: ${blue};
+      background: transparent;
+
+      &:hover {
+        color: ${whiteDark};
+        background-color: ${blueDark};
+      }
+    `,
+  },
+})
+
+export default styled.button`
+  border: ${buttonBorder};
+
+  /* Fold in theme css above */
+  ${ButtonTheme}
+`
