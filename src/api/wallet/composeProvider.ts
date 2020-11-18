2af76012bddb9202f315bc8971601956c2090811
Anxo Rodriguez <anxolin@gmail.com>
2020-11-18 12:18:25 +0100

    Add comments change var

--- src/api/wallet/composeProvider.ts
+++ src/api/wallet/composeProvider.ts
@@ -201,9 +201,11 @@ export const composeProvider = <T extends Provider>(
         if (!txConfig) return false
 
         if (!txConfig.gas) {
+          // Remove gasPrice from the estimation props, since they broke after last hardfork
+          //  https://github.com/gnosis/dex-react/pull/1618
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { gasPrice, ...otherProps } = txConfig
-          const gasLimit = await web3.eth.estimateGas(otherProps).catch((error) => {
+          const { gasPrice, ...txConfigEstimation } = txConfig
+          const gasLimit = await web3.eth.estimateGas(txConfigEstimation).catch((error) => {
             console.error('[composeProvider] Error estimating gas, probably failing transaction', txConfig)
             throw error
           })
