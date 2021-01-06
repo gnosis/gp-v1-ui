4f11386fbec1b4cb37f157d6118a1c911c5ed6af
Anxo Rodriguez <anxolin@gmail.com>
2021-01-06 18:53:54 +0100

    Add entry point and remove unneded files

---
+++ src/apps/trade/index.tsx
@@ -0,0 +1,9 @@
+import 'react-hot-loader'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import TradeApp from './TradeApp'
+
+const root = document.getElementById('root')
+ReactDOM.render(<TradeApp />, root)
