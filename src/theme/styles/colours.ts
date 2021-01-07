7f6bcb8bb996ec23c53fb4f269afd32424010d13
David Sato <david.sato64@gmail.com>
2021-01-07 18:57:40 +0000

    added colours sheet inside theme > styles > colours

--- src/theme/styles/colours.ts
+++ src/theme/styles/colours.ts
@@ -1,106 +1,59 @@
-export const DEFAULT_COLOURS = {
+export const BASE_COLOURS = {
   // base
   white: '#FFF',
   black: '#000',
   whiteDark: '#e9e9f0',
-  red1: '#FF6871',
-  red2: '#F82D3A',
-  green1: '#09371d',
-  green2: '#a9ffcd',
-  yellow1: '#FFE270',
-  yellow2: '#F3841E',
+  red1: '#FF305B',
+  red2: '#FF6871',
+  red3: '#F82D3A',
+  green1: '#00C46E',
+  green2: '#09371d',
+  green3: '#a9ffcd',
+  yellow1: '#f1851d',
+  yellow2: '#f1851d',
   blue1: '#2172E5',
   blue2: '#3F77FF',
-  // whiteDark: '#e9e9f0',
-  // blueDark: '#185afb',
-  // purple: '#8958FF',
-  // bgLight: '#edf2f7',
-  // bgDark: 'linear-gradient(0deg, #21222E 0.05%, #2C2D3F 100%)',
-  // blackLight: '#181a1b',
-  // successLight: '#5ca95c',
-  // successDark: '#00BE2E',
-  // dangerLight: '#e55353',
-  // dangerDark: '#eb4025',
-  // warningLight: '#f1851d',
-  // warningDark: '#f1851d',
-  // disabledLightOpaque: '#ffffff80',
-  // disabledDarkOpaque: '#ffffff80',
-  // disabledLight: '#31323E',
-  // disabledDark: '#31323E',
-  // mainGradient: 'linear-gradient(270deg, #8958FF 0%, #3F77FF 100%)',
-  // mainGradientDarker: 'linear-gradient(270deg, #6a2cff 0%, #185afb 100%)',
+}
+
+export const BASE_GRADIENTS = {
+  gradient1: 'linear-gradient(270deg, #8958FF 0%, #3F77FF 100%);',
+  gradient2: 'linear-gradient(270deg, #8958FF 30%, #3F77FF 100%)',
 }
 
 export const LIGHT_COLOURS = {
   // text
-  text1: '#000000',
-  text2: '#565A69',
-  text3: '#888D9B',
-  text4: '#C3C5CB',
-  text5: '#EDEEF2',
+  primaryText1: BASE_COLOURS.white,
+  secondaryText1: '#EDEDED',
+  secondaryText2: '#9797B8',
   textDisabled: '#31323E',
 
   // backgrounds / greys
   bg1: '#ffc1ff',
   bg2: '#F7F8FA',
-  bg3: '#C1FFC1',
-  bg4: '#00a400',
-  bg5: '#888D9B',
   bgDisabled: '#ffffff80',
 
-  //specialty colors
-  modalBG: 'rgba(0,0,0,0.3)',
-  advancedBG: 'rgba(255,255,255,0.6)',
-
-  //primary colors
-  primary1: '#ff007a',
-  primary2: '#FF8CC3',
-  primary3: '#FF99C9',
-  primary4: '#F6DDE8',
-  primary5: '#FDEAF1',
-
-  // color text
-  primaryText1: '#ff007a',
-
-  // secondary colors
-  secondary1: '#ff007a',
-  secondary2: '#F6DDE8',
-  secondary3: '#FDEAF1',
+  // gradients
+  gradient1: '#8958FF',
+  gradient2: '#3F77FF',
 }
 
 export const DARK_COLOURS = {
   // text
-  text1: '#FFFFFF',
-  text2: '#C3C5CB',
-  text3: '#6C7284',
-  text4: '#565A69',
-  text5: '#2C2F36',
+  primaryText1: BASE_COLOURS.white,
+  secondaryText1: '#EDEDED',
+  secondaryText2: '#9797B8',
   textDisabled: '#31323E',
 
   // backgrounds / greys
   bg1: '#212429',
   bg2: '#2C2F36',
-  bg3: '#40444F',
-  bg4: '#565A69',
-  bg5: '#6C7284',
   bgDisabled: '#ffffff80',
 
-  //specialty colors
-  modalBG: 'rgba(0,0,0,.425)',
-  advancedBG: 'rgba(0,0,0,0.1)',
+  // gradients
+  gradient1: '#8958FF',
+  gradient2: '#3F77FF',
 
-  //primary colors
-  primary1: '#2172E5',
-  primary2: '#3680E7',
-  primary3: '#4D8FEA',
-  primary4: '#376bad70',
-  primary5: '#153d6f70',
-
-  // color text
-  primaryText1: '#6da8ff',
-
-  // secondary colors
-  secondary1: '#2172E5',
-  secondary2: '#17000b26',
-  secondary3: '#17000b26',
+  // TODO: add to theme, not colour palette
+  // gradientForm1: 'linear-gradient(270deg, #8958FF 0%, #3F77FF 100%)',
+  // gradientForm2: 'linear-gradient(270deg, #8958FF 30%, #3F77FF 100%)',
 }
